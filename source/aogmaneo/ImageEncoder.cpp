e79358218f028db2963f52b39e6e9bcaa691407c
Eric Laukien <ericlaukien@gmail.com>
2020-06-15 10:43:50 -0400

    Min delta

--- source/aogmaneo/ImageEncoder.cpp
+++ source/aogmaneo/ImageEncoder.cpp
@@ -230,7 +230,7 @@ void ImageEncoder::initRandom(
     // Hidden Cs
     hiddenCs = ByteBuffer(numHiddenColumns, 0);
 
-    hiddenResources = FloatBuffer(numHidden, 0.5f);
+    hiddenResources = FloatBuffer(numHidden, 1.0f);
 }
 
 void ImageEncoder::step(
