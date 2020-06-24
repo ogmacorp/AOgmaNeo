56b8f16105061862a17b40240b3d910dfffeaadd
Eric Laukien <ericlaukien@gmail.com>
2020-06-24 19:41:08 -0400

    Tuning

--- source/aogmaneo/SparseCoder.h
+++ source/aogmaneo/SparseCoder.h
@@ -63,9 +63,9 @@ public:
     // Defaults
     SparseCoder()
     :
-    alpha(0.1f),
+    alpha(1.0f),
     beta(0.5f),
-    minVigilance(0.8f)
+    minVigilance(0.85f)
     {}
 
     // Create a sparse coding layer with random initialization
