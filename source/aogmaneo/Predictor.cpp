1f9c2d82689f7a5f77ef0336195ad33605b6c374
Eric Laukien <ericlaukien@gmail.com>
2020-07-13 18:53:50 -0400

    Tests

--- source/aogmaneo/Predictor.cpp
+++ source/aogmaneo/Predictor.cpp
@@ -79,44 +79,46 @@ void Predictor::learn(
 
     int targetC = (*hiddenTargetCs)[hiddenColumnIndex];
 
-    int hiddenIndexTarget = address3(Int3(pos.x, pos.y, targetC), hiddenSize);
-    int hiddenIndexMax = address3(Int3(pos.x, pos.y, hiddenCs[hiddenColumnIndex]), hiddenSize);
+    if (targetC != hiddenCs[hiddenColumnIndex]) {
+        int hiddenIndexTarget = address3(Int3(pos.x, pos.y, targetC), hiddenSize);
+        int hiddenIndexMax = address3(Int3(pos.x, pos.y, hiddenCs[hiddenColumnIndex]), hiddenSize);
 
-    for (int vli = 0; vli < visibleLayers.size(); vli++) {
-        VisibleLayer &vl = visibleLayers[vli];
-        const VisibleLayerDesc &vld = visibleLayerDescs[vli];
+        for (int vli = 0; vli < visibleLayers.size(); vli++) {
+            VisibleLayer &vl = visibleLayers[vli];
+            const VisibleLayerDesc &vld = visibleLayerDescs[vli];
 
-        int diam = vld.radius * 2 + 1;
+            int diam = vld.radius * 2 + 1;
 
-        // Projection
-        Float2 hToV = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hiddenSize.x),
-            static_cast<float>(vld.size.y) / static_cast<float>(hiddenSize.y));
+            // Projection
+            Float2 hToV = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hiddenSize.x),
+                static_cast<float>(vld.size.y) / static_cast<float>(hiddenSize.y));
 
-        Int2 visibleCenter = project(pos, hToV);
+            Int2 visibleCenter = project(pos, hToV);
 
-        // Lower corner
-        Int2 fieldLowerBound(visibleCenter.x - vld.radius, visibleCenter.y - vld.radius);
+            // Lower corner
+            Int2 fieldLowerBound(visibleCenter.x - vld.radius, visibleCenter.y - vld.radius);
 
-        // Bounds of receptive field, clamped to input size
-        Int2 iterLowerBound(max(0, fieldLowerBound.x), max(0, fieldLowerBound.y));
-        Int2 iterUpperBound(min(vld.size.x - 1, visibleCenter.x + vld.radius), min(vld.size.y - 1, visibleCenter.y + vld.radius));
+            // Bounds of receptive field, clamped to input size
+            Int2 iterLowerBound(max(0, fieldLowerBound.x), max(0, fieldLowerBound.y));
+            Int2 iterUpperBound(min(vld.size.x - 1, visibleCenter.x + vld.radius), min(vld.size.y - 1, visibleCenter.y + vld.radius));
 
-        for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
-            for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
-                int visibleColumnIndex = address2(Int2(ix, iy), Int2(vld.size.x,  vld.size.y));
+            for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
+                for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
+                    int visibleColumnIndex = address2(Int2(ix, iy), Int2(vld.size.x,  vld.size.y));
 
-                if (vl.inputCsPrev[visibleColumnIndex] != vl.inputCsPrevPrev[visibleColumnIndex]) {
-                    Int2 offset(ix - fieldLowerBound.x, iy - fieldLowerBound.y);
+                    if (vl.inputCsPrev[visibleColumnIndex] != vl.inputCsPrevPrev[visibleColumnIndex]) {
+                        Int2 offset(ix - fieldLowerBound.x, iy - fieldLowerBound.y);
 
-                    unsigned char inC = vl.inputCsPrev[visibleColumnIndex];
+                        unsigned char inC = vl.inputCsPrev[visibleColumnIndex];
 
-                    int wiTarget = inC + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenIndexTarget));
-                    int wiMax = inC + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenIndexMax));
+                        int wiTarget = inC + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenIndexTarget));
+                        int wiMax = inC + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenIndexMax));
 
-                    vl.weights[wiTarget] = min<int>(255 - 1, vl.weights[wiTarget]) + 1;
-                    vl.weights[wiMax] = max<int>(1, vl.weights[wiMax]) - 1;
+                        vl.weights[wiTarget] = min<int>(255 - 1, vl.weights[wiTarget]) + 1;
+                        vl.weights[wiMax] = max<int>(1, vl.weights[wiMax]) - 1;
+                    }
                 }
-            }
+        }
     }
 }
 
