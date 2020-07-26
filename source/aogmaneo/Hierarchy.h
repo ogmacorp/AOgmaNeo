923f23487ac3edf318007e4cc76aade848e742c3
Eric Laukien <ericlaukien@gmail.com>
2020-07-26 12:52:35 -0400

    Tuning

--- source/aogmaneo/Hierarchy.h
+++ source/aogmaneo/Hierarchy.h
@@ -39,11 +39,11 @@ public:
 
         LayerDesc()
         :
-        hiddenSize(4, 4, 16),
-        clumpSize(2, 2),
-        ffRadius(2),
-        pRadius(2),
-        aRadius(2),
+        hiddenSize(4, 4, 32),
+        clumpSize(4, 4),
+        ffRadius(4),
+        pRadius(4),
+        aRadius(4),
         ticksPerUpdate(2),
         temporalHorizon(2),
         historyCapacity(32)
