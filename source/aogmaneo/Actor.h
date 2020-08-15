14b03539463b967664655f80ea7d80327a981196
Eric Laukien <ericlaukien@gmail.com>
2020-08-15 13:46:04 -0400

    Cleanup

--- source/aogmaneo/Actor.h
+++ source/aogmaneo/Actor.h
@@ -88,8 +88,8 @@ public:
     // Defaults
     Actor()
     :
-    alpha(0.02f),
-    beta(0.1f),
+    alpha(0.01f),
+    beta(0.05f),
     gamma(0.99f)
     {}
 
--- source/aogmaneo/Predictor.h
+++ source/aogmaneo/Predictor.h
@@ -64,7 +64,7 @@ public:
     // Defaults
     Predictor()
     :
-    alpha(0.5f)
+    alpha(1.0f)
     {}
 
     // Create with random initialization
