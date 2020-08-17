f7355cbf34a799569705019fd38dace50d569a69
Eric Laukien <ericlaukien@gmail.com>
2020-08-17 11:04:02 -0400

    No early stop

--- source/aogmaneo/SparseCoder.cpp
+++ source/aogmaneo/SparseCoder.cpp
@@ -193,7 +193,7 @@ void SparseCoder::learn(
 
         sum /= max(1, count);
 
-        float delta = alpha * ((vc == targetC ? 1.0f : 0.0f) - sigmoid(vl.reconstruction[visibleIndex]));
+        float delta = alpha * ((vc == targetC ? 1.0f : 0.0f) - sigmoid(sum));
 
         for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
             for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
