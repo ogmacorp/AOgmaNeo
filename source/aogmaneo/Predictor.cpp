dd1d3397b99cfaef9d26f89579390a022a508d15
Eric Laukien <ericlaukien@gmail.com>
2020-08-29 17:30:42 -0400

    Exp scale enc

--- source/aogmaneo/Predictor.cpp
+++ source/aogmaneo/Predictor.cpp
@@ -173,7 +173,7 @@ void Predictor::initRandom(
         vl.weights.resize(numHidden * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = 127;
+            vl.weights[i] = 120 + rand() % 16;
 
         vl.inputCsPrev = ByteBuffer(numVisibleColumns, 0);
     }
--- source/aogmaneo/SparseCoder.cpp
+++ source/aogmaneo/SparseCoder.cpp
@@ -196,10 +196,8 @@ void SparseCoder::initRandom(
         vl.weights.resize(numHidden * area * vld.size.z);
 
         // Initialize to random values
-        char range = 16;
-
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = 255 - rand() % range;
+            vl.weights[i] = 255 - rand() % 16;
 
         vl.reconstruction = FloatBuffer(numVisible, 0);
     }
