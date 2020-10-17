f3a293e7025fcf241bedc57f2d7efc1f1270bc31
Eric Laukien <ericlaukien@gmail.com>
2020-10-17 13:18:06 -0400

    Tuning

--- source/aogmaneo/SparseCoder.cpp
+++ source/aogmaneo/SparseCoder.cpp
@@ -158,7 +158,7 @@ void SparseCoder::initRandom(
 
         if (vld.recurrent) {
             for (int i = 0; i < vl.weights.size(); i++)
-                vl.weights[i] = randf(-0.01f, 0.01f);
+                vl.weights[i] = randf(-0.001f, 0.001f);
         }
         else {
             for (int i = 0; i < vl.weights.size(); i++)
