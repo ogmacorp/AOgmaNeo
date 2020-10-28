608d4e15d599910921c06acdd0c261a7dd93a264
Eric Laukien <ericlaukien@gmail.com>
2020-10-28 15:10:21 -0400

    Latest api

--- CMake/FindAOgmaNeo.cmake
+++ CMake/FindAOgmaNeo.cmake
@@ -12,7 +12,7 @@
 # AOGMANEO_LIBRARY, the name of the library to link against
 # AOGMANEO_FOUND, if false, do not try to link to OgmaNeo
 # AOGMANEO_INCLUDE_DIR, where to find OgmaNeo headers
-#
+
 if(AOGMANEO_INCLUDE_DIR)
     # Already in cache, be silent
     set(AOGMANEO_FIND_QUIETLY TRUE)
@@ -21,6 +21,7 @@ endif(AOGMANEO_INCLUDE_DIR)
 find_path(AOGMANEO_INCLUDE_DIR aogmaneo/Hierarchy.h)
 
 set(AOGMANEO_NAMES aogmaneo AOgmaNeo AOGMANEO)
+
 find_library(AOGMANEO_LIBRARY NAMES ${AOGMANEO_NAMES})
 
 # Per-recommendation
