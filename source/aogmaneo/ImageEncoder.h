7f3e1908d568c93749d3ee6ccf518efe0daa563d
Eric Laukien <ericlaukien@gmail.com>
2020-12-06 08:41:59 -0500

    Tuning

--- source/aogmaneo/ImageEncoder.h
+++ source/aogmaneo/ImageEncoder.h
@@ -66,7 +66,7 @@ public:
     // Defaults
     ImageEncoder()
     :
-    alpha(0.05f)
+    alpha(0.03f)
     {}
 
     // Create a sparse coding layer with random initialization
--- source/aogmaneo/SparseCoder.h
+++ source/aogmaneo/SparseCoder.h
@@ -74,7 +74,7 @@ public:
     // Defaults
     SparseCoder()
     :
-    alpha(0.05f)
+    alpha(0.03f)
     {}
 
     // Create a sparse coding layer with random initialization
