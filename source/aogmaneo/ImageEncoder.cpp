224c3fe1d6e3385befc7838f8b2fd1087bb75fab
Eric Laukien <ericlaukien@gmail.com>
2020-12-14 12:47:53 -0500

    Fixes

--- source/aogmaneo/ImageEncoder.cpp
+++ source/aogmaneo/ImageEncoder.cpp
@@ -82,6 +82,8 @@ void ImageEncoder::forward(
 
             int hiddenCellIndex = address3(Int3(columnPos.x, columnPos.y, hc), hiddenSize);
 
+            float rate = (dhc == 0 ? 1.0f : 0.5f) * hiddenRates[hiddenCellIndex];
+
             // For each visible layer
             for (int vli = 0; vli < visibleLayers.size(); vli++) {
                 VisibleLayer &vl = visibleLayers[vli];
@@ -115,12 +117,12 @@ void ImageEncoder::forward(
 
                             unsigned char weight = vl.protos[wiStart + vc];
 
-                            vl.protos[wiStart + vc] = roundftoi(min(255.0f, max(0.0f, weight + hiddenRates[hiddenCellIndex] * (static_cast<float>(input) - static_cast<float>(weight)))));
+                            vl.protos[wiStart + vc] = roundftoi(min(255.0f, max(0.0f, weight + rate * (static_cast<float>(input) - static_cast<float>(weight)))));
                         }
                     }
             }
 
-            hiddenRates[hiddenCellIndex] -= alpha * hiddenRates[hiddenCellIndex];
+            hiddenRates[hiddenCellIndex] -= alpha * rate;
         }
     }
 }
