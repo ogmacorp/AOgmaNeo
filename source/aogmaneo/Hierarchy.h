c3f52df931b9e5c6dc16429f38b9df2e3b349ce1
Eric Laukien <ericlaukien@gmail.com>
2020-12-20 18:55:23 -0500

    Update

--- source/aogmaneo/Hierarchy.h
+++ source/aogmaneo/Hierarchy.h
@@ -65,7 +65,6 @@ public:
     // Describes a layer for construction. For the first layer, the IODesc overrides the parameters that are the same name
     struct LayerDesc {
         Int3 hiddenSize; // Size of hidden layer
-        int numPriorities;
 
         int ffRadius; // Feed forward radius
         int pRadius; // Prediction radius
@@ -76,7 +75,6 @@ public:
         LayerDesc()
         :
         hiddenSize(4, 4, 16),
-        numPriorities(4),
         ffRadius(2),
         pRadius(2),
         ticksPerUpdate(2),
@@ -85,7 +83,6 @@ public:
 
         LayerDesc(
             const Int3 &hiddenSize,
-            int numPriorities,
             int ffRadius,
             int pRadius,
             int ticksPerUpdate,
@@ -93,7 +90,6 @@ public:
         )
         :
         hiddenSize(hiddenSize),
-        numPriorities(numPriorities),
         ffRadius(ffRadius),
         pRadius(pRadius),
         ticksPerUpdate(ticksPerUpdate),
