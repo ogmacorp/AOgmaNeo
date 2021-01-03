f7220a3d0190cc78c79059209620a15717d8a4b8
Eric Laukien <ericlaukien@gmail.com>
2021-01-03 09:49:28 -0500

    Tests

--- source/aogmaneo/Predictor.cpp
+++ source/aogmaneo/Predictor.cpp
@@ -226,7 +226,7 @@ void Predictor::initRandom(
 
         // Initialize to random values
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(-1.0f, 1.0f);
+            vl.weights[i] = randf(-0.01f, 0.01f);
 
         vl.inputCIsPrev = IntBuffer(numVisibleColumns, 0);
     }
