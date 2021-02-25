1c111761d77c27bc74b7184034a375b2f6216d70
Eric Laukien <ericlaukien@gmail.com>
2021-02-25 14:25:13 -0500

    Impl

--- source/aogmaneo/SparseCoder.h
+++ source/aogmaneo/SparseCoder.h
@@ -62,8 +62,8 @@ public:
     // Defaults
     SparseCoder()
     :
-    alpha(0.01f),
-    beta(0.001f)
+    alpha(0.1f),
+    beta(0.01f)
     {}
 
     // Create a sparse coding layer with random initialization
