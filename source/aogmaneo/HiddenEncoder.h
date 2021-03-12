13f61bd4a15cccf2d3206f65094de875df96c0a2
Eric Laukien <ericlaukien@gmail.com>
2021-03-11 19:16:48 -0500

    Updated API

---
+++ source/aogmaneo/HiddenEncoder.h
@@ -0,0 +1,128 @@
+// ----------------------------------------------------------------------------
+//  AOgmaNeo
+//  Copyright(c) 2020 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of AOgmaNeo is licensed to you under the terms described
+//  in the AOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include "Helpers.h"
+
+namespace aon {
+// Sparse coder
+class HiddenEncoder {
+public:
+    // Visible layer descriptor
+    struct VisibleLayerDesc {
+        Int3 size; // Size of input
+
+        int radius; // Radius onto input
+
+        // Defaults
+        VisibleLayerDesc()
+        :
+        size(4, 4, 16),
+        radius(2)
+        {}
+    };
+
+    // Visible layer
+    struct VisibleLayer {
+        FloatBuffer weights;
+
+        FloatBuffer reconstruction;
+    };
+
+private:
+    Int3 hiddenSize; // Size of hidden/output layer
+
+    IntBuffer hiddenCIs;
+
+    // Visible layers and associated descriptors
+    Array<VisibleLayer> visibleLayers;
+    Array<VisibleLayerDesc> visibleLayerDescs;
+    
+    // --- Kernels ---
+    
+    void forward(
+        const Int2 &columnPos,
+        const Array<const IntBuffer*> &inputCIs
+    );
+
+    void learn(
+        const Int2 &columnPos,
+        const IntBuffer* inputCIs,
+        int vli
+    );
+
+public:
+    float lr; // Learning rate
+
+    HiddenEncoder()
+    :
+    lr(0.1f)
+    {}
+
+    // Create a sparse coding layer with random initialization
+    void initRandom(
+        const Int3 &hiddenSize, // Hidden/output size
+        const Array<VisibleLayerDesc> &visibleLayerDescs // Descriptors for visible layers
+    );
+
+    void step(
+        const Array<const IntBuffer*> &inputCIs, // Input states
+        bool learnEnabled // Whether to learn
+    );
+
+    // Serialization
+    int size() const; // Returns size in bytes
+    int stateSize() const; // Returns size of state in bytes
+
+    void write(
+        StreamWriter &writer
+    ) const;
+
+    void read(
+        StreamReader &reader
+    );
+
+    void writeState(
+        StreamWriter &writer
+    ) const;
+
+    void readState(
+        StreamReader &reader
+    );
+
+    // Get the number of visible layers
+    int getNumVisibleLayers() const {
+        return visibleLayers.size();
+    }
+
+    // Get a visible layer
+    const VisibleLayer &getVisibleLayer(
+        int i // Index of visible layer
+    ) const {
+        return visibleLayers[i];
+    }
+
+    // Get a visible layer descriptor
+    const VisibleLayerDesc &getVisibleLayerDesc(
+        int i // Index of visible layer
+    ) const {
+        return visibleLayerDescs[i];
+    }
+
+    // Get the hidden states
+    const IntBuffer &getHiddenCIs() const {
+        return hiddenCIs;
+    }
+
+    // Get the hidden size
+    const Int3 &getHiddenSize() const {
+        return hiddenSize;
+    }
+};
+} // namespace aon
