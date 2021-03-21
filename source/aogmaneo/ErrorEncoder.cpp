eb43a40ad040ff2831808e99a1992ab40e39e6b3
Eric Laukien <ericlaukien@gmail.com>
2021-03-21 12:44:18 -0400

    Softmax again

--- source/aogmaneo/ErrorEncoder.cpp
+++ source/aogmaneo/ErrorEncoder.cpp
@@ -130,12 +130,30 @@ void ErrorEncoder::forward(
 
         sum /= max(1, count);
 
-        hiddenActivations[hiddenCellIndex] = sigmoid(sum);
+        hiddenActivations[hiddenCellIndex] = sum;
 
         if (sum > maxActivation || maxIndex == -1) {
             maxActivation = sum;
             maxIndex = hc;
         }
+    }
+
+    float total = 0.0f;
+
+    for (int hc = 0; hc < hiddenSize.z; hc++) {
+        int hiddenCellIndex = address3(Int3(columnPos.x, columnPos.y, hc), hiddenSize);
+
+        hiddenActivations[hiddenCellIndex] = expf(hiddenActivations[hiddenCellIndex] - maxActivation);
+
+        total += hiddenActivations[hiddenCellIndex];
+    }
+
+    float scale = 1.0f / max(0.0001f, total);
+
+    for (int hc = 0; hc < hiddenSize.z; hc++) {
+        int hiddenCellIndex = address3(Int3(columnPos.x, columnPos.y, hc), hiddenSize);
+
+        hiddenActivations[hiddenCellIndex] *= scale;
     }
 
     hiddenCIs[hiddenColumnIndex] = maxIndex;
