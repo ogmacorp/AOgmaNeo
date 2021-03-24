404dab82b45c123c55ac65564c360c888a708370
Eric Laukien <ericlaukien@gmail.com>
2021-03-24 11:06:59 -0400

    Tests

--- source/aogmaneo/Predictor.h
+++ source/aogmaneo/Predictor.h
@@ -65,8 +65,8 @@ public:
     // Defaults
     Predictor()
     :
-    alpha(0.1f),
-    targetRange(0.05f)
+    alpha(0.2f),
+    targetRange(0.1f)
     {}
 
     // Create with random initialization
