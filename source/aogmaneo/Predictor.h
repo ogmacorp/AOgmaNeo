056fdbef08c97047936756656aa5a2cb315ac0e5
Eric Laukien <ericlaukien@gmail.com>
2021-03-24 10:57:58 -0400

    Target range again

--- source/aogmaneo/Predictor.h
+++ source/aogmaneo/Predictor.h
@@ -65,8 +65,8 @@ public:
     // Defaults
     Predictor()
     :
-    alpha(0.5f),
-    targetRange(0.1f)
+    alpha(0.1f),
+    targetRange(0.05f)
     {}
 
     // Create with random initialization
