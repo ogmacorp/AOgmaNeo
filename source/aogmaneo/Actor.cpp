f3651cce7bcc3c3df8f2397f613cd52c96aee873
Eric Laukien <ericlaukien@gmail.com>
2021-04-17 16:00:20 -0400

    Tests

--- source/aogmaneo/Actor.cpp
+++ source/aogmaneo/Actor.cpp
@@ -157,8 +157,8 @@ void Actor::forward(
                     for (int vc = 0; vc < vld.size.z; vc++) {
                         int wi = vc + wiStart;
 
-                        if (vc == inCIPrev)
-                            vl.traces[wi] = (hc == targetCI ? vl.traces[wi] * traceDecay + 1.0f : 0.0f);
+                        if (vc == inCIPrev && hc == targetCI)
+                            vl.traces[wi] = vl.traces[wi] * traceDecay + 1.0f;
                         else
                             vl.traces[wi] *= traceDecay;
 
