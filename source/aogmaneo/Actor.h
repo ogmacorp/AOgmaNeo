dc96c6ac7a5687ec1078f2436cfc5ec7e2abcd58
Eric Laukien <ericlaukien@gmail.com>
2021-04-25 13:35:56 -0400

    Tuning

--- source/aogmaneo/Actor.h
+++ source/aogmaneo/Actor.h
@@ -90,8 +90,8 @@ public:
     vlr(0.01f),
     alr(0.01f),
     discount(0.99f),
-    minSteps(16),
-    historyIters(16)
+    minSteps(8),
+    historyIters(3)
     {}
 
     // Initialized randomly
