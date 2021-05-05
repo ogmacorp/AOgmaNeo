3cdc329d1ebe0f606a1f0554795cbd957b2756d2
Eric Laukien <ericlaukien@gmail.com>
2021-05-04 20:27:56 -0400

    Tests

--- source/aogmaneo/Actor.cpp
+++ source/aogmaneo/Actor.cpp
@@ -116,7 +116,7 @@ void Actor::forward(
         nextValue += hiddenValues[hiddenCellIndex] * (hc == maxIndex ? 1.0f - epsilon : epsilon / (hiddenSize.z - 1));
     }
 
-    float delta = lr * tanh(reward + discount * nextValue - valuePrev);
+    float delta = lr * (reward + discount * nextValue - valuePrev);
 
     for (int hc = 0; hc < hiddenSize.z; hc++) {
         int hiddenCellIndex = address3(Int3(columnPos.x, columnPos.y, hc), hiddenSize);
