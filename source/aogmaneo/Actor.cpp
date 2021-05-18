eb39cee62b3a043da34126b1e7414ead6ff01555
Eric Laukien <ericlaukien@gmail.com>
2021-05-18 14:49:51 -0400

    Impl

--- source/aogmaneo/Actor.cpp
+++ source/aogmaneo/Actor.cpp
@@ -270,7 +270,7 @@ void Actor::learn(
 
         hiddenActivations[hiddenCellIndex] *= scale;
 
-        float deltaAction = (mimic ? beta : beta * tanh(tdErrorValue)) * ((hc == targetCI ? 1.0f : 0.0f) - hiddenActivations[hiddenCellIndex]);
+        float deltaAction = (mimic || tdErrorValue > 0.0f ? beta : -0.5f * beta) * ((hc == targetCI ? 1.0f : 0.0f) - hiddenActivations[hiddenCellIndex]);
 
         for (int vli = 0; vli < visibleLayers.size(); vli++) {
             VisibleLayer &vl = visibleLayers[vli];
