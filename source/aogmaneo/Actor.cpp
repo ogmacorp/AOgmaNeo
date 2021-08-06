25d561de708d804c8b6bd8e96649b28b89a16cd5
Eric Laukien <ericlaukien@gmail.com>
2021-08-06 13:43:03 -0400

    Tuning

--- source/aogmaneo/Actor.cpp
+++ source/aogmaneo/Actor.cpp
@@ -198,7 +198,7 @@ void Actor::learn(
 
     float newValue = q + g * maxActivation;
 
-    float delta = lr * tanh(newValue - sumPrev);
+    float delta = lr * (newValue - sumPrev);
 
     for (int vli = 0; vli < visibleLayers.size(); vli++) {
         VisibleLayer &vl = visibleLayers[vli];
