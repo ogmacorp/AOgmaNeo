5641239f11560681b1360c51d886b4e6ea50bf0e
Eric Laukien <ericlaukien@gmail.com>
2021-08-10 18:50:14 -0400

    Impl

--- source/aogmaneo/Hierarchy.cpp
+++ source/aogmaneo/Hierarchy.cpp
@@ -143,7 +143,7 @@ void Hierarchy::initRandom(
         hiddenCIsPrev[l] = eLayers[l].getHiddenCIs();
 
         // Default recurrences
-        setRecurrence(l, 0.5f);
+        setRecurrence(l, 0.25f);
     }
 }
 
