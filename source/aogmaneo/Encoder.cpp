adef5e769143670a10f1e7c91a6a8c01393bbc0a
Eric Laukien <ericlaukien@gmail.com>
2021-08-10 12:31:24 -0400

    Fix in save load

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -202,7 +202,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = rand() % 9 - 4;
+            vl.weights[i] = rand() % 9;
 
         vl.reconstruction = SByteBuffer(numVisibleCells, 0);
     }
