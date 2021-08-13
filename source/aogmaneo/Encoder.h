e3959c39a2b6697dafdf07009f4d03f4b65f940e
Eric Laukien <ericlaukien@gmail.com>
2021-08-13 17:22:36 -0400

    Tuning

--- source/aogmaneo/Encoder.h
+++ source/aogmaneo/Encoder.h
@@ -84,8 +84,8 @@ public:
     // Defaults
     Encoder()
     :
-    lr(0.02f),
-    falloff(0.02f)
+    lr(0.01f),
+    falloff(0.01f)
     {}
 
     // Create a sparse coding layer with random initialization
