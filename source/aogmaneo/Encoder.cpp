85610748d9e2065c4c1ff7450a20abd5e48295a3
Eric Laukien <ericlaukien@gmail.com>
2021-09-02 22:55:34 -0400

    Fixes to save load

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -247,7 +247,7 @@ int Encoder::size() const {
     for (int vli = 0; vli < visibleLayers.size(); vli++) {
         const VisibleLayer &vl = visibleLayers[vli];
 
-        size += sizeof(VisibleLayerDesc) + 2 * vl.weights.size() * sizeof(float) + sizeof(float);
+        size += sizeof(VisibleLayerDesc) + vl.weights.size() * sizeof(float) + sizeof(float);
     }
 
     return size;
