f82901cae63c2e76baf24a36fa64fc1544aab8c5
Eric Laukien <ericlaukien@gmail.com>
2021-10-10 20:40:07 -0400

    Tests

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -121,7 +121,7 @@ void Decoder::learn(
 
         sum /= max(1, count);
 
-        float delta = lr * ((hc == targetCI) * strength - sum);
+        float delta = lr * strength * ((hc == targetCI) - sigmoid(sum));
             
         for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
             for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
