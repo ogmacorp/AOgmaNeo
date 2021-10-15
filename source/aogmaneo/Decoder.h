55dc35164c202f063bfae4a73b90a8beee27cf2c
Eric Laukien <ericlaukien@gmail.com>
2021-10-14 22:00:08 -0400

    Tests

--- source/aogmaneo/Decoder.h
+++ source/aogmaneo/Decoder.h
@@ -80,8 +80,8 @@ public:
     // Defaults
     Decoder()
     :
-    lr(0.05f),
-    discount(0.95f),
+    lr(0.1f),
+    discount(0.97f),
     genGoalNoise(0.1f),
     qSteps(5),
     historyIters(16)
