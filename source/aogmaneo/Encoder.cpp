d17cec90faa0c3d083e8d7f5a28bafdac35b85f7
Eric Laukien <ericlaukien@gmail.com>
2021-11-03 17:49:31 -0400

    Tests

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -159,11 +159,8 @@ void Encoder::learn(
         for (int vc = 0; vc < vld.size.z; vc++) {
             int visibleCellIndex = vc + visibleCellsStart;
 
-            float delta = lr * ((vc == targetCI) - min(1.0f, max(0.0f, vl.reconstruction[visibleCellIndex])));
+            float delta = lr * ((vc == targetCI) - expf(vl.reconstruction[visibleCellIndex]));
 
-            if (delta == 0.0f)
-                continue;
-      
             for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
                 for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
                     Int2 hiddenPos = Int2(ix, iy);
@@ -294,7 +291,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(0.99f, 1.0f);
+            vl.weights[i] = randf(0.0f, 0.01f);
 
         vl.reconstruction = FloatBuffer(numVisibleCells, 0.0f);
     }
