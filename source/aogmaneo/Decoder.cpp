a055dbc27f7b5e376fb80c45297bc518f95bb174
Eric Laukien <ericlaukien@gmail.com>
2021-11-09 11:58:32 -0500

    Tests

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -223,7 +223,7 @@ void Decoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(-1.0f, 1.0f);
+            vl.weights[i] = randf(-0.01f, 0.01f);
     }
 
     // Hidden CIs
--- source/aogmaneo/StateAdapter.h
+++ source/aogmaneo/StateAdapter.h
@@ -50,8 +50,8 @@ public:
     // Defaults
     StateAdapter()
     :
-    lr(0.1f),
-    discount(0.95f),
+    lr(0.01f),
+    discount(0.99f),
     historyIters(16)
     {}
 
