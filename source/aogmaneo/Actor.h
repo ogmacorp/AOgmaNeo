9e8e7eab0a77a8bb4395014e918cfd9f63877917
Eric Laukien <ericlaukien@gmail.com>
2021-12-12 13:49:40 -0500

    Tests

--- source/aogmaneo/Actor.h
+++ source/aogmaneo/Actor.h
@@ -91,9 +91,9 @@ public:
     vlr(0.01f),
     alr(0.01f),
     discount(0.99f),
-    temperature(0.1f),
+    temperature(0.01f),
     minSteps(8),
-    historyIters(8)
+    historyIters(16)
     {}
 
     // Initialized randomly
