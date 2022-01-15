091e9dcf2af5c5940c792a33d731ed52976987a3
Eric Laukien <ericlaukien@gmail.com>
2022-01-15 11:45:36 -0500

    Sigmoid

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -139,7 +139,7 @@ void Encoder::backward(
 
     sum /= max(0.0001f, total);
 
-    vl.reconstruction[visibleColumnIndex] = tanh(max(0.0f, sum));
+    vl.reconstruction[visibleColumnIndex] = sigmoid(sum);
 }
 
 void Encoder::learn(
@@ -207,7 +207,7 @@ void Encoder::learn(
 
         sum /= max(0.0001f, total);
 
-        float delta = lr * ((vc == targetCI) - tanh(max(0.0f, sum)));
+        float delta = lr * ((vc == targetCI) - sigmoid(sum));
   
         for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
             for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
