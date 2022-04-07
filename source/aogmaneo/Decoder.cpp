5fea5c1d29ea2f4fc5351d0d56ef7db234ace218
Eric Laukien <ericlaukien@gmail.com>
2022-04-07 14:26:54 -0400

    Cleanup

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -299,7 +299,7 @@ void Decoder::generateErrors(
 }
 
 int Decoder::size() const {
-    int size = sizeof(Int3) + 2 * sizeof(float) + hiddenActivations.size() * sizeof(float) + hiddenCIs.size() * sizeof(int) + sizeof(int);
+    int size = sizeof(Int3) + sizeof(float) + hiddenActivations.size() * sizeof(float) + hiddenCIs.size() * sizeof(int) + sizeof(int);
 
     for (int vli = 0; vli < visibleLayers.size(); vli++) {
         const VisibleLayer &vl = visibleLayers[vli];
