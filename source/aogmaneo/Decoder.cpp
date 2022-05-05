5c5b6d200ff9943bca09c6985cd0f82ab7b2711a
Eric Laukien <ericlaukien@gmail.com>
2022-05-05 19:49:59 -0400

    Sigmoid

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -65,7 +65,7 @@ void Decoder::forward(
 
         sum /= count;
 
-        hiddenActivations[hiddenCellIndex] = min(1.0f, max(0.0f, sum));
+        hiddenActivations[hiddenCellIndex] = sigmoid(sum);
 
         if (sum > maxActivation || maxIndex == -1) {
             maxActivation = sum;
