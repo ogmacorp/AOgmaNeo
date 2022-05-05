b9ce53b8436df00b6c797f2fc25b6d52cb91d422
Eric Laukien <ericlaukien@gmail.com>
2022-05-05 12:04:14 -0400

    Tests

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -65,7 +65,7 @@ void Decoder::forward(
 
         sum /= count;
 
-        hiddenActivations[hiddenCellIndex] = sigmoid(sum);
+        hiddenActivations[hiddenCellIndex] = min(1.0f, max(0.0f, sum));
 
         if (sum > maxActivation || maxIndex == -1) {
             maxActivation = sum;
