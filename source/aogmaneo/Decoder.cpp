a203d2141ca384ad5a8c37e72028a0092aef68f7
Eric Laukien <ericlaukien@gmail.com>
2022-05-06 14:08:41 -0400

    Tests

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -134,6 +134,7 @@ void Decoder::learn(
 
         float rate = (di == maxDendriteIndex ? lr : boost);
         float activation = hiddenActivations[di + hiddenColumnIndex * numDendrites];
+        activation *= activation;
 
         int diam = vld.radius * 2 + 1;
 
