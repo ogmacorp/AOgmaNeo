40ec5e22fbf7842944cf2563b28823189a98f563
Eric Laukien <ericlaukien@gmail.com>
2022-05-24 12:41:44 -0400

    Impl

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -323,7 +323,7 @@ void Decoder::read(
     int diam = vld.radius * 2 + 1;
     int area = diam * diam;
 
-    vl.weights.resize(numHiddenCells * area * vld.size.z);
+    vl.weights.resize(numHiddenCells * area * vld.size.z * vld.size.z);
 
     reader.read(reinterpret_cast<void*>(&vl.weights[0]), vl.weights.size() * sizeof(float));
 
