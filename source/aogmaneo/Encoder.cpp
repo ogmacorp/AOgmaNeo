3d22fce46e04bb7cc632c1fda114f225ce52c69b
Eric Laukien <ericlaukien@gmail.com>
2022-05-29 17:01:43 -0400

    Experiments

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -154,7 +154,7 @@ void Encoder::learn(
 
                     int wi = vc + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenCellIndexMax));
 
-                    vl.weights[wi] += delta * vl.weights[wi] * (1.0f - vl.weights[wi]);
+                    vl.weights[wi] += delta * vl.weights[wi];
                 }
             }
     }
@@ -188,7 +188,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(0.0f, 1.0f);
+            vl.weights[i] = randf(0.99f, 1.0f);
     }
 
     hiddenCIs = IntBuffer(numHiddenColumns, 0);
