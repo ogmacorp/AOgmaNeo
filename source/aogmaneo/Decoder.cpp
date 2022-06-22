5bc5ac0685c382435d7f44cbf51f3c79749c612d
Eric Laukien <ericlaukien@gmail.com>
2022-06-21 22:35:19 -0400

    Experiments

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -57,7 +57,7 @@ void Decoder::forward(
 
                     int wi = inCI + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenCellIndex));
 
-                    sum += logf(max(0.0001f, vl.weights[wi]));
+                    sum += weightLookup(vl.weights[wi]);
                 }
         }
 
@@ -84,6 +84,56 @@ void Decoder::learn(
 
     int targetCI = (*hiddenTargetCIs)[hiddenColumnIndex];
 
+    if (hiddenCIs[hiddenColumnIndex] != targetCI) {
+        // Select strongest dendrite
+        int maxDendriteIndex = -1;
+        float maxDendriteActivation = -999999.0f;
+
+        for (int di = 0; di < numDendrites; di++) {
+            int hiddenCellIndex = (hiddenCIs[hiddenColumnIndex] * numDendrites + di) + hiddenCellsStart;
+
+            if (hiddenActivations[hiddenCellIndex] > maxDendriteActivation || maxDendriteIndex == -1) {
+                maxDendriteActivation = hiddenActivations[hiddenCellIndex];
+                maxDendriteIndex = di;
+            }
+        }
+
+        int hiddenCellIndexMax = (hiddenCIs[hiddenColumnIndex] * numDendrites + maxDendriteIndex) + hiddenCellsStart;
+
+        for (int vli = 0; vli < visibleLayers.size(); vli++) {
+            VisibleLayer &vl = visibleLayers[vli];
+            const VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+            int diam = vld.radius * 2 + 1;
+
+            // Projection
+            Float2 hToV = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hiddenSize.x),
+                static_cast<float>(vld.size.y) / static_cast<float>(hiddenSize.y));
+
+            Int2 visibleCenter = project(columnPos, hToV);
+
+            // Lower corner
+            Int2 fieldLowerBound(visibleCenter.x - vld.radius, visibleCenter.y - vld.radius);
+
+            // Bounds of receptive field, clamped to input size
+            Int2 iterLowerBound(max(0, fieldLowerBound.x), max(0, fieldLowerBound.y));
+            Int2 iterUpperBound(min(vld.size.x - 1, visibleCenter.x + vld.radius), min(vld.size.y - 1, visibleCenter.y + vld.radius));
+
+            for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
+                for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
+                    int visibleColumnIndex = address2(Int2(ix, iy), Int2(vld.size.x,  vld.size.y));
+
+                    int inCIPrev = vl.inputCIsPrev[visibleColumnIndex];
+
+                    Int2 offset(ix - fieldLowerBound.x, iy - fieldLowerBound.y);
+
+                    int wi = inCIPrev + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenCellIndexMax));
+
+                    vl.weights[wi] = max(0.0f, vl.weights[wi] - lr);
+                }
+        }
+    }
+
     // Select strongest dendrite
     int maxDendriteIndex = -1;
     float maxDendriteActivation = -999999.0f;
@@ -105,6 +155,8 @@ void Decoder::learn(
         for (int vli = 0; vli < visibleLayers.size(); vli++) {
             VisibleLayer &vl = visibleLayers[vli];
             const VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+            const float visibleSizeZInv = 1.0f / vld.size.z;
 
             int diam = vld.radius * 2 + 1;
 
@@ -134,7 +186,7 @@ void Decoder::learn(
                     for (int vc = 0; vc < vld.size.z; vc++) {
                         int wi = vc + wiStart;
 
-                        vl.weights[wi] += rate * ((vc == inCIPrev) - vl.weights[wi]);
+                        vl.weights[wi] = min(1.0f, max(0.0f, vl.weights[wi] + rate * ((vc == inCIPrev) - visibleSizeZInv)));
                     }
                 }
         }
@@ -170,7 +222,7 @@ void Decoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(0.0f, 0.01f);
+            vl.weights[i] = randf(0.0f, 0.0001f);
 
         vl.inputCIsPrev = IntBuffer(numVisibleColumns, 0);
     }
@@ -216,7 +268,7 @@ int Decoder::size() const {
         const VisibleLayer &vl = visibleLayers[vli];
         const VisibleLayerDesc &vld = visibleLayerDescs[vli];
 
-        size += sizeof(VisibleLayerDesc) + 2 * vl.weights.size() * sizeof(float) + vl.inputCIsPrev.size() * sizeof(int);
+        size += sizeof(VisibleLayerDesc) + vl.weights.size() * sizeof(float) + vl.inputCIsPrev.size() * sizeof(int);
     }
 
     return size;
