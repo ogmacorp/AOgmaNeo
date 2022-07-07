f32813dca8d5025e4599cf8b4bd6a703b19805ab
Eric Laukien <ericlaukien@gmail.com>
2022-07-07 11:55:31 -0400

    Impl

--- source/aogmaneo/Hierarchy.cpp
+++ source/aogmaneo/Hierarchy.cpp
@@ -193,8 +193,7 @@ void Hierarchy::initRandom(
 void Hierarchy::step(
     const Array<const IntBuffer*> &inputCIs,
     bool learnEnabled,
-    float reward,
-    bool mimic
+    float reward
 ) {
     // First tick is always 0
     ticks[0] = 0;
@@ -272,7 +271,7 @@ void Hierarchy::step(
 
             if (l == 0) {
                 for (int d = 0; d < aLayers.size(); d++)
-                    aLayers[d].step(layerInputCIs, inputCIs[iIndices[d + ioSizes.size()]], reward, learnEnabled, mimic);
+                    aLayers[d].step(layerInputCIs, inputCIs[iIndices[d + ioSizes.size()]], reward, learnEnabled);
             }
         }
     }
