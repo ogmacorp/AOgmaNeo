fe6b47c47781fece10242ae837865487c04ad4bf
Eric Laukien <ericlaukien@gmail.com>
2022-07-22 09:59:41 -0400

    Squash td error again

--- source/aogmaneo/Actor.cpp
+++ source/aogmaneo/Actor.cpp
@@ -98,7 +98,7 @@ void Actor::activate(
 
     hiddenCIs[hiddenColumnIndex] = maxIndex;
 
-    hiddenTDErrors[hiddenColumnIndex] = reward + discount * maxActivation - valuePrev;
+    hiddenTDErrors[hiddenColumnIndex] = tanh(reward + discount * maxActivation - valuePrev);
 }
 
 void Actor::learn(
