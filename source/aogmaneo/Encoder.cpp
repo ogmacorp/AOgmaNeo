7303556993d193920561be75b13c8105b8b26866
Eric Laukien <ericlaukien@gmail.com>
2022-08-12 14:48:08 -0400

    Impl

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -37,7 +37,7 @@ void Encoder::forward(
             if (act == 0.0f)
                 continue;
 
-            float delta = lr * (*hiddenErrors)[hiddenCellIndex] * (1.0f - act * act) - reg * (numNonZero > 1);
+            float delta = lr * tanhf((*hiddenErrors)[hiddenCellIndex]) * (1.0f - act * act) - reg * (numNonZero > 1);
 
             for (int vli = 0; vli < visibleLayers.size(); vli++) {
                 VisibleLayer &vl = visibleLayers[vli];
