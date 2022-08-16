7fc9c1c9147fd240cf03ac4b84a6f2f339861c0d
Eric Laukien <ericlaukien@gmail.com>
2022-08-16 12:07:04 -0400

    Q

--- source/aogmaneo/Hierarchy.h
+++ source/aogmaneo/Hierarchy.h
@@ -102,8 +102,7 @@ public:
     void step(
         const Array<const IntBuffer*> &inputCIs, // Inputs to remember
         bool learnEnabled = true, // Whether learning is enabled
-        float reward = 0.0f, // Reward
-        bool mimic = false // Mimicry mode
+        float reward = 0.0f // Reward
     );
 
     // Serialization
