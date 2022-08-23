7fbf825ca6fed77b8590c9ca8d0236cb1b69722d
Eric Laukien <ericlaukien@gmail.com>
2022-08-23 19:42:04 -0400

    Tests

---
+++ source/aogmaneo/Encoder.cpp
@@ -0,0 +1,332 @@
+// ----------------------------------------------------------------------------
+//  AOgmaNeo
+//  Copyright(c) 2020-2022 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of AOgmaNeo is licensed to you under the terms described
+//  in the AOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#include "ReconEncoder.h"
+
+using namespace aon;
+
+void ReconEncoder::forward(
+    const Int2 &columnPos,
+    const Array<const IntBuffer*> &inputCIs
+) {
+    int hiddenColumnIndex = address2(columnPos, Int2(hiddenSize.x, hiddenSize.y));
+
+    int hiddenCellsStart = hiddenColumnIndex * hiddenSize.z;
+
+    int maxIndex = -1;
+    float maxActivation = -999999.0f;
+
+    for (int hc = 0; hc < hiddenSize.z; hc++) {
+        int hiddenCellIndex = hc + hiddenCellsStart;
+
+        float sum = 0.0f;
+
+        for (int vli = 0; vli < visibleLayers.size(); vli++) {
+            VisibleLayer &vl = visibleLayers[vli];
+            const VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+            int diam = vld.radius * 2 + 1;
+ 
+            // Projection
+            Float2 hToV = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hiddenSize.x),
+                static_cast<float>(vld.size.y) / static_cast<float>(hiddenSize.y));
+
+            Int2 visibleCenter = project(columnPos, hToV);
+
+            // Lower corner
+            Int2 fieldLowerBound(visibleCenter.x - vld.radius, visibleCenter.y - vld.radius);
+
+            // Bounds of receptive field, clamped to input size
+            Int2 iterLowerBound(max(0, fieldLowerBound.x), max(0, fieldLowerBound.y));
+            Int2 iterUpperBound(min(vld.size.x - 1, visibleCenter.x + vld.radius), min(vld.size.y - 1, visibleCenter.y + vld.radius));
+
+            float subSum = 0.0f;
+            int subCount = (iterUpperBound.x - iterLowerBound.x + 1) * (iterUpperBound.y - iterLowerBound.y + 1);
+
+            for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
+                for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
+                    int visibleColumnIndex = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
+
+                    Int2 offset(ix - fieldLowerBound.x, iy - fieldLowerBound.y);
+
+                    int inCI = (*inputCIs[vli])[visibleColumnIndex];
+
+                    int wi = inCI + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenCellIndex));
+
+                    subSum += vl.weights[wi];
+                }
+
+            subSum /= subCount;
+
+            sum += subSum * vl.importance;
+        }
+
+        if (sum > maxActivation || maxIndex == -1) {
+            maxActivation = sum;
+            maxIndex = hc;
+        }
+    }
+
+    hiddenCIs[hiddenColumnIndex] = maxIndex;
+}
+
+void ReconEncoder::learn(
+    const Int2 &columnPos,
+    const IntBuffer* inputCIs,
+    int vli
+) {
+    VisibleLayer &vl = visibleLayers[vli];
+    VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+    int diam = vld.radius * 2 + 1;
+
+    int visibleColumnIndex = address2(columnPos, Int2(vld.size.x, vld.size.y));
+
+    int visibleCellsStart = visibleColumnIndex * vld.size.z;
+
+    int targetCI = (*inputCIs)[visibleColumnIndex];
+
+    // Projection
+    Float2 vToH = Float2(static_cast<float>(hiddenSize.x) / static_cast<float>(vld.size.x),
+        static_cast<float>(hiddenSize.y) / static_cast<float>(vld.size.y));
+
+    Float2 hToV = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hiddenSize.x),
+        static_cast<float>(vld.size.y) / static_cast<float>(hiddenSize.y));
+
+    Int2 reverseRadii(ceilf(vToH.x * (vld.radius * 2 + 1) * 0.5f), ceilf(vToH.y * (vld.radius * 2 + 1) * 0.5f));
+
+    Int2 hiddenCenter = project(columnPos, vToH);
+
+    // Lower corner
+    Int2 fieldLowerBound(hiddenCenter.x - reverseRadii.x, hiddenCenter.y - reverseRadii.y);
+
+    // Bounds of receptive field, clamped to input size
+    Int2 iterLowerBound(max(0, fieldLowerBound.x), max(0, fieldLowerBound.y));
+    Int2 iterUpperBound(min(hiddenSize.x - 1, hiddenCenter.x + reverseRadii.x), min(hiddenSize.y - 1, hiddenCenter.y + reverseRadii.y));
+
+    int maxIndex = -1;
+    float maxActivation = -999999.0f;
+
+    for (int vc = 0; vc < vld.size.z; vc++) {
+        int visibleCellIndex = vc + visibleCellsStart;
+
+        float sum = 0.0f;
+        int count = 0;
+
+        for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
+            for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
+                Int2 hiddenPos = Int2(ix, iy);
+
+                int hiddenColumnIndex = address2(hiddenPos, Int2(hiddenSize.x, hiddenSize.y));
+
+                Int2 visibleCenter = project(hiddenPos, hToV);
+
+                if (inBounds(columnPos, Int2(visibleCenter.x - vld.radius, visibleCenter.y - vld.radius), Int2(visibleCenter.x + vld.radius + 1, visibleCenter.y + vld.radius + 1))) {
+                    Int2 offset(columnPos.x - visibleCenter.x + vld.radius, columnPos.y - visibleCenter.y + vld.radius);
+
+                    int hiddenCellIndexMax = hiddenCIs[hiddenColumnIndex] + hiddenColumnIndex * hiddenSize.z;
+
+                    int wi = vc + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenCellIndexMax));
+
+                    sum += vl.weights[wi];
+                    count++;
+                }
+            }
+
+        sum /= max(1, count);
+
+        vl.reconsTemp[visibleCellIndex] = sum;
+
+        if (sum > maxActivation || maxIndex == -1) {
+            maxActivation = sum;
+            maxIndex = vc;
+        }
+    }
+
+    if (maxIndex != targetCI) {
+        for (int vc = 0; vc < vld.size.z; vc++) {
+            int visibleCellIndex = vc + visibleCellsStart;
+
+            float delta = lr * ((vc == targetCI) - expf(vl.reconsTemp[visibleCellIndex]));
+      
+            for (int ix = iterLowerBound.x; ix <= iterUpperBound.x; ix++)
+                for (int iy = iterLowerBound.y; iy <= iterUpperBound.y; iy++) {
+                    Int2 hiddenPos = Int2(ix, iy);
+
+                    int hiddenColumnIndex = address2(hiddenPos, Int2(hiddenSize.x, hiddenSize.y));
+
+                    Int2 visibleCenter = project(hiddenPos, hToV);
+
+                    if (inBounds(columnPos, Int2(visibleCenter.x - vld.radius, visibleCenter.y - vld.radius), Int2(visibleCenter.x + vld.radius + 1, visibleCenter.y + vld.radius + 1))) {
+                        Int2 offset(columnPos.x - visibleCenter.x + vld.radius, columnPos.y - visibleCenter.y + vld.radius);
+
+                        int hiddenCellIndexMax = hiddenCIs[hiddenColumnIndex] + hiddenColumnIndex * hiddenSize.z;
+
+                        int wi = vc + vld.size.z * (offset.y + diam * (offset.x + diam * hiddenCellIndexMax));
+
+                        vl.weights[wi] += delta;
+                    }
+                }
+        }
+    }
+}
+
+void ReconEncoder::initRandom(
+    const Int3 &hiddenSize,
+    const Array<VisibleLayerDesc> &visibleLayerDescs
+) {
+    this->visibleLayerDescs = visibleLayerDescs;
+
+    this->hiddenSize = hiddenSize;
+
+    visibleLayers.resize(visibleLayerDescs.size());
+
+    // Pre-compute dimensions
+    int numHiddenColumns = hiddenSize.x * hiddenSize.y;
+    int numHiddenCells = numHiddenColumns * hiddenSize.z;
+
+    // Create layers
+    for (int vli = 0; vli < visibleLayers.size(); vli++) {
+        VisibleLayer &vl = visibleLayers[vli];
+        const VisibleLayerDesc &vld = this->visibleLayerDescs[vli];
+
+        int numVisibleColumns = vld.size.x * vld.size.y;
+        int numVisibleCells = numVisibleColumns * vld.size.z;
+
+        int diam = vld.radius * 2 + 1;
+        int area = diam * diam;
+
+        vl.weights.resize(numHiddenCells * area * vld.size.z);
+
+        for (int i = 0; i < vl.weights.size(); i++)
+            vl.weights[i] = randf(-1.0f, 0.0f);
+
+        vl.reconsTemp = FloatBuffer(numVisibleCells, 0.0f);
+    }
+
+    hiddenCIs = IntBuffer(numHiddenColumns, 0);
+}
+
+void ReconEncoder::step(
+    const Array<const IntBuffer*> &inputCIs,
+    bool learnEnabled
+) {
+    int numHiddenColumns = hiddenSize.x * hiddenSize.y;
+    
+    #pragma omp parallel for
+    for (int i = 0; i < numHiddenColumns; i++)
+        forward(Int2(i / hiddenSize.y, i % hiddenSize.y), inputCIs);
+
+    if (learnEnabled) {
+        for (int vli = 0; vli < visibleLayers.size(); vli++) {
+            const VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+            int numVisibleColumns = vld.size.x * vld.size.y;
+        
+            #pragma omp parallel for
+            for (int i = 0; i < numVisibleColumns; i++)
+                learn(Int2(i / vld.size.y, i % vld.size.y), inputCIs[vli], vli);
+        }
+    }
+}
+
+int ReconEncoder::size() const {
+    int size = sizeof(Int3) + sizeof(float) + hiddenCIs.size() * sizeof(int) + sizeof(int);
+
+    for (int vli = 0; vli < visibleLayers.size(); vli++) {
+        const VisibleLayer &vl = visibleLayers[vli];
+
+        size += sizeof(VisibleLayerDesc) + vl.weights.size() * sizeof(float) + sizeof(float);
+    }
+
+    return size;
+}
+
+int ReconEncoder::stateSize() const {
+    return hiddenCIs.size() * sizeof(int);
+}
+
+void ReconEncoder::write(
+    StreamWriter &writer
+) const {
+    writer.write(reinterpret_cast<const void*>(&hiddenSize), sizeof(Int3));
+
+    writer.write(reinterpret_cast<const void*>(&lr), sizeof(float));
+
+    writer.write(reinterpret_cast<const void*>(&hiddenCIs[0]), hiddenCIs.size() * sizeof(int));
+
+    int numVisibleLayers = visibleLayers.size();
+
+    writer.write(reinterpret_cast<const void*>(&numVisibleLayers), sizeof(int));
+    
+    for (int vli = 0; vli < visibleLayers.size(); vli++) {
+        const VisibleLayer &vl = visibleLayers[vli];
+        const VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+        writer.write(reinterpret_cast<const void*>(&vld), sizeof(VisibleLayerDesc));
+
+        writer.write(reinterpret_cast<const void*>(&vl.weights[0]), vl.weights.size() * sizeof(float));
+
+        writer.write(reinterpret_cast<const void*>(&vl.importance), sizeof(float));
+    }
+}
+
+void ReconEncoder::read(
+    StreamReader &reader
+) {
+    reader.read(reinterpret_cast<void*>(&hiddenSize), sizeof(Int3));
+
+    int numHiddenColumns = hiddenSize.x * hiddenSize.y;
+    int numHiddenCells = numHiddenColumns * hiddenSize.z;
+
+    reader.read(reinterpret_cast<void*>(&lr), sizeof(float));
+
+    hiddenCIs.resize(numHiddenColumns);
+
+    reader.read(reinterpret_cast<void*>(&hiddenCIs[0]), hiddenCIs.size() * sizeof(int));
+
+    int numVisibleLayers = visibleLayers.size();
+
+    reader.read(reinterpret_cast<void*>(&numVisibleLayers), sizeof(int));
+
+    visibleLayers.resize(numVisibleLayers);
+    visibleLayerDescs.resize(numVisibleLayers);
+    
+    for (int vli = 0; vli < visibleLayers.size(); vli++) {
+        VisibleLayer &vl = visibleLayers[vli];
+        VisibleLayerDesc &vld = visibleLayerDescs[vli];
+
+        reader.read(reinterpret_cast<void*>(&vld), sizeof(VisibleLayerDesc));
+
+        int numVisibleColumns = vld.size.x * vld.size.y;
+        int numVisibleCells = numVisibleColumns * vld.size.z;
+
+        int diam = vld.radius * 2 + 1;
+        int area = diam * diam;
+
+        vl.weights.resize(numHiddenCells * area * vld.size.z);
+
+        reader.read(reinterpret_cast<void*>(&vl.weights[0]), vl.weights.size() * sizeof(float));
+
+        vl.reconsTemp = FloatBuffer(numVisibleCells, 0.0f);
+
+        reader.read(reinterpret_cast<void*>(&vl.importance), sizeof(float));
+    }
+}
+
+void ReconEncoder::writeState(
+    StreamWriter &writer
+) const {
+    writer.write(reinterpret_cast<const void*>(&hiddenCIs[0]), hiddenCIs.size() * sizeof(int));
+}
+
+void ReconEncoder::readState(
+    StreamReader &reader
+) {
+    reader.read(reinterpret_cast<void*>(&hiddenCIs[0]), hiddenCIs.size() * sizeof(int));
+}
---
+++ source/aogmaneo/Encoder.h
@@ -0,0 +1,144 @@
+// ----------------------------------------------------------------------------
+//  AOgmaNeo
+//  Copyright(c) 2020-2022 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of AOgmaNeo is licensed to you under the terms described
+//  in the AOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include "Helpers.h"
+
+namespace aon {
+// Sparse coder
+class ReconEncoder {
+public:
+    // Visible layer descriptor
+    struct VisibleLayerDesc {
+        Int3 size; // Size of input
+
+        int radius; // Radius onto input
+
+        // Defaults
+        VisibleLayerDesc()
+        :
+        size(4, 4, 16),
+        radius(2)
+        {}
+    };
+
+    // Visible layer
+    struct VisibleLayer {
+        FloatBuffer weights;
+
+        FloatBuffer reconsTemp;
+
+        float importance;
+
+        VisibleLayer()
+        :
+        importance(1.0f)
+        {}
+    };
+
+private:
+    Int3 hiddenSize; // Size of hidden/output layer
+
+    IntBuffer hiddenCIs;
+
+    // Visible layers and associated descriptors
+    Array<VisibleLayer> visibleLayers;
+    Array<VisibleLayerDesc> visibleLayerDescs;
+    
+    // --- Kernels ---
+    
+    void forward(
+        const Int2 &columnPos,
+        const Array<const IntBuffer*> &inputCIs
+    );
+
+    void learn(
+        const Int2 &columnPos,
+        const IntBuffer* inputCIs,
+        int vli
+    );
+
+public:
+    float lr;
+
+    ReconEncoder()
+    :
+    lr(0.1f)
+    {}
+
+    // Create a sparse coding layer with random initialization
+    void initRandom(
+        const Int3 &hiddenSize, // Hidden/output size
+        const Array<VisibleLayerDesc> &visibleLayerDescs // Descriptors for visible layers
+    );
+
+    void step(
+        const Array<const IntBuffer*> &inputCIs, // Input states
+        bool learnEnabled // Whether to learn
+    );
+
+    // Serialization
+    int size() const; // Returns size in bytes
+    int stateSize() const; // Returns size of state in bytes
+
+    void write(
+        StreamWriter &writer
+    ) const;
+
+    void read(
+        StreamReader &reader
+    );
+
+    void writeState(
+        StreamWriter &writer
+    ) const;
+
+    void readState(
+        StreamReader &reader
+    );
+
+    // Get the number of visible layers
+    int getNumVisibleLayers() const {
+        return visibleLayers.size();
+    }
+
+    // Get a visible layer
+    VisibleLayer &getVisibleLayer(
+        int vli // Index of visible layer
+    ) {
+        return visibleLayers[vli];
+    }
+
+    // Get a visible layer
+    const VisibleLayer &getVisibleLayer(
+        int vli // Index of visible layer
+    ) const {
+        return visibleLayers[vli];
+    }
+
+    // Get a visible layer descriptor
+    const VisibleLayerDesc &getVisibleLayerDesc(
+        int vli // Index of visible layer
+    ) const {
+        return visibleLayerDescs[vli];
+    }
+
+    // Get the hidden states
+    const IntBuffer &getHiddenCIs() const {
+        return hiddenCIs;
+    }
+
+    // Get the hidden size
+    const Int3 &getHiddenSize() const {
+        return hiddenSize;
+    }
+};
+} // namespace aon
+
+
