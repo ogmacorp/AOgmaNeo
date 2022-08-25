a2a039c1a42607015b6392cc74f96d487f44f2b7
Eric Laukien <ericlaukien@gmail.com>
2022-08-25 19:41:41 -0400

    Tuning

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -328,7 +328,7 @@ void Decoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(0.0f, 0.01f);
+            vl.weights[i] = randf(-0.01f, 0.01f);
 
         vl.rates = FloatBuffer(numHiddenColumns * area * vld.size.z, 1.0f);
 
