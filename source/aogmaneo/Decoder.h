6af804bd84d108c5ae6b572a27ee686c5a243a3c
Eric Laukien <ericlaukien@gmail.com>
2022-08-26 22:13:21 -0400

    Tests

--- source/aogmaneo/Decoder.h
+++ source/aogmaneo/Decoder.h
@@ -31,11 +31,8 @@ public:
     // Visible layer
     struct VisibleLayer {
         FloatBuffer weights;
-        FloatBuffer rates;
 
         IntBuffer inputCIsPrev; // Previous timestep (prev) input states
-
-        FloatBuffer gates;
     };
 
 private:
@@ -56,12 +53,6 @@ private:
         const Array<const IntBuffer*> &inputCIs
     );
 
-    void backward(
-        const Int2 &columnPos,
-        const IntBuffer* hiddenTargetCIs,
-        int vli
-    );
-
     void learn(
         const Int2 &columnPos,
         const IntBuffer* hiddenTargetCIs
@@ -69,13 +60,11 @@ private:
 
 public:
     float lr; // Learning rate
-    float decay;
 
     // Defaults
     Decoder()
     :
-    lr(1.0f),
-    decay(0.001f)
+    lr(1.0f)
     {}
 
     // Create with random initialization
