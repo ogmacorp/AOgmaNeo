26c6ec92797711715edfeebfc47876f543d4c6a0
Eric Laukien <ericlaukien@gmail.com>
2022-09-22 17:03:14 -0400

    Experiments 2

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -205,7 +205,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = 255 - rand() % 5;
+            vl.weights[i] = 255 - rand() % 128;
 
         vl.reconsTemp = FloatBuffer(numVisibleCells);
     }
