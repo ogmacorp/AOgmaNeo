6df33ed6d9ece9fd6906d7c1b75e900dbc6ace4b
Eric Laukien <ericlaukien@gmail.com>
2022-09-25 19:07:44 -0400

    No forget

--- source/aogmaneo/Decoder.h
+++ source/aogmaneo/Decoder.h
@@ -30,7 +30,7 @@ public:
 
     // Visible layer
     struct VisibleLayer {
-        SByteBuffer weights;
+        ByteBuffer weights;
 
         IntBuffer inputCIsPrev; // Previous timestep (prev) input states
     };
