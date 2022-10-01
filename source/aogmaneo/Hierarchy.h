5fba4c6cedc4334ce8190192e1ee284794af1a58
Eric Laukien <ericlaukien@gmail.com>
2022-09-30 22:33:50 -0400

    Tests

--- source/aogmaneo/Hierarchy.h
+++ source/aogmaneo/Hierarchy.h
@@ -32,21 +32,12 @@ public:
 
         int historyCapacity;
 
-        IODesc()
-        :
-        size(4, 4, 16),
-        type(prediction),
-        eRadius(2),
-        dRadius(2),
-        historyCapacity(64)
-        {}
-
         IODesc(
-            const Int3 &size,
-            IOType type,
-            int eRadius,
-            int dRadius,
-            int historyCapacity
+            const Int3 &size = Int3(4, 4, 16),
+            IOType type = prediction,
+            int eRadius = 2,
+            int dRadius = 2,
+            int historyCapacity = 64
         )
         :
         size(size),
@@ -67,21 +58,12 @@ public:
         int ticksPerUpdate; // Number of ticks a layer takes to update (relative to previous layer)
         int temporalHorizon; // Temporal distance into the past addressed by the layer. Should be greater than or equal to ticksPerUpdate
 
-        LayerDesc()
-        :
-        hiddenSize(4, 4, 16),
-        eRadius(2),
-        dRadius(2),
-        ticksPerUpdate(2),
-        temporalHorizon(4)
-        {}
-
         LayerDesc(
-            const Int3 &hiddenSize,
-            int eRadius,
-            int dRadius,
-            int ticksPerUpdate,
-            int temporalHorizon
+            const Int3 &hiddenSize = Int3(4, 4, 16),
+            int eRadius = 2,
+            int dRadius = 2,
+            int ticksPerUpdate = 2,
+            int temporalHorizon = 4
         )
         :
         hiddenSize(hiddenSize),
