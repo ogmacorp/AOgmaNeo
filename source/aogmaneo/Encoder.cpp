9af43d64dd0229b1264a3d365a6f7ff8b8e7927a
Eric Laukien <ericlaukien@gmail.com>
2023-01-14 10:35:25 -0500

    Count enc

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -185,7 +185,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = rand() % 3 - 1;
+            vl.weights[i] = rand() % 7 - 3;
     }
 
     hiddenCIs = IntBuffer(numHiddenColumns, 0);
