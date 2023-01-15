74d6d69d014f8b99bea36aaa38b1640d2be422b6
Eric Laukien <ericlaukien@gmail.com>
2023-01-15 16:13:44 -0500

    Experiments

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -195,7 +195,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(0.9999f, 1.0f);
+            vl.weights[i] = randf(0.99f, 1.0f);
     }
 
     hiddenMaxActs = FloatBuffer(numHiddenColumns, 0.0f);
