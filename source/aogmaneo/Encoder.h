ba38cb0d5256fa4bfb881cc2c80f17ccb9918b2c
Eric Laukien <ericlaukien@gmail.com>
2023-01-16 21:14:00 -0500

    Tests

--- source/aogmaneo/Encoder.h
+++ source/aogmaneo/Encoder.h
@@ -80,10 +80,10 @@ public:
 
     Encoder()
     :
-    gap(0.0001f),
+    gap(0.01f),
     vigilance(0.9f),
     lr(0.5f),
-    lRadius(1)
+    lRadius(2)
     {}
 
     // Create a sparse coding layer with random initialization
