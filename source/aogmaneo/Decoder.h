fc0d4c3d64257dd3934e9a98549e222d5d0966fe
Eric Laukien <ericlaukien@gmail.com>
2023-02-07 14:27:13 -0500

    Tuning

--- source/aogmaneo/Decoder.h
+++ source/aogmaneo/Decoder.h
@@ -66,9 +66,9 @@ public:
     // Defaults
     Decoder()
     :
-    scale(4.0f),
+    scale(2.0f),
     lr(0.2f),
-    stability(32.0f)
+    stability(16.0f)
     {}
 
     // Create with random initialization
