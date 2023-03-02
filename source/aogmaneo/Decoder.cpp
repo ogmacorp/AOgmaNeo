5c420da0e063ff699b61d68aba2d375bac3c8916
Eric Laukien <ericlaukien@gmail.com>
2023-03-02 14:31:40 -0500

    Tests

--- source/aogmaneo/Decoder.cpp
+++ source/aogmaneo/Decoder.cpp
@@ -156,7 +156,7 @@ void Decoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = -(rand() % 5);
+            vl.weights[i] = rand() % 5;
 
         vl.inputCIsPrev = IntBuffer(numVisibleColumns, 0);
     }
