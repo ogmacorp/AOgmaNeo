8ca969cbed016957c9498962e2738d7b5fe159c0
Eric Laukien <ericlaukien@gmail.com>
2023-03-16 20:44:03 -0400

    Tests

--- source/aogmaneo/Encoder.cpp
+++ source/aogmaneo/Encoder.cpp
@@ -201,7 +201,7 @@ void Encoder::initRandom(
         vl.weights.resize(numHiddenCells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = rand() % 5;
+            vl.weights[i] = -(rand() % 5);
 
         vl.reconActs.resize(numVisibleCells);
     }
