28bc9d24ea3b9d110bf229680fc53584458ab3c2
Eric Laukien <ericlaukien@gmail.com>
2023-03-23 23:14:33 -0400

    Tuning

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -49,8 +49,8 @@ public:
         Params()
         :
         choice(0.0001f),
-        vigilance(0.8f),
-        lr(0.2f),
+        vigilance(0.9f),
+        lr(0.5f),
         l_radius(2)
         {}
     };
