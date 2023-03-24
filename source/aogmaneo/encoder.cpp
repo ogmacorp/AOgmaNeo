af427838dfd49c71381ad0e0e422de6fa561b524
Eric Laukien <ericlaukien@gmail.com>
2023-03-23 23:31:25 -0400

    Tuning

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -151,7 +151,7 @@ void Encoder::learn(
     for (int vc = 0; vc < vld.size.z; vc++) {
         int visible_cell_index = vc + visible_cells_start;
 
-        int delta = roundf(params.lr * 127.0f * ((vc == target_ci) - expf(min(0.0f, vl.recon_acts[visible_cell_index] * params.scale))));
+        int delta = roundf(params.lr * 127.0f * ((vc == target_ci) - sigmoidf(vl.recon_acts[visible_cell_index] * params.scale)));
 
         for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
             for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
@@ -202,7 +202,7 @@ void Encoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = -(rand() % 5);
+            vl.weights[i] = rand() % 64;
 
         vl.recon_acts.resize(num_visible_cells);
     }
