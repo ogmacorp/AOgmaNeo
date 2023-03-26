759418ed412d54df5c62345f89976b401e8f8365
Eric Laukien <ericlaukien@gmail.com>
2023-03-25 22:47:36 -0400

    Experiments

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -105,10 +105,10 @@ void Decoder::learn(
 
     int target_ci = (*hidden_target_cis)[hidden_column_index];
 
-    if (hidden_cis[hidden_column_index] == target_ci) {
-        int hidden_cell_index_target = target_ci + hidden_cells_start;
+    for (int hc = 0; hc < hidden_size.z; hc++) {
+        int hidden_cell_index = hc + hidden_cells_start;
 
-        int delta = roundf(params.lr * 127.0f * (1.0f - hidden_acts[hidden_cell_index_target]));
+        int delta = roundf(params.lr * 127.0f * ((hc == target_ci) - hidden_acts[hidden_cell_index]));
 
         for (int vli = 0; vli < visible_layers.size(); vli++) {
             Visible_Layer &vl = visible_layers[vli];
@@ -137,50 +137,10 @@ void Decoder::learn(
 
                     Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
 
-                    int wi = in_ciprev + vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index_target));
+                    int wi = in_ciprev + vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index));
 
-                    vl.weights[wi] = min(127, vl.weights[wi] + delta);
+                    vl.weights[wi] = min(127, max(-127, vl.weights[wi] + delta));
                 }
-        }
-    }
-    else {
-        for (int hc = 0; hc < hidden_size.z; hc++) {
-            int hidden_cell_index = hc + hidden_cells_start;
-
-            int delta = roundf(params.lr * 127.0f * ((hc == target_ci) - hidden_acts[hidden_cell_index]));
-
-            for (int vli = 0; vli < visible_layers.size(); vli++) {
-                Visible_Layer &vl = visible_layers[vli];
-                const Visible_Layer_Desc &vld = visible_layer_descs[vli];
-
-                int diam = vld.radius * 2 + 1;
-
-                // projection
-                Float2 h_to_v = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hidden_size.x),
-                    static_cast<float>(vld.size.y) / static_cast<float>(hidden_size.y));
-
-                Int2 visible_center = project(column_pos, h_to_v);
-
-                // lower corner
-                Int2 field_lower_bound(visible_center.x - vld.radius, visible_center.y - vld.radius);
-
-                // bounds of receptive field, clamped to input size
-                Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
-                Int2 iter_upper_bound(min(vld.size.x - 1, visible_center.x + vld.radius), min(vld.size.y - 1, visible_center.y + vld.radius));
-
-                for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
-                    for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
-                        int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
-
-                        int in_ciprev = vl.input_cis_prev[visible_column_index];
-
-                        Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
-
-                        int wi = in_ciprev + vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index));
-
-                        vl.weights[wi] = min(127, max(-127, vl.weights[wi] + delta));
-                    }
-            }
         }
     }
 }
@@ -213,7 +173,7 @@ void Decoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = rand() % 5;
+            vl.weights[i] = rand() % 5 - 2;
 
         vl.input_cis_prev = Int_Buffer(num_visible_columns, 0);
     }
