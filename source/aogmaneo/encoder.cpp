2849811e3bd9b11437d30d6d88c2975afefc4776
Eric Laukien <ericlaukien@gmail.com>
2023-03-28 14:05:13 -0400

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -23,7 +23,7 @@ void Encoder::forward(
     int max_index = -1;
     float max_activation = 0.0f;
 
-    int max_complete_index = -1;
+    int max_complete_index = 0;
     float max_complete_activation = 0.0f;
 
     for (int hc = 0; hc < hidden_commits[hidden_column_index]; hc++) {
@@ -52,7 +52,7 @@ void Encoder::forward(
             Int2 iter_upper_bound(min(vld.size.x - 1, visible_center.x + vld.radius), min(vld.size.y - 1, visible_center.y + vld.radius));
 
             int sub_sum = 0;
-            int sub_count = 0;
+            int sub_count = (iter_upper_bound.x - iter_lower_bound.x + 1) * (iter_upper_bound.y - iter_lower_bound.y + 1);
 
             for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
                 for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
@@ -60,18 +60,14 @@ void Encoder::forward(
 
                     int in_ci = (*input_cis[vli])[visible_column_index];
 
-                    if (in_ci == -1)
-                        continue;
-
                     Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
 
                     int wi = offset.y + diam * (offset.x + diam * hidden_cell_index);
 
                     sub_sum += (vl.weight_indices[wi] == in_ci) * vl.weights[wi];
-                    sub_count++;
                 }
 
-            sum += (sub_sum / 255.0f) / max(1, sub_count) * vl.importance;
+            sum += (sub_sum / 255.0f) / sub_count * vl.importance;
             total_importance += vl.importance;
         }
 
@@ -86,7 +82,7 @@ void Encoder::forward(
             }
         }
 
-        if (activation > max_complete_activation || max_complete_index == -1) {
+        if (activation > max_complete_activation) {
             max_complete_activation = activation;
             max_complete_index = hc;
         }
