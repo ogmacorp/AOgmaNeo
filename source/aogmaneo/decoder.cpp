852188e1084106369266a7eb7ba225d3169bf371
Eric Laukien <ericlaukien@gmail.com>
2023-04-01 21:14:04 -0400

    Tuning

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -153,7 +153,7 @@ void Decoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = rand() % 5 - 2;
+            vl.weights[i] = rand() % 5;
 
         vl.input_cis_prev = Int_Buffer(num_visible_columns, 0);
     }
