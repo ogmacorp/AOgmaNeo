671b1ccdc9cdd83959172caf371b093b4c0543c7
Eric Laukien <ericlaukien@gmail.com>
2023-04-02 12:23:08 -0400

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -152,7 +152,7 @@ void Encoder::learn(
     for (int vc = 0; vc < vld.size.z; vc++) {
         int visible_cell_index = vc + visible_cells_start;
 
-        int delta = roundf(params.lr * 127.0f * ((vc == target_ci) - expf(vl.recon_acts[visible_cell_index])));
+        int delta = roundf(params.lr * 127.0f * ((vc == target_ci) - sigmoidf(vl.recon_acts[visible_cell_index])));
 
         for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
             for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
@@ -203,7 +203,7 @@ void Encoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = rand() % 5;
+            vl.weights[i] = rand() % 128;
 
         vl.recon_acts.resize(num_visible_cells);
     }
