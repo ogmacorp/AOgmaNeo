cc27045e7a0c4e1721433b42ead723f11db33311
Eric Laukien <ericlaukien@gmail.com>
2023-04-04 14:25:52 -0400

    Experiments

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -71,9 +71,9 @@ void Decoder::update_gates(
 
     const float byte_inv = 1.0f / 255.0f;
 
-    float m_inv = 1.0f - m * byte_inv;
+    float mf = m * byte_inv;
 
-    vl.gates[visible_column_index] = powf(m_inv, params.curve);
+    vl.gates[visible_column_index] = 1.0f - powf(mf, params.curve);
 }
 
 void Decoder::forward(
