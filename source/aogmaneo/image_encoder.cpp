c71d5422ceb2774e5fad730c6944deb8fcb631f5
Eric Laukien <ericlaukien@gmail.com>
2023-04-13 22:35:01 -0400

    Tuning

--- source/aogmaneo/image_encoder.cpp
+++ source/aogmaneo/image_encoder.cpp
@@ -135,7 +135,7 @@ void Image_Encoder::forward(
                     }
             }
 
-            hidden_rates[hidden_cell_index] -= min(1.0f, max(0.0f, params.lr * sqrtf(-max_activation))) * rate;
+            hidden_rates[hidden_cell_index] -= (1.0f - expf(-params.lr * sqrtf(-max_activation))) * rate;
         }
     }
 }
