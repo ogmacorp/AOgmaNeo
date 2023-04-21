e6aabd446e59ac7161e1481f28c386bfd61db97b
Eric Laukien <ericlaukien@gmail.com>
2023-04-21 11:53:11 -0400

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -188,7 +188,7 @@ void Encoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(0.0f, 1.0f);
+            vl.weights[i] = randf(0.99f, 1.0f);
     }
 
     hidden_cis = Int_Buffer(num_hidden_columns, 0);
