c679940df95e1a9379ae52e7f3959e94b8f66977
Eric Laukien <ericlaukien@gmail.com>
2023-05-13 20:34:03 -0400

    No early stop

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -257,39 +257,26 @@ void Encoder::learn(
 
         sum /= max(1, count);
 
-        vl.recon_acts[visible_cell_index] = sum;
+        float delta = params.lr * ((vc == target_ci) - expf(sum - 1.0f));
 
-        if (sum > max_activation) {
-            max_activation = sum;
-            max_index = vc;
-        }
-    }
+        for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
+            for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
+                Int2 hidden_pos = Int2(ix, iy);
 
-    if (max_index != target_ci) {
-        for (int vc = 0; vc < vld.size.z; vc++) {
-            int visible_cell_index = vc + visible_cells_start;
+                int hidden_column_index = address2(hidden_pos, Int2(hidden_size.x, hidden_size.y));
 
-            float delta = params.lr * ((vc == target_ci) - expf(vl.recon_acts[visible_cell_index] - 1.0f));
+                Int2 visible_center = project(hidden_pos, h_to_v);
 
-            for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
-                for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
-                    Int2 hidden_pos = Int2(ix, iy);
+                if (in_bounds(column_pos, Int2(visible_center.x - vld.radius, visible_center.y - vld.radius), Int2(visible_center.x + vld.radius + 1, visible_center.y + vld.radius + 1))) {
+                    int hidden_cell_index_max = hidden_cis[hidden_column_index] + hidden_column_index * hidden_size.z;
 
-                    int hidden_column_index = address2(hidden_pos, Int2(hidden_size.x, hidden_size.y));
+                    Int2 offset(column_pos.x - visible_center.x + vld.radius, column_pos.y - visible_center.y + vld.radius);
 
-                    Int2 visible_center = project(hidden_pos, h_to_v);
+                    int wi = vc + vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index_max));
 
-                    if (in_bounds(column_pos, Int2(visible_center.x - vld.radius, visible_center.y - vld.radius), Int2(visible_center.x + vld.radius + 1, visible_center.y + vld.radius + 1))) {
-                        int hidden_cell_index_max = hidden_cis[hidden_column_index] + hidden_column_index * hidden_size.z;
-
-                        Int2 offset(column_pos.x - visible_center.x + vld.radius, column_pos.y - visible_center.y + vld.radius);
-
-                        int wi = vc + vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index_max));
-
-                        vl.weights[wi] += delta * hidden_gates[hidden_column_index];
-                    }
+                    vl.weights[wi] += delta * hidden_gates[hidden_column_index];
                 }
-        }
+            }
     }
 
     for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
@@ -344,7 +331,7 @@ void Encoder::init_random(
 
         vl.usages = Byte_Buffer(vl.weights.size(), 0);
 
-        vl.recon_acts.resize(num_visible_cells);
+        vl.recon_acts.resize(num_visible_columns);
     }
 
     hidden_cis = Int_Buffer(num_hidden_columns, 0);
@@ -485,7 +472,7 @@ void Encoder::read(
         reader.read(reinterpret_cast<void*>(&vl.weights[0]), vl.weights.size() * sizeof(float));
         reader.read(reinterpret_cast<void*>(&vl.usages[0]), vl.usages.size() * sizeof(Byte));
 
-        vl.recon_acts.resize(num_visible_cells);
+        vl.recon_acts.resize(num_visible_columns);
 
         reader.read(reinterpret_cast<void*>(&vl.importance), sizeof(float));
     }
