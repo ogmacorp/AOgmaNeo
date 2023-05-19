b67ade28746e1f4e78d53382c81c9d8ef9e41632
Eric Laukien <ericlaukien@gmail.com>
2023-05-19 12:21:05 -0400

    Tests

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -200,7 +200,7 @@ void Encoder::init_random(
 
     hidden_max_acts.resize(num_hidden_columns);
 
-    hidden_rates = Float_Buffer(num_hidden_cells, 0.5f);
+    hidden_rates = Float_Buffer(num_hidden_cells, 1.0f);
 }
 
 void Encoder::step(
