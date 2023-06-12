39b1c24a17f0330b44b4194f084086378115f8fc
Eric Laukien <ericlaukien@gmail.com>
2023-06-11 21:37:45 -0400

    Softmax dec

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -50,7 +50,7 @@ public:
 
         Params()
         :
-        lr(0.1f),
+        lr(1.0f),
         gcurve(8.0f)
         {}
     };
@@ -59,8 +59,6 @@ private:
     Int3 hidden_size; // size of hidden/output layer
 
     Int_Buffer hidden_cis;
-
-    Float_Buffer hidden_acts;
 
     Float_Buffer hidden_gates;
 
