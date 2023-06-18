5881deaddf2721fe59354fec55ae508313d62797
Eric Laukien <ericlaukien@gmail.com>
2023-06-18 14:38:29 -0400

    Impl

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -49,9 +49,9 @@ public:
 
         Params()
         :
-        choice(0.01f),
-        vigilance(0.9f),
-        lr(0.1f),
+        choice(0.0001f),
+        vigilance(0.95f),
+        lr(0.5f),
         l_radius(2)
         {}
     };
