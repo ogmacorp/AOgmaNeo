64fcc12ae8cc7726af31ff014edda9f6545bdf92
Eric Laukien <ericlaukien@gmail.com>
2023-06-22 10:48:10 -0400

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -55,7 +55,7 @@ void Encoder::forward(
 
                     int wi = in_ci_prev + vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index_prev));
 
-                    vl.weights[wi] += delta;
+                    vl.weights[wi] = min(1.0f, max(0.0f, vl.weights[wi] + delta));
                     vl.usages[wi] = min(255, vl.usages[wi] + 1);
                 }
         }
@@ -206,7 +206,7 @@ void Encoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = randf(-0.01f, 0.01f);
+            vl.weights[i] = randf(0.99f, 1.0f);
 
         vl.usages = Byte_Buffer(vl.weights.size(), 0);
 
