a56b3472373e1b9cee8526d7c7189e8a31232519
Eric Laukien <ericlaukien@gmail.com>
2023-06-28 14:06:03 -0400

    Tests

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -172,7 +172,7 @@ void Decoder::update_gates(
             }
         }
 
-    vl.gates[visible_column_index] = expf(-(sum / 255.0f) / max(1, count * hidden_size.z) * params.gcurve);
+    vl.gates[visible_column_index] = expf(-(sum / 255.0f) / (max(1, count) * hidden_size.z) * params.gcurve);
 }
 
 void Decoder::learn(
