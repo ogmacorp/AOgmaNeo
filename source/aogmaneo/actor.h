a651c68c315c06b709404a261f5895c6c6b6b3bb
Eric Laukien <ericlaukien@gmail.com>
2023-06-29 13:10:15 -0400

    Actor tuning

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -53,8 +53,8 @@ public:
 
         Params()
         :
-        vlr(0.01f),
-        alr(0.01f),
+        vlr(0.02f),
+        alr(0.02f),
         bias(0.5f),
         discount(0.99f),
         temperature(1.0f),
