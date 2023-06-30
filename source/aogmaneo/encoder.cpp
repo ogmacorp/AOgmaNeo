ba6a214bbac958bcd782a0c48681e5259efb5e5a
Eric Laukien <ericlaukien@gmail.com>
2023-06-29 21:35:26 -0400

    Tuning

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -158,6 +158,8 @@ void Encoder::update_gates(
         Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
         Int2 iter_upper_bound(min(vld.size.x - 1, visible_center.x + vld.radius), min(vld.size.y - 1, visible_center.y + vld.radius));
 
+        count += (iter_upper_bound.x - iter_lower_bound.x + 1) * (iter_upper_bound.y - iter_lower_bound.y + 1);
+
         for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
             for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
                 int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
@@ -170,12 +172,11 @@ void Encoder::update_gates(
                     int wi = vc + wi_start;
 
                     sum += vl.usages[wi];
-                    count++;
                 }
             }
     }
 
-    hidden_gates[hidden_column_index] = expf(-(sum / 255.0f) / max(1, count) * params.gcurve);
+    hidden_gates[hidden_column_index] = expf(-(sum / 255.0f) / count * params.gcurve);
 }
 
 void Encoder::init_random(
