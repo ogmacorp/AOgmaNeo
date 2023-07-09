01adecd82e4692a871f872ee18dc3ef000db06ae
Eric Laukien <ericlaukien@gmail.com>
2023-07-09 13:22:18 -0400

    Cleanup

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -117,7 +117,7 @@ void Encoder::forward(
 
     learn_cis[hidden_column_index] = max_index;
 
-    hidden_maxs[hidden_column_index] = max_match;
+    hidden_maxs[hidden_column_index] = max_activation;
 
     hidden_cis[hidden_column_index] = max_complete_index;
 }
