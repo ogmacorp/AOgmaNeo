b211054296a6176a9f91a8ba64562287f29da53a
Eric Laukien <ericlaukien@gmail.com>
2023-07-28 14:56:39 -0400

    Usage tracking

--- source/aogmaneo/decoder.h
+++ source/aogmaneo/decoder.h
@@ -32,17 +32,23 @@ public:
     struct Visible_Layer {
         Byte_Buffer weights;
 
+        Byte_Buffer usages;
+
+        Float_Buffer gates;
+
         Int_Buffer input_cis_prev; // previous timestep (prev) input states
     };
 
     struct Params {
         float temperature; // temperature of softmax, MUST be > 0
         float lr; // learning rate
+        float gcurve; // gain curve for anti-forget
 
         Params()
         :
-        temperature(0.1f),
-        lr(16.0f)
+        temperature(0.01f),
+        lr(4.0f),
+        gcurve(8.0f)
         {}
     };
 
@@ -59,11 +65,19 @@ private:
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
 
+    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
+
     // --- kernels ---
 
     void forward(
         const Int2 &column_pos,
         const Array<const Int_Buffer*> &input_cis,
+        const Params &params
+    );
+
+    void update_gates(
+        const Int2 &column_pos,
+        int vli,
         const Params &params
     );
 
