dd6a7c22845b24777be9076eac095c654afcbb42
Eric Laukien <ericlaukien@gmail.com>
2023-08-12 16:09:03 -0400

    Tests 4

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -125,6 +125,9 @@ void Decoder::learn(
 
     int delta = rand_roundf(params.lr * (1.0f - hidden_acts[hidden_cell_index_target]), state);
 
+    if (delta == 0)
+        return;
+
     for (int vli = 0; vli < visible_layers.size(); vli++) {
         Visible_Layer &vl = visible_layers[vli];
         const Visible_Layer_Desc &vld = visible_layer_descs[vli];
