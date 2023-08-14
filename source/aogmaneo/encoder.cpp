2a73a0d1ee2d64aa0526e930516041a485888258
Eric Laukien <ericlaukien@gmail.com>
2023-08-14 13:22:55 -0400

    Experiments 4

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -162,10 +162,18 @@ void Encoder::learn(
             }
         }
 
+    int max_index = 0;
+    float max_activation = 0.0f;
+
     for (int vc = 0; vc < vld.size.z; vc++) {
         int visible_cell_index = vc + visible_cells_start;
 
         vl.recon_acts[visible_cell_index] /= max(1, count) * 255;
+
+        if (vl.recon_acts[visible_cell_index] > max_activation) {
+            max_activation = vl.recon_acts[visible_cell_index];
+            max_index = vc;
+        }
 
         vl.recon_acts[visible_cell_index] = expf((vl.recon_acts[visible_cell_index] - 1.0f) * params.scale);
 
