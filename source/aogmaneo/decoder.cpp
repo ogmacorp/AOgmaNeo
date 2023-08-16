4fa3d1770fd2004460db901ef34702dc8a30e2c4
Eric Laukien <ericlaukien@gmail.com>
2023-08-15 22:09:03 -0400

    Tests

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -27,6 +27,7 @@ void Decoder::forward(
     }
 
     int count = 0;
+    Byte min_weight = 255;
 
     for (int vli = 0; vli < visible_layers.size(); vli++) {
         Visible_Layer &vl = visible_layers[vli];
@@ -61,8 +62,6 @@ void Decoder::forward(
 
                 int wi_start = hidden_size.z * (offset.y + diam * (offset.x + diam * (in_ci + vld.size.z * hidden_column_index)));
 
-                Byte min_weight = 255;
-
                 for (int hc = 0; hc < hidden_size.z; hc++) {
                     int hidden_cell_index = hc + hidden_cells_start;
 
@@ -70,14 +69,6 @@ void Decoder::forward(
 
                     hidden_sums[hidden_cell_index] += vl.weights[wi];
                     min_weight = min(min_weight, vl.weights[wi]);
-                }
-
-                if (learn_enabled) {
-                    for (int hc = 0; hc < hidden_size.z; hc++) {
-                        int wi = hc + wi_start;
-
-                        vl.weights[wi] -= min_weight;
-                    }
                 }
             }
     }
@@ -115,6 +106,47 @@ void Decoder::forward(
     }
 
     hidden_cis[hidden_column_index] = max_index;
+
+    if (learn_enabled) {
+        for (int vli = 0; vli < visible_layers.size(); vli++) {
+            Visible_Layer &vl = visible_layers[vli];
+            const Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+            int diam = vld.radius * 2 + 1;
+
+            // projection
+            Float2 h_to_v = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hidden_size.x),
+                static_cast<float>(vld.size.y) / static_cast<float>(hidden_size.y));
+
+            Int2 visible_center = project(column_pos, h_to_v);
+
+            // lower corner
+            Int2 field_lower_bound(visible_center.x - vld.radius, visible_center.y - vld.radius);
+
+            // bounds of receptive field, clamped to input size
+            Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
+            Int2 iter_upper_bound(min(vld.size.x - 1, visible_center.x + vld.radius), min(vld.size.y - 1, visible_center.y + vld.radius));
+
+            const Int_Buffer &vl_input_cis = *input_cis[vli];
+
+            for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
+                for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
+                    int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
+
+                    int in_ci = vl_input_cis[visible_column_index];
+
+                    Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
+
+                    int wi_start = hidden_size.z * (offset.y + diam * (offset.x + diam * (in_ci + vld.size.z * hidden_column_index)));
+
+                    for (int hc = 0; hc < hidden_size.z; hc++) {
+                        int wi = hc + wi_start;
+
+                        vl.weights[wi] -= min_weight;
+                    }
+                }
+        }
+    }
 }
 
 void Decoder::learn(
