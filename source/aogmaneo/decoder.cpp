20b70754c2d12f980a3ac97f488fe41d6352e7ec
Eric Laukien <ericlaukien@gmail.com>
2023-08-17 13:20:20 -0400

    Tests

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -110,6 +110,7 @@ void Decoder::forward(
 void Decoder::learn(
     const Int2 &column_pos,
     const Int_Buffer* hidden_target_cis,
+    unsigned long* state,
     const Params &params
 ) {
     int hidden_column_index = address2(column_pos, Int2(hidden_size.x, hidden_size.y));
@@ -168,7 +169,7 @@ void Decoder::learn(
 
                     int wi = hc + wi_start;
 
-                    vl.weights[wi] = min(255, max(0, vl.weights[wi] + roundf(hidden_deltas[hidden_cell_index] * rate)));
+                    vl.weights[wi] = min(255, max(0, vl.weights[wi] + rand_roundf(hidden_deltas[hidden_cell_index] * rate, state)));
                 }
             }
     }
@@ -271,9 +272,14 @@ void Decoder::step(
     int num_hidden_columns = hidden_size.x * hidden_size.y;
 
     if (learn_enabled) {
+        unsigned int base_state = rand();
+
         PARALLEL_FOR
-        for (int i = 0; i < num_hidden_columns; i++)
-            learn(Int2(i / hidden_size.y, i % hidden_size.y), hidden_target_cis, params);
+        for (int i = 0; i < num_hidden_columns; i++) {
+            unsigned long state = rand_get_state(base_state + i * rand_subseed_offset);
+
+            learn(Int2(i / hidden_size.y, i % hidden_size.y), hidden_target_cis, &state, params);
+        }
 
         PARALLEL_FOR
         for (int i = 0; i < visible_pos_vlis.size(); i++) {
