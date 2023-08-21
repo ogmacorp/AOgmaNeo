50d25450ee1616316968acd4d52e623e9f670739
Eric Laukien <ericlaukien@gmail.com>
2023-08-21 16:51:25 -0400

    Tuning

--- source/aogmaneo/hierarchy.h
+++ source/aogmaneo/hierarchy.h
@@ -63,7 +63,7 @@ public:
             int up_radius = 2,
             int down_radius = 2,
             int ticks_per_update = 2,
-            int temporal_horizon = 4
+            int temporal_horizon = 2
         )
         :
         hidden_size(hidden_size),
