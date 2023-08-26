6ebfac196ca0a65bec8caa284d85cf151b23e7bd
Eric Laukien <ericlaukien@gmail.com>
2023-08-26 18:29:52 -0400

    Experiments 2

--- source/aogmaneo/decoder.h
+++ source/aogmaneo/decoder.h
@@ -30,23 +30,17 @@ public:
 
     // visible layer
     struct Visible_Layer {
-        Byte_Buffer weights;
+        Float_Buffer weights;
 
         Int_Buffer input_cis_prev; // previous timestep (prev) input states
-
-        Float_Buffer gates;
     };
 
     struct Params {
-        float scale; // scale of softmax
         float lr; // learning rate
-        float gcurve; // gate curve
 
         Params()
         :
-        scale(32.0f),
-        lr(0.05f),
-        gcurve(16.0f)
+        lr(0.01f)
         {}
     };
 
@@ -55,28 +49,18 @@ private:
 
     Int_Buffer hidden_cis; // hidden state
 
-    Int_Buffer hidden_sums;
     Float_Buffer hidden_acts;
-
-    Float_Buffer hidden_deltas;
 
     // visible layers and descs
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
-
-    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
 
     // --- kernels ---
 
     void forward(
         const Int2 &column_pos,
         const Array<const Int_Buffer*> &input_cis,
-        const Params &params
-    );
-
-    void update_gates(
-        const Int2 &column_pos,
-        int vli,
+        bool learn_enabled,
         const Params &params
     );
 
