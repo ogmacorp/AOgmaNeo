fea46ce2acdaf6e5f61b36cd66d1fd25cd98f12d
Eric Laukien <ericlaukien@gmail.com>
2023-10-02 23:52:55 -0400

    Experiments

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -49,8 +49,8 @@ public:
         Params()
         :
         choice(0.0001f),
-        vigilance(0.95f),
-        lr(0.1f),
+        vigilance(0.9f),
+        lr(0.5f),
         l_radius(2)
         {}
     };
