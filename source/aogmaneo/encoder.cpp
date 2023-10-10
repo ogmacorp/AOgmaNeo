6b89743a3083fc7174d189b1e0c990e1e7dffacd
Eric Laukien <ericlaukien@gmail.com>
2023-10-10 13:09:25 -0400

    max_activation

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -13,7 +13,6 @@ using namespace aon;
 void Encoder::forward(
     const Int2 &column_pos,
     const Array<const Int_Buffer*> &input_cis,
-    unsigned long* state,
     const Params &params
 ) {
     int hidden_column_index = address2(column_pos, Int2(hidden_size.x, hidden_size.y));
@@ -83,7 +82,6 @@ void Encoder::forward(
 
     int max_index = -1;
     float max_activation = 0.0f;
-    float max_match = 0.0f;
 
     int max_complete_index = 0;
     float max_complete_activation = 0.0f;
@@ -101,7 +99,6 @@ void Encoder::forward(
         if (match >= params.vigilance) {
             if (activation > max_activation) {
                 max_activation = activation;
-                max_match = match;
                 max_index = hc;
             }
         }
@@ -114,7 +111,7 @@ void Encoder::forward(
 
     learn_cis[hidden_column_index] = max_index;
 
-    hidden_maxs[hidden_column_index] = max_match + randf(state) * noise_small;
+    hidden_maxs[hidden_column_index] = max_activation;
 
     hidden_cis[hidden_column_index] = (max_index == -1 ? max_complete_index : max_index);
 }
@@ -197,8 +194,7 @@ void Encoder::learn(
                 }
         }
 
-        if (dhc == 0)
-            hidden_commits[hidden_cell_index] = true;
+        hidden_commits[hidden_cell_index] = true;
     }
 }
 
@@ -253,14 +249,9 @@ void Encoder::step(
 ) {
     int num_hidden_columns = hidden_size.x * hidden_size.y;
     
-    unsigned int base_state = rand();
-
     PARALLEL_FOR
-    for (int i = 0; i < num_hidden_columns; i++) {
-        unsigned long state = rand_get_state(base_state + i * rand_subseed_offset);
-
-        forward(Int2(i / hidden_size.y, i % hidden_size.y), input_cis, &state, params);
-    }
+    for (int i = 0; i < num_hidden_columns; i++)
+        forward(Int2(i / hidden_size.y, i % hidden_size.y), input_cis, params);
 
     if (learn_enabled) {
         PARALLEL_FOR
