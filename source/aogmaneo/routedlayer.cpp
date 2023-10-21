c5a3f2bbfaa7f2522f48c1003c47f416812532a0
Eric Laukien <ericlaukien@gmail.com>
2023-10-21 12:20:53 -0400

    Impl

---
+++ source/aogmaneo/routedlayer.cpp
@@ -0,0 +1,352 @@
+// ----------------------------------------------------------------------------
+//  AOgmaNeo
+//  Copyright(c) 2020-2023 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of AOgmaNeo is licensed to you under the terms described
+//  in the AOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#include "routedlayer.h"
+
+using namespace aon;
+
+void Routed_Layer::forward(
+    const Int2 &column_pos,
+    const Array<Int_Buffer_View> &input_cis,
+    const Array<Float_Buffer_View> &input_acts,
+    Int_Buffer_View route_cis,
+    const Params &params
+) {
+    int hidden_column_index = address2(column_pos, Int2(hidden_size.x, hidden_size.y));
+
+    int hidden_cells_start = hidden_column_index * hidden_size.z;
+
+    int route_ci = route_cis[hidden_column_index];
+
+    int hidden_cell_index_route = route_ci + hidden_cells_start;
+
+    float activation = 0.0f;
+    int count = 0;
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+        int diam = vld.radius * 2 + 1;
+
+        // projection
+        Float2 h_to_v = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hidden_size.x),
+            static_cast<float>(vld.size.y) / static_cast<float>(hidden_size.y));
+
+        Int2 visible_center = project(column_pos, h_to_v);
+
+        // lower corner
+        Int2 field_lower_bound(visible_center.x - vld.radius, visible_center.y - vld.radius);
+
+        // bounds of receptive field, clamped to input size
+        Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
+        Int2 iter_upper_bound(min(vld.size.x - 1, visible_center.x + vld.radius), min(vld.size.y - 1, visible_center.y + vld.radius));
+
+        count += (iter_upper_bound.x - iter_lower_bound.x + 1) * (iter_upper_bound.y - iter_lower_bound.y + 1);
+
+        Int_Buffer_View vl_input_cis = input_cis[vli];
+        Float_Buffer_View vl_input_acts = input_acts[vli];
+
+        for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
+            for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
+                int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
+
+                int in_ci = vl_input_cis[visible_column_index];
+                float in_act = vl_input_acts[visible_column_index];
+
+                Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
+
+                int wi = route_ci + hidden_size.z * (offset.y + diam * (offset.x + diam * (in_ci + vld.size.z * hidden_column_index)));
+
+                activation += vl.weights[wi] * in_act;
+            }
+    }
+
+    activation /= (count * 127);
+
+    hidden_acts[hidden_column_index] = activation * params.scale;
+}
+
+void Routed_Layer::backward(
+    const Int2 &column_pos,
+    Int_Buffer_View input_cis,
+    Float_Buffer_View input_acts,
+    Int_Buffer_View route_cis,
+    Float_Buffer_View errors,
+    int vli,
+    bool learn_enabled,
+    unsigned long* state,
+    const Params &params
+) {
+    Visible_Layer &vl = visible_layers[vli];
+    Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+    int diam = vld.radius * 2 + 1;
+
+    int visible_column_index = address2(column_pos, Int2(vld.size.x, vld.size.y));
+
+    int visible_cells_start = visible_column_index * vld.size.z;
+
+    // projection
+    Float2 v_to_h = Float2(static_cast<float>(hidden_size.x) / static_cast<float>(vld.size.x),
+        static_cast<float>(hidden_size.y) / static_cast<float>(vld.size.y));
+
+    Float2 h_to_v = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hidden_size.x),
+        static_cast<float>(vld.size.y) / static_cast<float>(hidden_size.y));
+                
+    Int2 reverse_radii(ceilf(v_to_h.x * (vld.radius * 2 + 1) * 0.5f), ceilf(v_to_h.y * (vld.radius * 2 + 1) * 0.5f));
+
+    Int2 hidden_center = project(column_pos, v_to_h);
+
+    // lower corner
+    Int2 field_lower_bound(hidden_center.x - reverse_radii.x, hidden_center.y - reverse_radii.y);
+
+    // bounds of receptive field, clamped to input size
+    Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
+    Int2 iter_upper_bound(min(hidden_size.x - 1, hidden_center.x + reverse_radii.x), min(hidden_size.y - 1, hidden_center.y + reverse_radii.y));
+
+    int hidden_stride = vld.size.z * diam * diam;
+    
+    int in_ci = input_cis[visible_column_index];
+    float in_act = input_acts[visible_column_index];
+
+    float sum = 0.0f;
+    int count = 0;
+
+    for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
+        for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
+            Int2 hidden_pos = Int2(ix, iy);
+
+            int hidden_column_index = address2(hidden_pos, Int2(hidden_size.x, hidden_size.y));
+
+            Int2 visible_center = project(hidden_pos, h_to_v);
+
+            if (in_bounds(column_pos, Int2(visible_center.x - vld.radius, visible_center.y - vld.radius), Int2(visible_center.x + vld.radius + 1, visible_center.y + vld.radius + 1))) {
+                Int2 offset(column_pos.x - visible_center.x + vld.radius, column_pos.y - visible_center.y + vld.radius);
+
+                int route_ci = route_cis[hidden_column_index];
+
+                int wi = route_ci + hidden_size.z * (offset.y + diam * (offset.x + diam * (in_ci + vld.size.z * hidden_column_index)));
+
+                sum += errors[hidden_column_index] * vl.weights[wi];
+                count++;
+
+                if (learn_enabled)
+                    vl.weights[wi] = min(127, max(-127, vl.weights[wi] + rand_roundf(params.lr * 127.0f * errors[hidden_column_index] * in_act, state)));
+            }
+        }
+
+    sum /= max(1, count * 127);
+
+    vl.errors[visible_column_index] = sum * params.scale;
+}
+
+void Routed_Layer::init_random(
+    const Int3 &hidden_size,
+    const Array<Visible_Layer_Desc> &visible_layer_descs
+) {
+    this->visible_layer_descs = visible_layer_descs; 
+
+    this->hidden_size = hidden_size;
+
+    visible_layers.resize(visible_layer_descs.size());
+
+    // pre-compute dimensions
+    int num_hidden_columns = hidden_size.x * hidden_size.y;
+    int num_hidden_cells = num_hidden_columns * hidden_size.z;
+
+    int total_num_visible_columns = 0;
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = this->visible_layer_descs[vli];
+
+        int num_visible_columns = vld.size.x * vld.size.y;
+        int num_visible_cells = num_visible_columns * vld.size.z;
+
+        total_num_visible_columns += num_visible_columns;
+
+        int diam = vld.radius * 2 + 1;
+        int area = diam * diam;
+
+        vl.weights.resize(num_hidden_cells * area * vld.size.z);
+
+        for (int i = 0; i < vl.weights.size(); i++)
+            vl.weights[i] = (rand() % init_weight_noise) - init_weight_noise / 2;
+
+        vl.errors.resize(num_visible_columns);
+    }
+
+    hidden_acts = Float_Buffer(num_hidden_columns, 0.0f);
+
+    // generate helper buffers for parallelization
+    visible_pos_vlis.resize(total_num_visible_columns);
+
+    int index = 0;
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = this->visible_layer_descs[vli];
+
+        int num_visible_columns = vld.size.x * vld.size.y;
+
+        for (int i = 0; i < num_visible_columns; i++) {
+            visible_pos_vlis[index] = Int3(i / vld.size.y, i % vld.size.y, vli);
+            index++;
+        }
+    }
+}
+
+void Routed_Layer::forward(
+    const Array<Int_Buffer_View> &input_cis,
+    const Array<Float_Buffer_View> &input_acts,
+    Int_Buffer_View route_cis,
+    const Params &params
+) {
+    int num_hidden_columns = hidden_size.x * hidden_size.y;
+
+    PARALLEL_FOR
+    for (int i = 0; i < num_hidden_columns; i++)
+        forward(Int2(i / hidden_size.y, i % hidden_size.y), input_cis, input_acts, route_cis, params);
+}
+
+void Routed_Layer::backward(
+    const Array<Int_Buffer_View> &input_cis,
+    const Array<Float_Buffer_View> &input_acts,
+    Int_Buffer_View route_cis,
+    Float_Buffer_View errors,
+    bool learn_enabled,
+    const Params &params
+) {
+    unsigned int base_state = rand();
+
+    PARALLEL_FOR
+    for (int i = 0; i < visible_pos_vlis.size(); i++) {
+        Int2 pos = Int2(visible_pos_vlis[i].x, visible_pos_vlis[i].y);
+        int vli = visible_pos_vlis[i].z;
+
+        unsigned long state = rand_get_state(base_state + i * rand_subseed_offset);
+
+        backward(pos, input_cis[vli], input_acts[vli], route_cis, errors, vli, learn_enabled, &state, params);
+    }
+}
+
+void Routed_Layer::clear_state() {
+    hidden_acts.fill(0.0f);
+}
+
+int Routed_Layer::size() const {
+    int size = sizeof(Int3) + hidden_acts.size() * sizeof(float) + sizeof(int);
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        const Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+        size += sizeof(Visible_Layer_Desc) + vl.weights.size() * sizeof(S_Byte);
+    }
+
+    return size;
+}
+
+int Routed_Layer::state_size() const {
+    return hidden_acts.size() * sizeof(float);
+}
+
+void Routed_Layer::write(
+    Stream_Writer &writer
+) const {
+    writer.write(reinterpret_cast<const void*>(&hidden_size), sizeof(Int3));
+
+    writer.write(reinterpret_cast<const void*>(&hidden_acts[0]), hidden_acts.size() * sizeof(float));
+    
+    int num_visible_layers = visible_layers.size();
+
+    writer.write(reinterpret_cast<const void*>(&num_visible_layers), sizeof(int));
+    
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        const Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+        writer.write(reinterpret_cast<const void*>(&vld), sizeof(Visible_Layer_Desc));
+
+        writer.write(reinterpret_cast<const void*>(&vl.weights[0]), vl.weights.size() * sizeof(S_Byte));
+    }
+}
+
+void Routed_Layer::read(
+    Stream_Reader &reader
+) {
+    reader.read(reinterpret_cast<void*>(&hidden_size), sizeof(Int3));
+
+    int num_hidden_columns = hidden_size.x * hidden_size.y;
+    int num_hidden_cells = num_hidden_columns * hidden_size.z;
+
+    hidden_acts.resize(num_hidden_columns);
+
+    reader.read(reinterpret_cast<void*>(&hidden_acts[0]), hidden_acts.size() * sizeof(float));
+
+    int num_visible_layers;
+
+    reader.read(reinterpret_cast<void*>(&num_visible_layers), sizeof(int));
+
+    visible_layers.resize(num_visible_layers);
+    visible_layer_descs.resize(num_visible_layers);
+
+    int total_num_visible_columns = 0;
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        Visible_Layer &vl = visible_layers[vli];
+        Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+        reader.read(reinterpret_cast<void*>(&vld), sizeof(Visible_Layer_Desc));
+
+        int num_visible_columns = vld.size.x * vld.size.y;
+        int num_visible_cells = num_visible_columns * vld.size.z;
+
+        total_num_visible_columns += num_visible_columns;
+
+        int diam = vld.radius * 2 + 1;
+        int area = diam * diam;
+
+        vl.weights.resize(num_hidden_cells * area * vld.size.z);
+
+        reader.read(reinterpret_cast<void*>(&vl.weights[0]), vl.weights.size() * sizeof(S_Byte));
+
+        vl.errors.resize(num_visible_columns);
+    }
+
+    // generate helper buffers for parallelization
+    visible_pos_vlis.resize(total_num_visible_columns);
+
+    int index = 0;
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = this->visible_layer_descs[vli];
+
+        int num_visible_columns = vld.size.x * vld.size.y;
+
+        for (int i = 0; i < num_visible_columns; i++) {
+            visible_pos_vlis[index] = Int3(i / vld.size.y, i % vld.size.y, vli);
+            index++;
+        }
+    }
+}
+
+void Routed_Layer::write_state(
+    Stream_Writer &writer
+) const {
+    writer.write(reinterpret_cast<const void*>(&hidden_acts[0]), hidden_acts.size() * sizeof(float));
+}
+
+void Routed_Layer::read_state(
+    Stream_Reader &reader
+) {
+    reader.read(reinterpret_cast<void*>(&hidden_acts[0]), hidden_acts.size() * sizeof(float));
+}
+
---
+++ source/aogmaneo/routedlayer.h
@@ -0,0 +1,168 @@
+// ----------------------------------------------------------------------------
+//  AOgmaNeo
+//  Copyright(c) 2020-2023 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of AOgmaNeo is licensed to you under the terms described
+//  in the AOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include "helpers.h"
+
+namespace aon {
+// a routed layer
+class Routed_Layer {
+public:
+    // visible layer descriptor
+    struct Visible_Layer_Desc {
+        Int3 size; // size of input
+
+        int radius; // radius onto input
+
+        // defaults
+        Visible_Layer_Desc()
+        :
+        size(4, 4, 16),
+        radius(2)
+        {}
+    };
+
+    // visible layer
+    struct Visible_Layer {
+        S_Byte_Buffer weights;
+
+        Float_Buffer errors;
+    };
+
+    struct Params {
+        float scale; // scale of softmax
+        float lr; // learning rate
+
+        Params()
+        :
+        scale(4.0f),
+        lr(0.02f)
+        {}
+    };
+
+private:
+    Int3 hidden_size; // size of the output/hidden/prediction
+
+    Float_Buffer hidden_acts;
+
+    // visible layers and descs
+    Array<Visible_Layer> visible_layers;
+    Array<Visible_Layer_Desc> visible_layer_descs;
+
+    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
+
+    // --- kernels ---
+
+    void forward(
+        const Int2 &column_pos,
+        const Array<Int_Buffer_View> &input_cis,
+        const Array<Float_Buffer_View> &input_acts,
+        Int_Buffer_View route_cis,
+        const Params &params
+    );
+
+    void backward(
+        const Int2 &column_pos,
+        Int_Buffer_View input_cis,
+        Float_Buffer_View input_acts,
+        Int_Buffer_View route_cis,
+        Float_Buffer_View errors,
+        int vli,
+        bool learn_enabled,
+        unsigned long* state,
+        const Params &params
+    );
+
+public:
+    // create with random initialization
+    void init_random(
+        const Int3 &hidden_size, // hidden/output/prediction size
+        const Array<Visible_Layer_Desc> &visible_layer_descs
+    );
+
+    void forward(
+        const Array<Int_Buffer_View> &input_cis,
+        const Array<Float_Buffer_View> &input_acts,
+        Int_Buffer_View route_cis,
+        const Params &params
+    );
+
+    void backward(
+        const Array<Int_Buffer_View> &input_cis,
+        const Array<Float_Buffer_View> &input_acts,
+        Int_Buffer_View route_cis,
+        Float_Buffer_View errors,
+        bool learn_enabled,
+        const Params &params
+    );
+
+    void clear_state();
+
+    // serialization
+    int size() const; // returns size in Bytes
+    int state_size() const; // returns size of state in Bytes
+
+    void write(
+        Stream_Writer &writer
+    ) const;
+
+    void read(
+        Stream_Reader &reader
+    );
+
+    void write_state(
+        Stream_Writer &writer
+    ) const;
+
+    void read_state(
+        Stream_Reader &reader
+    );
+
+    // get number of visible layers
+    int get_num_visible_layers() const {
+        return visible_layers.size();
+    }
+
+    // get a visible layer
+    Visible_Layer &get_visible_layer(
+        int i // index of visible layer
+    ) {
+        return visible_layers[i];
+    }
+
+    // get a visible layer
+    const Visible_Layer &get_visible_layer(
+        int i // index of visible layer
+    ) const {
+        return visible_layers[i];
+    }
+
+    // get a visible layer descriptor
+    const Visible_Layer_Desc &get_visible_layer_desc(
+        int i // index of visible layer
+    ) const {
+        return visible_layer_descs[i];
+    }
+
+    // get the hidden states (predictions)
+    const Int_Buffer &get_hidden_cis() const {
+        return hidden_cis;
+    }
+
+    // get the hidden states (predictions)
+    const Float_Buffer &get_hidden_acts() const {
+        return hidden_acts;
+    }
+
+    // get the hidden size
+    const Int3 &get_hidden_size() const {
+        return hidden_size;
+    }
+};
+}
