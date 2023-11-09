832e2c3fd2c4b6f2894f509c9ae67d000f1ff461
Eric Laukien <ericlaukien@gmail.com>
2023-11-08 20:10:25 -0500

    Tests

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -164,7 +164,7 @@ void Decoder::update_gates(
                 for (int hc =  0; hc < hidden_size.z; hc++) {
                     int wi = hc + wi_start;
 
-                    float w = (vl.weights[wi] - 127.0f) * half_byte_inv;
+                    float w = (127.0f - vl.weights[wi]) * half_byte_inv;
 
                     sum += w * w;
                 }
