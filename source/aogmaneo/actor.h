b7a0c075450d1b5910c7b15ab4e80f5154febafd
Eric Laukien <ericlaukien@gmail.com>
2023-12-06 19:56:53 -0500

    Experiments

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -51,8 +51,8 @@ public:
 
         Params()
         :
-        vlr(0.01f),
-        alr(0.01f),
+        vlr(0.02f),
+        alr(0.02f),
         discount(0.99f),
         min_steps(8),
         history_iters(8)
