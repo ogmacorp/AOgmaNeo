a8555a145e64a5811308bd13f9fd2708b4b4e3eb
Eric Laukien <ericlaukien@gmail.com>
2023-12-31 17:31:38 -0500

    Tuning

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -204,7 +204,7 @@ void Decoder::update_gates(
 
                         int wi = di + wi_start;
 
-                        float w = (128.0f - vl.weights[wi]) * half_byte_inv;
+                        float w = (127.0f - vl.weights[wi]) * half_byte_inv;
 
                         sum += w * w;
                     }
@@ -319,7 +319,7 @@ void Decoder::init_random(
         vl.weights.resize(num_dendrites * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = 128 + (rand() % init_weight_noisei);
+            vl.weights[i] = 127 + (rand() % init_weight_noisei);
 
         vl.input_cis_prev = Int_Buffer(num_visible_columns, 0);
 
