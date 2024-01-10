de4c2a40c9f9282ea1694c661174444164b76600
Eric Laukien <ericlaukien@gmail.com>
2024-01-09 19:18:44 -0500

    Sparsere

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -48,10 +48,10 @@ public:
 
         Params()
         :
-        choice(0.0001f),
-        vigilance(0.9f),
-        lr(0.1f),
-        l_radius(1)
+        choice(0.01f),
+        vigilance(0.5f),
+        lr(0.5f),
+        l_radius(2)
         {}
     };
 
