855a33c5987fe483dd2d4365c68a0a4b7ffc88b6
Eric Laukien <ericlaukien@gmail.com>
2024-02-12 20:19:04 -0500

    Tests

--- source/aogmaneo/decoder.h
+++ source/aogmaneo/decoder.h
@@ -41,8 +41,8 @@ public:
 
         Params()
         :
-        scale(8.0f),
-        lr(0.05f)
+        scale(16.0f),
+        lr(0.02f)
         {}
     };
 
--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -48,8 +48,8 @@ public:
 
         Params()
         :
-        scale(8.0f),
-        lr(0.05f)
+        scale(16.0f),
+        lr(0.02f)
         {}
     };
 
