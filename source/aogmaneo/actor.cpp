492ada104e6d72055a7c3a26430c796504fbf5e8
Eric Laukien <ericlaukien@gmail.com>
2024-03-30 20:03:53 -0400

    Abs trace

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -301,7 +301,7 @@ void Actor::forward(
                                 if (vc == in_ci_prev) {
                                     float trace = vl.policy_traces[wi];
 
-                                    vl.policy_traces[wi] += error * policy_dendrite_acts_prev[dendrite_index] * expf(-trace * trace * params.trace_curve);
+                                    vl.policy_traces[wi] += error * policy_dendrite_acts_prev[dendrite_index] * expf(-abs(trace) * params.trace_curve);
                                 }
 
                                 vl.policy_weights[wi] += policy_error_partial * vl.policy_traces[wi];
@@ -318,7 +318,7 @@ void Actor::forward(
                             if (vc == in_ci_prev) {
                                 float trace = vl.value_traces[wi];
 
-                                vl.value_traces[wi] += value_dendrite_acts_prev[dendrite_index] * expf(-trace * trace * params.trace_curve);
+                                vl.value_traces[wi] += value_dendrite_acts_prev[dendrite_index] * expf(-abs(trace) * params.trace_curve);
                             }
 
                             vl.value_weights[wi] += value_delta * vl.value_traces[wi];
