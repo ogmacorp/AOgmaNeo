9575e9a03b0371d178ccf0b0993ec0e5007c7578
Eric Laukien <ericlaukien@gmail.com>
2024-04-24 19:40:40 -0400

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -31,9 +31,7 @@ public:
     // visible layer
     struct Visible_Layer {
         Byte_Buffer weights;
-
-        Int_Buffer recon_sums;
-
+        
         float importance;
 
         Visible_Layer()
@@ -43,65 +41,48 @@ public:
     };
 
     struct Params {
-        float scale; // recon curve
+        float choice; // choice parameter, lower makes it select matchier columns over ones with less overall weights (total)
+        float vigilance; // ART vigilance
         float lr; // learning rate
-        int spatial_recon_tolerance;
-        int recurrent_recon_tolerance;
+        int l_radius; // second stage inhibition radius
 
         Params()
         :
-        scale(4.0f),
-        lr(0.02f),
-        spatial_recon_tolerance(1),
-        recurrent_recon_tolerance(1)
+        choice(0.01f),
+        vigilance(0.8f),
+        lr(0.5f),
+        l_radius(2)
         {}
     };
 
 private:
     Int3 hidden_size; // size of hidden/output layer
-    int spatial_activity;
-    int recurrent_radius;
 
-    Int_Buffer spatial_cis;
     Int_Buffer hidden_cis;
-    Int_Buffer hidden_cis_prev;
 
-    Float_Buffer hidden_acts;
+    Int_Buffer learn_cis;
+
+    Float_Buffer hidden_sums;
+
+    Float_Buffer hidden_totals;
+
+    Float_Buffer hidden_maxs;
 
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
     
-    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
-    
-    Byte_Buffer recurrent_weights;
-
-    Int_Buffer recurrent_recon_sums;
-
     // --- kernels ---
-
-    void forward_spatial(
+    
+    void forward(
         const Int2 &column_pos,
         const Array<Int_Buffer_View> &input_cis,
         const Params &params
     );
 
-    void forward_recurrent(
+    void learn(
         const Int2 &column_pos,
-        const Params &params
-    );
-
-    void learn_spatial(
-        const Int2 &column_pos,
-        Int_Buffer_View input_cis,
-        int vli,
-        unsigned long* state,
-        const Params &params
-    );
-
-    void learn_recurrent(
-        const Int2 &column_pos,
-        unsigned long* state,
+        const Array<Int_Buffer_View> &input_cis,
         const Params &params
     );
 
@@ -109,8 +90,6 @@ public:
     // create a sparse coding layer with random initialization
     void init_random(
         const Int3 &hidden_size, // hidden/output size
-        int spatial_activity,
-        int recurrent_radius,
         const Array<Visible_Layer_Desc> &visible_layer_descs // descriptors for visible layers
     );
 
@@ -123,9 +102,9 @@ public:
     void clear_state();
 
     // serialization
-    long size() const; // returns size in bytes
-    long state_size() const; // returns size of state in bytes
-    long weights_size() const; // returns size of weights in bytes
+    long size() const; // returns size in Bytes
+    long state_size() const; // returns size of state in Bytes
+    long weights_size() const; // returns size of weights in Bytes
 
     void write(
         Stream_Writer &writer
