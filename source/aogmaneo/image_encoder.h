7d5978a6f0a416646511b2591b6cf64c2f0f4c20
Eric Laukien <ericlaukien@gmail.com>
2024-05-02 17:50:02 -0400

    Tests

--- source/aogmaneo/image_encoder.h
+++ source/aogmaneo/image_encoder.h
@@ -30,26 +30,27 @@ public:
 
     // visible layer
     struct Visible_Layer {
-        Byte_Buffer weights;
-        Byte_Buffer recon_weights; // for reconstruction
+        Byte_Buffer weights0; // regular
+        Byte_Buffer weights1; // complement
+        Byte_Buffer weights_recon; // for reconstruction
 
         Byte_Buffer reconstruction;
     };
 
     struct Params {
-        float falloff; // amount less when not maximal (multiplier)
+        float choice;
+        float vigilance;
         float lr; // learning rate
-        float scale; // scale of reconstruction
+        float scale;
         float rr; // reconstruction rate
-        int radius; // SOM neighborhood radius
         
         Params()
         :
-        falloff(0.9f),
-        lr(0.05f),
+        choice(0.01f),
+        vigilance(0.95f),
+        lr(0.5f),
         scale(2.0f),
-        rr(0.01f),
-        radius(3)
+        rr(0.01f)
         {}
     };
 
@@ -57,11 +58,6 @@ private:
     Int3 hidden_size; // size of hidden/output layer
 
     Int_Buffer hidden_cis; // hidden states
-
-    Float_Buffer hidden_acts;
-    Float_Buffer hidden_totals;
-
-    Float_Buffer hidden_resources;
 
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
@@ -100,7 +96,7 @@ public:
     void step(
         const Array<Byte_Buffer_View> &inputs, // input states
         bool learn_enabled, // whether to learn
-        bool learn_recon = true // whether to learn a reconstruction as well (conditional on learn_enabled)
+        bool learn_recon = true // whether to learn reconstruction weights
     );
 
     void reconstruct(
@@ -116,7 +112,7 @@ public:
     // serialization
     long size() const; // returns size in bytes
     long state_size() const; // returns state size in bytes
-    long weights_size() const; // returns recon_weights size in bytes
+    long weights_size() const; // returns weights size in bytes
 
     void write(
         Stream_Writer &writer
