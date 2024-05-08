2f3ba4d9c77707518a8eb744a0371e7551dc4f2c
Eric Laukien <ericlaukien@gmail.com>
2024-05-08 16:43:10 -0400

    Tests

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -53,7 +53,7 @@ public:
         plr(0.002f),
         leak(0.01f),
         discount(0.99f),
-        policy_clip(0.5f),
+        policy_clip(0.25f),
         value_clip(1.0f),
         trace_decay(0.97f)
         {}
