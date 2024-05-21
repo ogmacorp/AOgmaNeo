d4ac746a662c1a167cb973ec5acd5fa2dd391a8c
Eric Laukien <ericlaukien@gmail.com>
2024-05-21 12:03:16 -0400

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -33,6 +33,7 @@ public:
         Byte_Buffer weights;
 
         Int_Buffer recon_sums;
+        Float_Buffer recon_deltas;
 
         float importance;
 
@@ -45,13 +46,11 @@ public:
     struct Params {
         float scale; // recon curve
         float lr; // learning rate
-        int early_stop_cells; // if target of reconstruction is in top <this number> cells, stop early
 
         Params()
         :
-        scale(8.0f),
-        lr(0.02f),
-        early_stop_cells(1)
+        scale(4.0f),
+        lr(0.02f)
         {}
     };
 
