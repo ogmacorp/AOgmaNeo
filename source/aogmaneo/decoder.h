94e6ecb9544c8b20e2c0a5446c283b818f008dc4
Eric Laukien <ericlaukien@gmail.com>
2024-06-06 13:55:54 -0400

    Tests

--- source/aogmaneo/decoder.h
+++ source/aogmaneo/decoder.h
@@ -37,12 +37,14 @@ public:
         float scale; // scale of activations
         float lr; // weight learning rate
         float leak; // relu leak
+        float stability; // how stable the weights are (inverse of plasticity)
 
         Params()
         :
         scale(8.0f),
         lr(0.02f),
-        leak(0.01f)
+        leak(0.01f),
+        stability(2.0f)
         {}
     };
 
