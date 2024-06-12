fd80a3ed694d6b96886879fe2920f46f40483c4a
Eric Laukien <ericlaukien@gmail.com>
2024-06-12 10:59:28 -0400

    Updated

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -183,7 +183,7 @@ void Encoder::learn(
         if (vc != target_ci && recon_sum >= target_sum)
             num_higher++;
 
-        float recon = expf(min(0, recon_sum - count * 127) * recon_scale);
+        float recon = expf((recon_sum - count * 255) * recon_scale);
 
         modulation = min(modulation, recon);
 
@@ -261,7 +261,7 @@ void Encoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = 127 - (rand() % init_weight_noisei);
+            vl.weights[i] = 255 - (rand() % init_weight_noisei);
 
         vl.recon_sums.resize(num_visible_cells);
         vl.recon_deltas.resize(num_visible_cells);
