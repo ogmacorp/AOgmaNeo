0a0482eeef4e094db0102bbdafb3c1aede871daf
Eric Laukien <ericlaukien@gmail.com>
2024-06-14 13:55:53 -0400

    Tests

--- source/aogmaneo/decoder.h
+++ source/aogmaneo/decoder.h
@@ -42,9 +42,9 @@ public:
         Params()
         :
         scale(4.0f),
-        lr(0.02f),
+        lr(0.04f),
         leak(0.01f),
-        stability(2.0f)
+        stability(1.0f)
         {}
     };
 
