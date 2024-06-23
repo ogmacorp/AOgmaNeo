d2fab4ab4cb2cccd1f40f72970f1e8cafeaa8e61
Eric Laukien <ericlaukien@gmail.com>
2024-06-23 16:35:03 -0400

    Tests

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -169,32 +169,40 @@ void Encoder::learn(
 
     const float recon_scale = sqrtf(1.0f / max(1, count)) / 255.0f * params.scale;
 
-    int target_sum = vl.recon_sums[target_ci + visible_cells_start];
-
-    int num_higher = 0;
-
-    float modulation = 1.0f;
+    int max_index = 0;
+    int max_recon_sum = 0;
 
     for (int vc = 0; vc < vld.size.z; vc++) {
         int visible_cell_index = vc + visible_cells_start;
 
         int recon_sum = vl.recon_sums[visible_cell_index];
 
-        if (vc != target_ci && recon_sum >= target_sum)
-            num_higher++;
+        if (recon_sum > max_recon_sum) {
+            max_recon_sum = recon_sum;
+            max_index = vc;
+        }
+    }
+
+    // early stop
+    if (max_index == target_ci)
+        return;
+
+    float modulation = 0.0f;
+
+    for (int vc = 0; vc < vld.size.z; vc++) {
+        int visible_cell_index = vc + visible_cells_start;
+
+        int recon_sum = vl.recon_sums[visible_cell_index];
 
         float recon = expf((recon_sum - count * 255) * recon_scale);
 
-        modulation = min(modulation, recon);
+        if (vc != max_index)
+            modulation += recon;
 
         vl.recon_deltas[visible_cell_index] = params.lr * 255.0f * ((vc == target_ci) - recon);
     }
 
-    // early stop
-    if (num_higher == 0)
-        return;
-
-    modulation = powf(modulation, params.stability);
+    modulation = powf(modulation / (vld.size.z - 1), params.stability);
 
     // re-use recon sums as integer deltas
     for (int vc = 0; vc < vld.size.z; vc++) {
--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -50,7 +50,7 @@ public:
 
         Params()
         :
-        scale(2.0f),
+        scale(4.0f),
         lr(0.02f),
         stability(2.0f)
         {}
