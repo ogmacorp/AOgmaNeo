3acf4fe517377c0e8ab7eed3a8492150a2715c38
Eric Laukien <ericlaukien@gmail.com>
2024-06-27 19:10:34 -0400

    Tuning

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -49,12 +49,12 @@ public:
 
         Params()
         :
-        vlr(0.001f),
-        plr(0.001f),
+        vlr(0.002f),
+        plr(0.002f),
         leak(0.01f),
         discount(0.99f),
         policy_clip(0.1f),
-        value_clip(0.2f),
+        value_clip(0.5f),
         trace_decay(0.97f)
         {}
     };
