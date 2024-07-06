4d3d0904283f179babac517e8e78277418650dcc
Eric Laukien <ericlaukien@gmail.com>
2024-07-05 20:13:10 -0400

    Tests

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -7,6 +7,7 @@
 // ----------------------------------------------------------------------------
 
 #include "encoder.h"
+#include <iostream>
 
 using namespace aon;
 
@@ -135,7 +136,7 @@ void Encoder::forward(
 
     learn_cis[hidden_column_index] = max_index;
 
-    hidden_comparisons[hidden_column_index] = (max_index == -1 ? 0.0f : max_complete_activation);
+    hidden_comparisons[hidden_column_index] = max_activation;
 
     hidden_cis[hidden_column_index] = max_complete_index;
 }
