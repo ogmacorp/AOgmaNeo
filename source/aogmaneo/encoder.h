dbd3e7f8de5a5d3c307d44ca3d86c7359dec45e9
Eric Laukien <ericlaukien@gmail.com>
2024-07-08 16:50:05 -0400

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -64,8 +64,8 @@ public:
         Params()
         :
         choice(0.1f),
-        vigilance(0.9f),
-        lr(0.1f),
+        vigilance(0.98f),
+        lr(1.0f),
         active_ratio(0.1f),
         l_radius(2)
         {}
