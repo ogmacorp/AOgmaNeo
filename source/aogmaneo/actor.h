2c8be50f87fecff9a4f6c54eb18f18a4360cc751
Eric Laukien <ericlaukien@gmail.com>
2024-07-15 14:50:30 -0400

    Tests

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -57,7 +57,7 @@ public:
         cons(0.01f),
         leak(0.01f),
         discount(0.99f),
-        delay_rate(0.1f),
+        delay_rate(0.5f),
         n_steps(5),
         history_iters(16)
         {}
