99a2638e2875413ae5f11979855239952ceac09f
Eric Laukien <ericlaukien@gmail.com>
2024-07-15 18:24:21 -0400

    Advantage value seperation

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -65,7 +65,7 @@ void Actor::forward(
 
                 Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
 
-                int wi_start_partial = hidden_size.z * (offset.y + diam * (offset.x + diam * (in_ci + vld.size.z * hidden_column_index)));
+                int wi_start_partial = offset.y + diam * (offset.x + diam * (in_ci + vld.size.z * hidden_column_index));
 
                 for (int hc = 0; hc < hidden_size.z; hc++) {
                     int hidden_cell_index = hc + hidden_cells_start;
