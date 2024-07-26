70aa3ae3b8abe767fd78e51fb92595d44d9697f5
Eric Laukien <ericlaukien@gmail.com>
2024-07-26 15:52:52 -0400

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -7,6 +7,7 @@
 // ----------------------------------------------------------------------------
 
 #include "encoder.h"
+#include <iostream>
 
 using namespace aon;
 
@@ -135,7 +136,7 @@ void Encoder::learn(
             if (in_bounds0(other_column_pos, Int2(hidden_size.x, hidden_size.y))) {
                 int other_hidden_column_index = address2(other_column_pos, Int2(hidden_size.x, hidden_size.y));
 
-                if (hidden_comparisons[other_hidden_column_index] >= hidden_max)
+                if (hidden_comparisons[other_hidden_column_index] > hidden_max)
                     num_higher++;
 
                 count++;
