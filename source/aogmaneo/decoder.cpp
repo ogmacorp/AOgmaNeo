e57cbf6508a06372685ab8be07fe767e37d5e77f
Eric Laukien <ericlaukien@gmail.com>
2024-07-26 13:56:43 -0400

    Experiments

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -152,7 +152,12 @@ void Decoder::learn(
 
     int target_ci = hidden_target_cis[hidden_column_index];
 
+    if (hidden_cis[hidden_column_index] == target_ci)
+        return;
+
     const int half_num_dendrites_per_cell = num_dendrites_per_cell / 2;
+
+    float modulation = powf(1.0f - hidden_acts[hidden_cis[hidden_column_index] + hidden_cells_start], params.stability);
 
     // find deltas
     for (int hc = 0; hc < hidden_size.z; hc++) {
@@ -160,7 +165,7 @@ void Decoder::learn(
 
         int dendrites_start = num_dendrites_per_cell * hidden_cell_index;
 
-        float error = params.lr * 127.0f * ((hc == target_ci) - hidden_acts[hidden_cell_index]);
+        float error = params.lr * 127.0f * modulation * ((hc == target_ci) - hidden_acts[hidden_cell_index]);
 
         for (int di = 0; di < num_dendrites_per_cell; di++) {
             int dendrite_index = di + dendrites_start;
--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -168,25 +168,47 @@ void Encoder::learn(
 
     const float recon_scale = sqrtf(1.0f / max(1, count)) / 255.0f * params.scale;
 
-    int target_sum = vl.recon_sums[target_ci + visible_cells_start];
-    int num_higher = 0;
+    int max_index = 0;
+    int max_recon_sum = 0;
 
     for (int vc = 0; vc < vld.size.z; vc++) {
         int visible_cell_index = vc + visible_cells_start;
 
         int recon_sum = vl.recon_sums[visible_cell_index];
 
-        if (vc != target_ci && recon_sum >= target_sum)
-            num_higher++;
-
-        float recon = expf((recon_sum - count * 255) * recon_scale);
-
-        vl.recon_sums[visible_cell_index] = roundf(params.lr * 255.0f * ((vc == target_ci) - recon));
+        if (recon_sum > max_recon_sum) {
+            max_recon_sum = recon_sum;
+            max_index = vc;
+        }
     }
 
     // early stop
-    if (num_higher == 0)
+    if (max_index == target_ci)
         return;
+
+    float modulation = 0.0f;
+
+    for (int vc = 0; vc < vld.size.z; vc++) {
+        int visible_cell_index = vc + visible_cells_start;
+
+        int recon_sum = vl.recon_sums[visible_cell_index];
+
+        float recon = expf((recon_sum - count * 255) * recon_scale);
+
+        if (vc != max_index)
+            modulation += recon;
+
+        vl.recon_deltas[visible_cell_index] = params.lr * 255.0f * ((vc == target_ci) - recon);
+    }
+
+    modulation = powf(modulation / (vld.size.z - 1), params.stability);
+
+    // re-use recon sums as integer deltas
+    for (int vc = 0; vc < vld.size.z; vc++) {
+        int visible_cell_index = vc + visible_cells_start;
+
+        vl.recon_sums[visible_cell_index] = roundf(modulation * vl.recon_deltas[visible_cell_index]);
+    }
 
     for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
         for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
@@ -249,6 +271,7 @@ void Encoder::init_random(
             vl.weights[i] = 255 - (rand() % init_weight_noisei);
 
         vl.recon_sums.resize(num_visible_cells);
+        vl.recon_deltas.resize(num_visible_cells);
     }
 
     hidden_cis = Int_Buffer(num_hidden_columns, 0);
@@ -392,6 +415,7 @@ void Encoder::read(
         reader.read(&vl.weights[0], vl.weights.size() * sizeof(Byte));
 
         vl.recon_sums.resize(num_visible_cells);
+        vl.recon_deltas.resize(num_visible_cells);
 
         reader.read(&vl.importance, sizeof(float));
     }
