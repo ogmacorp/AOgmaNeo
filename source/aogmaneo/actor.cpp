5c36c7b5a35370604f3b324c91343e9c407110dc
Eric Laukien <ericlaukien@gmail.com>
2024-08-13 12:33:43 -0400

    Improved mimicry

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -200,11 +200,11 @@ void Actor::forward(
     if (learn_enabled) {
         float td_error_value = reward + params.discount * value - value_prev;
         
-        float value_delta = params.vlr * td_error_value;
+        float value_delta = params.vlr * tanhf(td_error_value);
 
         int target_ci = hidden_target_cis_prev[hidden_column_index];
 
-        float policy_delta_partial = params.plr * (1.0f - mimic) * td_error_value;
+        float policy_delta_partial = params.plr * ((1.0f - mimic) * td_error_value + mimic);
 
         for (int di = 0; di < value_num_dendrites_per_cell; di++) {
             int dendrite_index = di + value_dendrites_start;
@@ -270,7 +270,7 @@ void Actor::forward(
                             if (vc == in_ci_prev)
                                 vl.value_traces[wi] += value_dendrite_acts_prev[dendrite_index]; // accumulating trace
 
-                            vl.value_weights[wi] += min(params.value_clip, max(-params.value_clip, value_delta * vl.value_traces[wi]));
+                            vl.value_weights[wi] += value_delta * vl.value_traces[wi];
                             vl.value_traces[wi] *= params.trace_decay;
                         }
 
@@ -286,13 +286,10 @@ void Actor::forward(
 
                                 int wi = di + wi_start;
 
-                                if (vc == in_ci_prev) {
+                                if (vc == in_ci_prev)
                                     vl.policy_traces[wi] += policy_dendrite_acts_prev[dendrite_index]; // accumulating trace
 
-                                    vl.policy_weights[wi] += mimic * policy_dendrite_acts_prev[dendrite_index];
-                                }
-
-                                vl.policy_weights[wi] += min(params.policy_clip, max(-params.policy_clip, policy_delta_partial * vl.policy_traces[wi]));
+                                vl.policy_weights[wi] += policy_delta_partial * vl.policy_traces[wi];
                                 vl.policy_traces[wi] *= params.trace_decay;
                             }
                         }
