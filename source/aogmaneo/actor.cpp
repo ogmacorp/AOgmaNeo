3df8b4dd5fdfe63afc3cf95520b79e23f1645130
Eric Laukien <ericlaukien@gmail.com>
2024-08-18 15:45:06 -0400

    Added policy TD limiter

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -204,7 +204,7 @@ void Actor::forward(
 
         float scaled_td_error = td_error / max(limit_small, hidden_td_scales[hidden_column_index]);
 
-        float value_delta = params.vlr * td_error;
+        float value_delta = params.vlr * scaled_td_error;
 
         int target_ci = hidden_target_cis_prev[hidden_column_index];
 
