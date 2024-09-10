9d3c8856477ba96aca358c50e06d86f43c5c5384
Eric Laukien <ericlaukien@gmail.com>
2024-09-10 19:19:59 -0400

    Impl

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -70,14 +70,10 @@ private:
 
     Int_Buffer hidden_cis;
 
+    Array<Vec<S>> hidden_vecs;
+
     Float_Buffer hidden_learn_vecs;
-    S_Byte_Buffer hidden_code_vecs;
-
-    Int_Buffer hidden_sums;
-
-    S_Byte_Buffer hidden_bundle_vecs;
-    S_Byte_Buffer hidden_temp_vecs;
-    S_Byte_Buffer hidden_vecs;
+    Array<Vec<S>> hidden_code_vecs;
 
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
@@ -85,41 +81,41 @@ private:
 
     // --- helpers ---
 
-    void multiply_a_at(
-        Vec<S>* codes,
-        Vec<S>* vs, // source vector
-        Bundle<S>* vd, // destination bundle
-        int cs
+    Bundle<S> multiply_a_at(
+        const Array<Vec<S>> &codes,
+        int codes_start, // start index into codebook
+        Vec<S> v, // vector to multiply
+        int num_codes
     ) {
-        // zero out vd
-        for (int r = 0; r < S; r++)
-            vd[r] = 0;
+        Bundle<S> vd = 0;
 
         // accumulate from above diagonal (symmetric)
-        for (int c = 0; c < cs; c++) {
+        for (int c = 0; c < num_codes; c++) {
             for (int c2 = 0; c2 < c; c2++) { // only compute up to diagonal
-                int a_at_elem = codes[c].dot(codes[c2]);
+                int a_at_elem = codes[codes_start + c].dot(codes[codes_start + c2]);
 
                 // exploit symmetry
                 for (int r = 0; r < S; r++) {
                     // upper triangle
-                    vd[r] += a_at_elem * vs[c];
+                    vd[r] += a_at_elem * v.get(r);
 
                     // lower triangle
                     int r_comp = S - r;
 
-                    vd[r_comp] += a_at_elem * vs[r_comp];
+                    vd[r_comp] += a_at_elem * v.get(r_comp);
                 }
             }
         }
 
         // accumlate from diagonal
-        for (int c = 0; c < cs; c++) {
-            int a_at_elem = codes[c].dot(codes[c]);
+        for (int c = 0; c < num_codes; c++) {
+            int a_at_elem = codes[codes_start + c].dot(codes[codes_start + c]);
 
             for (int r = 0; r < S; r++)
-                vd[r] += a_at_elem * vs[c];
+                vd[r] += a_at_elem * v.get(r);
         }
+
+        return vd;
     }
     
     // --- kernels ---
@@ -138,9 +134,11 @@ private:
         vl.input_vecs[visible_column_index] = vl.visible_pos_vecs[visible_column_index];
 
         for (int fi = 0; fi < vld.size.z; fi++) {
-            int in_ci = vl.input_cis[fi + vld.size.z * visible_column_index];
+            int visible_features_index = fi + vld.size.z * visible_column_index;
 
-            vl.input_vecs[visible_column_index] *= vl.visible_code_vecs[in_ci + vld.size.w * (fi + vld.size.z * visible_column_index)];
+            int in_ci = vl.input_cis[visible_features_index];
+
+            vl.input_vecs[visible_column_index] *= vl.visible_code_vecs[in_ci + vld.size.w * visible_features_index];
         }
     }
 
@@ -150,8 +148,6 @@ private:
         const Params &params
     ) {
         int hidden_column_index = address2(column_pos, Int2(hidden_size.x, hidden_size.y));
-
-        int hidden_vecs_start = vec_size * hidden_column_index;
 
         Bundle<S> sum = 0;
 
@@ -182,8 +178,6 @@ private:
                     for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
                         int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
 
-                        int visible_vecs_start = vec_size * visible_column_index;
-
                         Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
 
                         sum += vl.input_vecs[visible_column_index];
@@ -210,23 +204,10 @@ private:
 
                     int other_hidden_features_index = ofi + hidden_size.z * hidden_column_index;
 
-                    int other_hidden_codes_start = vec_size * (hidden_cis[ofi + hidden_size.z * hidden_column_index] + hidden_size.w * other_hidden_features_index);
-
-                    temp *= hidden_code_vecs[other_hidden_features_index];
+                    temp *= hidden_code_vecs[hidden_cis[other_hidden_features_index] + other_hidden_features_index * hidden_size.w];
                 }
 
-                int hidden_codes_start = vec_size * (hidden_cis[fi + hidden_size.z * hidden_column_index] + hidden_size.w * hidden_features_index);
-
-                View_Matrix<S_Byte> code_mat(&hidden_code_vecs[hidden_codes_start], vec_size, hidden_size.z * hidden_size.w);
-                
-                code_mat.multiply_a_at(&hidden_temp_vecs[hidden_vecs_start], &hidden_sums[hidden_vecs_start]);
-
-                // threshold
-                for (int vi = 0; vi < vec_size; vi++) {
-                    int hidden_vec_index = vi + hidden_vecs_start;
-
-                    hidden_temp_vecs[hidden_vec_index] = (hidden_sums[hidden_vec_index] * hidden_temp_vecs[hidden_vec_index] > 0) * 2 - 1;
-                }
+                temp = multiply_a_at(hidden_code_vecs, hidden_size.w * hidden_features_index).thin() * temp;
 
                 // find similarity to code
                 int max_index = 0;
@@ -235,13 +216,7 @@ private:
                 for (int hc = 0; hc < hidden_size.w; hc++) {
                     int hidden_cell_index = hc + hidden_cells_start;
 
-                    int similarity = 0;
-
-                    for (int vi = 0; vi < vec_size; vi++) {
-                        int hidden_vec_index = vi + vec_size * hidden_features_index;
-
-                        similarity += hidden_temp_vecs[hidden_vec_index] * hidden_code_vecs[vi + hidden_codes_start];
-                    }
+                    int similarity = temp.dot(hidden_code_vecs[hidden_cell_index]);
 
                     if (similarity > max_similarity) {
                         max_similarity = similarity;
@@ -254,24 +229,16 @@ private:
             }
         }
 
+        hidden_vec = 1; // set to identity
+
         // find final hidden vec
-        for (int vi = 0; vi < vec_size; vi++) {
-            int hidden_vec_index = vi + hidden_vecs_start;
-
-            hidden_vecs[hidden_vec_index] = 1; // identity
-        }
-
         for (int fi = 0; fi < hidden_size.z; fi++) {
             int hidden_features_index = fi + hidden_size.z * hidden_column_index;
 
-            int hidden_codes_start = vec_size * (hidden_cis[fi + hidden_size.z * hidden_column_index] + hidden_size.w * hidden_features_index);
-
-            for (int vi = 0; vi < vec_size; vi++) {
-                int hidden_vec_index = vi + hidden_vecs_start;
-
-                hidden_vecs[hidden_vec_index] *= hidden_code_vecs[vi + hidden_codes_start];
-            }
+            hidden_vec *= hidden_code_vecs[hidden_cis[hidden_features_index] + hidden_features_index * hidden_size.w];
         }
+
+        hidden_vecs[hidden_column_index] = hidden_vec;
     }
 
     void reconstruct(
@@ -287,7 +254,6 @@ private:
         int visible_column_index = address2(column_pos, Int2(vld.size.x, vld.size.y));
 
         int visible_cells_start = vld.size.z * visible_column_index;
-        int visible_vecs_start = vec_size * visible_column_index;
 
         // projection
         Float2 v_to_h = Float2(static_cast<float>(hidden_size.x) / static_cast<float>(vld.size.x),
@@ -307,11 +273,7 @@ private:
         Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
         Int2 iter_upper_bound(min(hidden_size.x - 1, hidden_center.x + reverse_radii.x), min(hidden_size.y - 1, hidden_center.y + reverse_radii.y));
 
-        for (int vi = 0; vi < vec_size; vi++) {
-            int visible_vec_index = vi + visible_vecs_start;
-
-            vl.visible_bundle_buffer[visible_vec_index] = 0;
-        }
+        Bundle<S> sum = 0;
 
         for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
             for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
@@ -322,20 +284,13 @@ private:
                 Int2 visible_center = project(hidden_pos, h_to_v);
 
                 if (in_bounds(column_pos, Int2(visible_center.x - vld.radius, visible_center.y - vld.radius), Int2(visible_center.x + vld.radius + 1, visible_center.y + vld.radius + 1))) {
-                    int hidden_cell_index_max = hidden_cis[hidden_column_index] + hidden_column_index * hidden_size.z;
-
-                    int hidden_codes_start_max = vec_size * hidden_cell_index_max;
-
                     Int2 offset(column_pos.x - visible_center.x + vld.radius, column_pos.y - visible_center.y + vld.radius);
 
-                    for (int vi = 0; vi < vec_size; vi++) {
-                        int visible_vec_index = vi + visible_vecs_start;
-                        int hidden_code_index = vi + hidden_codes_start_max;
-
-                        vl.visible_bundle_buffer[visible_vec_index] += hidden_vecs[vi + vec_size * hidden_column_index];
-                    }
+                    sum += hidden_vecs[hidden_column_index];
                 }
             }
+
+        Vec<S> visible_vec = sum.thin();
 
         // resonate
         for (int it = 0; it < params.resonate_iters; it++) {
@@ -345,12 +300,7 @@ private:
 
                 int visible_cells_start = vld.size.w * visible_features_index;
 
-                // set temp to vector we are trying to decode
-                for (int vi = 0; vi < vec_size; vi++) {
-                    int visible_vec_index = vi + vec_size * visible_features_index;
-
-                    vl.recon_temp_vecs[visible_vec_index] = (vl.visible_bundle_buffer[visible_vec_index] > 0) * 2 - 1;
-                }
+                Vec<S> temp = visible_vec;
 
                 // bind other feature estimates
                 for (int ofi = 0; ofi < vld.size.z; ofi++) {
@@ -359,27 +309,10 @@ private:
 
                     int other_visible_features_index = ofi + vld.size.z * visible_column_index;
 
-                    int other_visible_codes_start = vec_size * (vl.input_cis[ofi + vld.size.z * visible_column_index] + vld.size.w * other_visible_features_index);
-
-                    for (int ovi = 0; ovi < vec_size; ovi++) {
-                        int other_visible_vec_index = ovi + visible_vecs_start;
-
-                        vl.recon_temp_vecs[other_visible_vec_index] *= vl.visible_code_vecs[ovi + other_visible_codes_start];
-                    }
+                    temp *= vld.visible_code_vecs[vl.recon_cis[other_visible_features_index] + other_visible_features_index * vld.size.w];
                 }
 
-                int visible_codes_start = vec_size * (vl.recon_cis[fi + vld.size.z * visible_column_index] + vld.size.w * visible_features_index);
-
-                View_Matrix<S_Byte> code_mat(&hidden_code_vecs[visible_codes_start], vec_size, vld.size.z * vld.size.w);
-
-                code_mat.multiply_a_at(&vl.recon_temp_vecs[visible_vecs_start], &vl.recon_sums[visible_vecs_start]);
-
-                // multiply by self-correlation matrix
-                for (int vi = 0; vi < vec_size; vi++) {
-                    int visible_vec_index = vi + visible_vecs_start;
-
-                    vl.recon_temp_vecs[visible_vec_index] = (vl.recon_sums[visible_vec_index] * vl.recon_temp_vecs[visible_vec_index] > 0) * 2 - 1;
-                }
+                temp = multiply_a_at(vl.visible_code_vecs, vld.size.w * visible_features_index).thin() * temp;
 
                 // find similarity to code
                 int max_index = 0;
@@ -388,13 +321,7 @@ private:
                 for (int vc = 0; vc < vld.size.w; vc++) {
                     int visible_cell_index = vc + visible_cells_start;
 
-                    int similarity = 0;
-
-                    for (int vi = 0; vi < vec_size; vi++) {
-                        int visible_vec_index = vi + vec_size * visible_features_index;
-
-                        similarity += vl.recon_temp_vecs[visible_vec_index] * vl.visible_code_vecs[vi + visible_codes_start];
-                    }
+                    int similarity = temp.dot(vld.visible_code_vecs[visible_cell_index]);
 
                     if (similarity > max_similarity) {
                         max_similarity = similarity;
