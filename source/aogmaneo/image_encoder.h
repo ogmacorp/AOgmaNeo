8187aa0e4de1463eb02e84aa94a2b53ff4dc165f
Eric Laukien <ericlaukien@gmail.com>
2024-10-02 16:24:58 -0400

    Tuning

--- source/aogmaneo/image_encoder.h
+++ source/aogmaneo/image_encoder.h
@@ -45,11 +45,11 @@ public:
         
         Params()
         :
-        falloff(0.99f),
+        falloff(0.9f),
         lr(0.1f),
         scale(2.0f),
         rr(0.01f),
-        radius(1)
+        radius(2)
         {}
     };
 
