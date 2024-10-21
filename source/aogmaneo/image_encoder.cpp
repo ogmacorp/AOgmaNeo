1e8edbd77b5cfc21e98ab6198c92a6d69c6be602
Eric Laukien <ericlaukien@gmail.com>
2024-10-20 23:53:25 -0400

    Center pre-enc

--- source/aogmaneo/image_encoder.cpp
+++ source/aogmaneo/image_encoder.cpp
@@ -21,6 +21,50 @@ void Image_Encoder::forward(
 
     const float byte_inv = 1.0f / 255.0f;
 
+    float center = 0.0f;
+    int count = 0;
+
+    for (int vli = 0; vli < visible_layers.size(); vli++) {
+        Visible_Layer &vl = visible_layers[vli];
+        const Visible_Layer_Desc &vld = visible_layer_descs[vli];
+
+        int diam = vld.radius * 2 + 1;
+
+        // projection
+        Float2 h_to_v = Float2(static_cast<float>(vld.size.x) / static_cast<float>(hidden_size.x),
+            static_cast<float>(vld.size.y) / static_cast<float>(hidden_size.y));
+
+        Int2 visible_center = project(column_pos, h_to_v);
+
+        // lower corner
+        Int2 field_lower_bound(visible_center.x - vld.radius, visible_center.y - vld.radius);
+
+        // bounds of receptive field, clamped to input size
+        Int2 iter_lower_bound(max(0, field_lower_bound.x), max(0, field_lower_bound.y));
+        Int2 iter_upper_bound(min(vld.size.x - 1, visible_center.x + vld.radius), min(vld.size.y - 1, visible_center.y + vld.radius));
+
+        count += (iter_upper_bound.x - iter_lower_bound.x + 1) * (iter_upper_bound.y - iter_lower_bound.y + 1) * vld.size.z;
+
+        Byte_Buffer_View vl_inputs = inputs[vli];
+
+        for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
+            for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
+                int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
+
+                Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
+
+                int i_start = vld.size.z * (iy + ix * vld.size.y);
+
+                for (int vc = 0; vc < vld.size.z; vc++) {
+                    float input = vl_inputs[vc + i_start] * byte_inv;
+
+                    center += input;
+                }
+            }
+    }
+
+    center /= count;
+
     for (int hc = 0; hc < hidden_size.z; hc++) {
         int hidden_cell_index = hc + hidden_cells_start;
 
@@ -52,27 +96,23 @@ void Image_Encoder::forward(
             for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
                 int visible_column_index = address2(Int2(ix, iy), Int2(vld.size.x, vld.size.y));
 
-                int visible_cells_start = visible_column_index * vld.size.z;
-
                 Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
 
                 int wi_start_partial = vld.size.z * (offset.y + diam * (offset.x + diam * hidden_column_index));
 
+                int i_start = vld.size.z * (iy + ix * vld.size.y);
+
                 for (int vc = 0; vc < vld.size.z; vc++) {
                     int wi_start = hidden_size.z * (vc + wi_start_partial);
 
-                    float input = vl_inputs[vc + visible_cells_start] * byte_inv;
+                    float input_centered = vl_inputs[vc + i_start] * byte_inv - center;
 
                     for (int hc = 0; hc < hidden_size.z; hc++) {
                         int hidden_cell_index = hc + hidden_cells_start;
 
                         int wi = hc + wi_start;
 
-                        float w = vl.weights[wi] * byte_inv;
-
-                        float diff = input - w;
-
-                        hidden_acts[hidden_cell_index] -= diff * diff;
+                        hidden_acts[hidden_cell_index] += input_centered * vl.weights[wi];
                     }
                 }
             }
@@ -137,11 +177,11 @@ void Image_Encoder::forward(
                         for (int vc = 0; vc < vld.size.z; vc++) {
                             int wi = hc + hidden_size.z * (vc + wi_start_partial);
 
-                            float input = vl_inputs[vc + visible_cells_start] * byte_inv;
+                            float input_centered = vl_inputs[vc + visible_cells_start] * byte_inv;
 
-                            float w = vl.weights[wi] * byte_inv;
+                            float w = vl.weights[wi] * byte_inv * 2.0f - 1.0f;
 
-                            vl.weights[wi] = min(255, max(0, roundf(vl.weights[wi] + rate * 255.0f * (input - w))));
+                            vl.weights[wi] = min(255, max(0, roundf(vl.weights[wi] + rate * 255.0f * (input_centered - w))));
                         }
                     }
             }
