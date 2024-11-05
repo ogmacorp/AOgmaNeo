aedb307c1dd73cc958beff831bd0b9b395e2d038
Eric Laukien <ericlaukien@gmail.com>
2024-11-04 19:50:39 -0500

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -176,7 +176,7 @@ void Encoder::learn(
 
         int recon_sum = vl.recon_sums[visible_cell_index];
 
-        if (recon_sum >= target_sum)
+        if (vc != target_ci && recon_sum >= target_sum)
             num_higher++;
 
         // re-use sums as deltas
