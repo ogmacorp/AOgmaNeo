3832c09ba52918c58a13a4787d7e30dd00ff926f
Eric Laukien <ericlaukien@gmail.com>
2024-11-23 19:57:19 -0500

    Tests

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -50,6 +50,7 @@ public:
         float smoothing; // smooth value function, = 1 - lambda from TD(lambda)
         float bias; // bias toward positive policy updates
         float discount; // discount factor
+        float td_scale_decay; // decay on td error scaler
         int min_steps; // minimum steps before sample can be used
         int history_iters; // number of iterations over samples
 
@@ -61,6 +62,7 @@ public:
         smoothing(0.02f),
         bias(0.5f),
         discount(0.99f),
+        td_scale_decay(0.999f),
         min_steps(16),
         history_iters(16)
         {}
@@ -82,6 +84,8 @@ private:
     Float_Buffer policy_dendrite_acts;
 
     Float_Buffer hidden_values; // hidden value function output buffer
+
+    Float_Buffer hidden_td_scales;
 
     Circle_Buffer<History_Sample> history_samples; // history buffer, fixed length
 
