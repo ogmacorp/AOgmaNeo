7f78375912f38378d25617b3ad6a1166004aac66
Eric Laukien <ericlaukien@gmail.com>
2024-12-01 12:42:38 -0500

    Works

--- source/aogmaneo/hierarchy.cpp
+++ source/aogmaneo/hierarchy.cpp
@@ -166,7 +166,7 @@ void Hierarchy::step(
             layer_input_cis[0] = encoders[l - 1].get_hidden_cis();
 
         // activate sparse coder
-        encoders[l].step(layer_input_cis, false, params.layers[l].encoder);
+        encoders[l].step(layer_input_cis, learn_enabled, params.layers[l].encoder);
     }
 
     // backward
