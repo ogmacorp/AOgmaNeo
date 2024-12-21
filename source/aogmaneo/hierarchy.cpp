4147a41bb8320f30aa355e6e88020428e08e29ba
Eric Laukien <ericlaukien@gmail.com>
2024-12-21 13:38:55 -0500

    Experiments

--- source/aogmaneo/hierarchy.cpp
+++ source/aogmaneo/hierarchy.cpp
@@ -92,7 +92,7 @@ void Hierarchy::init_random(
                     if (l < encoders.size() - 1)
                         a_visible_layer_descs[1] = a_visible_layer_descs[0];
 
-                    actors[d_index].init_random(io_sizes[i], io_descs[i].num_dendrites_per_cell, io_descs[i].value_num_dendrites_per_cell, io_descs[i].history_capacity, a_visible_layer_descs);
+                    actors[d_index].init_random(io_sizes[i], io_descs[i].value_num_dendrites_per_cell, io_descs[i].num_dendrites_per_cell, io_descs[i].history_capacity, a_visible_layer_descs);
 
                     i_indices[io_sizes.size() + d_index] = i;
                     d_indices[i] = d_index;
