47f44dce3a99ab61a13d26c33aff24afdb791bc4
Eric Laukien <ericlaukien@gmail.com>
2024-12-24 19:36:43 -0500

    Experiments

--- source/aogmaneo/searcher.cpp
+++ source/aogmaneo/searcher.cpp
@@ -11,22 +11,28 @@
 using namespace aon;
 
 void Searcher::init_random(
-    const Int3 &config_size
+    const Int3 &config_size,
+    int num_dendrites
 ) {
     this->config_size = config_size;
+    this->num_dendrites = num_dendrites;
 
     // pre-compute dimensions
     int num_config_columns = config_size.x * config_size.y;
     int num_config_cells = num_config_columns * config_size.z;
 
-    rewards.resize(num_config_cells);
+    weights.resize(num_dendrites * num_config_cells);
 
-    for (int i = 0; i < rewards.size(); i++)
-        rewards[i] = randf(-init_weight_noisef, init_weight_noisef);
+    for (int i = 0; i < weights.size(); i++)
+        weights[i] = randf(-init_weight_noisef, init_weight_noisef);
 
     config_cis = Int_Buffer(num_config_columns, 0);
 
     temp_cis.resize(num_config_columns);
+
+    dendrite_acts.resize(num_dendrites);
+
+    dendrite_deltas.resize(num_dendrites);
 
     max_temps.resize(num_config_columns);
 }
@@ -38,32 +44,80 @@ void Searcher::step(
 ) {
     int num_config_columns = config_size.x * config_size.y;
 
-    // reward update
-    if (learn_enabled) { // learn from last activation
-        for (int i = 0; i < num_config_columns; i++) {
-            int config_ci = config_cis[i];
+    const int half_num_dendrites = num_dendrites / 2;
 
-            int wi = config_ci + config_size.z * i;
+    // activate dendrites
+    for (int di = 0; di < num_dendrites; di++)
+        dendrite_acts[di] = 0.0f;
 
-            rewards[wi] += params.lr * (reward - rewards[wi]);
+    for (int i = 0; i < num_config_columns; i++) {
+        int config_ci = config_cis[i];
+
+        for (int di = 0; di < num_dendrites; di++) {
+            float w = weights[di + num_dendrites * (config_ci + config_size.z * i)];
+
+            dendrite_acts[di] += w;
         }
     }
 
+    // reward prediction
+    float activation = 0.0f;
+
+    const float dendrite_scale = sqrtf(1.0f / num_config_columns);
+    const float activation_scale = sqrtf(1.0f / num_dendrites);
+
+    for (int di = 0; di < num_dendrites; di++) {
+        float act = dendrite_acts[di] * dendrite_scale;
+
+        dendrite_acts[di] = max(act * params.leak, act); // relu
+
+        activation += dendrite_acts[di] * ((di >= half_num_dendrites) * 2.0f - 1.0f);
+    }
+
+    activation *= activation_scale;
+
+    if (learn_enabled) { // learn from last activation
+        float error = reward - activation;
+
+        for (int di = 0; di < num_dendrites; di++)
+            dendrite_deltas[di] = params.lr * error * ((di >= half_num_dendrites) * 2.0f - 1.0f) * ((dendrite_acts[di] > 0.0f) * (1.0f - params.leak) + params.leak);
+
+        for (int i = 0; i < num_config_columns; i++) {
+            int config_ci = config_cis[i];
+
+            for (int di = 0; di < num_dendrites; di++) {
+                int wi = di + num_dendrites * (config_ci + config_size.z * i);
+
+                weights[wi] += dendrite_deltas[di];
+            }
+        }
+    }
+
+    // determine new representation with gradient of 1
+    for (int di = 0; di < num_dendrites; di++)
+        dendrite_deltas[di] = ((di >= half_num_dendrites) * 2.0f - 1.0f) * ((dendrite_acts[di] > 0.0f) * (1.0f - params.leak) + params.leak);
+
     for (int i = 0; i < num_config_columns; i++) {
         int config_ci = 0;
-        float max_reward = limit_min;
+        float max_grad = limit_min;
 
         for (int cc = 0; cc < config_size.z; cc++) {
-            float r = rewards[cc + config_size.z * i];
+            float grad = 0.0f;
 
-            if (r > max_reward) {
-                max_reward = r;
+            for (int di = 0; di < num_dendrites; di++) {
+                int wi = di + num_dendrites * (cc + config_size.z * i);
+
+                grad += weights[wi] * dendrite_deltas[di];
+            }
+
+            if (grad > max_grad) {
+                max_grad = grad;
                 config_ci = cc;
             }
         }
 
         temp_cis[i] = config_ci;
-        max_temps[i] = max_reward;
+        max_temps[i] = max_grad;
     }
 
     config_cis = margin_cis;
@@ -95,9 +149,9 @@ void Searcher::clear_state() {
 }
 
 long Searcher::size() const {
-    long size = sizeof(Int3) + config_cis.size() * sizeof(int);
+    long size = sizeof(Int3) + sizeof(int) + config_cis.size() * sizeof(int) + dendrite_acts.size() * sizeof(float) + sizeof(int);
 
-    size += rewards.size() * sizeof(float);
+    size += weights.size() * sizeof(float);
 
     return size;
 }
@@ -107,23 +161,25 @@ long Searcher::state_size() const {
 }
 
 long Searcher::weights_size() const {
-    return rewards.size() * sizeof(float);
+    return weights.size() * sizeof(float);
 }
 
 void Searcher::write(
     Stream_Writer &writer
 ) const {
     writer.write(&config_size, sizeof(Int3));
+    writer.write(&num_dendrites, sizeof(int));
 
     writer.write(&config_cis[0], config_cis.size() * sizeof(int));
     
-    writer.write(&rewards[0], rewards.size() * sizeof(float));
+    writer.write(&weights[0], weights.size() * sizeof(float));
 }
 
 void Searcher::read(
     Stream_Reader &reader
 ) {
     reader.read(&config_size, sizeof(Int3));
+    reader.read(&num_dendrites, sizeof(int));
 
     int num_config_columns = config_size.x * config_size.y;
     int num_config_cells = num_config_columns * config_size.z;
@@ -134,9 +190,13 @@ void Searcher::read(
 
     temp_cis.resize(num_config_columns);
 
-    rewards.resize(num_config_cells);
+    dendrite_acts.resize(num_dendrites);
 
-    reader.read(&rewards[0], rewards.size() * sizeof(float));
+    dendrite_deltas.resize(num_dendrites);
+
+    weights.resize(num_dendrites * num_config_cells);
+
+    reader.read(&weights[0], weights.size() * sizeof(float));
 
     max_temps.resize(num_config_columns);
 }
@@ -156,13 +216,13 @@ void Searcher::read_state(
 void Searcher::write_weights(
     Stream_Writer &writer
 ) const {
-    writer.write(&rewards[0], rewards.size() * sizeof(float));
+    writer.write(&weights[0], weights.size() * sizeof(float));
 }
 
 void Searcher::read_weights(
     Stream_Reader &reader
 ) {
-    reader.read(&rewards[0], rewards.size() * sizeof(float));
+    reader.read(&weights[0], weights.size() * sizeof(float));
 }
 
 void Searcher::merge(
@@ -171,21 +231,21 @@ void Searcher::merge(
 ) {
     switch (mode) {
     case merge_random:
-        for (int i = 0; i < rewards.size(); i++) {
+        for (int i = 0; i < weights.size(); i++) {
             int d = rand() % searchers.size();                
 
-            rewards[i] = searchers[d]->rewards[i];
+            weights[i] = searchers[d]->weights[i];
         }
 
         break;
     case merge_average:
-        for (int i = 0; i < rewards.size(); i++) {
+        for (int i = 0; i < weights.size(); i++) {
             float total = 0.0f;
 
             for (int d = 0; d < searchers.size(); d++)
-                total += searchers[d]->rewards[i];
+                total += searchers[d]->weights[i];
 
-            rewards[i] = total / searchers.size();
+            weights[i] = total / searchers.size();
         }
 
         break;
