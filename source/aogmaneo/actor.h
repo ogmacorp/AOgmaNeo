d94d8d81a6dcd01d3c52fad8fe7d494531a6f8d3
Eric Laukien <ericlaukien@gmail.com>
2024-12-28 23:53:59 -0500

    Experiments

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -52,11 +52,11 @@ public:
 
         Params()
         :
-        vlr(0.01f),
-        plr(0.01f),
+        vlr(0.02f),
+        plr(0.02f),
         leak(0.01f),
-        delay_rate(0.001f),
-        policy_clip(0.1f),
+        delay_rate(0.002f),
+        policy_clip(0.2f),
         discount(0.99f),
         td_scale_decay(0.999f),
         trace_decay(0.97f),
