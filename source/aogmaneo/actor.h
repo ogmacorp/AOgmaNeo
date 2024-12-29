c4c167ebe3fffedec7e152749ee9dca4115d6c84
Eric Laukien <ericlaukien@gmail.com>
2024-12-28 19:00:26 -0500

    Experiments

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------
 //  AOgmaNeo
-//  Copyright(c) 2020-2023 Ogma Intelligent Systems Corp. All rights reserved.
+//  Copyright(c) 2020-2024 Ogma Intelligent Systems Corp. All rights reserved.
 //
 //  This copy of AOgmaNeo is licensed to you under the terms described
 //  in the AOGMANEO_LICENSE.md file included in this distribution.
@@ -31,59 +31,56 @@ public:
     // visible layer
     struct Visible_Layer {
         Float_Buffer value_weights;
-        Float_Buffer adv_weights;
+        Float_Buffer value_traces;
         Float_Buffer policy_weights;
-    };
+        Float_Buffer policy_weights_delayed;
+        Float_Buffer policy_traces;
 
-    // history sample for delayed updates
-    struct History_Sample {
-        Array<Int_Buffer> input_cis;
-        Int_Buffer hidden_target_cis_prev;
-
-        float reward;
+        Int_Buffer input_cis_prev;
     };
 
     struct Params {
-        float qlr; // V/A/Q learning rate
+        float vlr; // value learning rate
         float plr; // policy learning rate
-        float leak; // ReLU leak
-        float discount; // discount fActor
-        float reweight; // reweight from AWAC
-        float td_scale_decay; // scaling factor slight decay
-        int n_steps; // q steps
-        int history_iters; // number of iterations over samples
+        float leak; // dendrite ReLU leak
+        float delay_rate; // rate of delayed value and policy weights
+        float policy_clip; // PPO policy clipping coefficient
+        float discount; // discount factor
+        float trace_decay; // eligibility trace decay
+        float trace_squash; // squash traces to be in a smaller range
 
         Params()
         :
-        qlr(0.01f),
-        plr(0.01f),
+        vlr(0.005f),
+        plr(0.001f),
         leak(0.01f),
+        delay_rate(0.001f),
+        policy_clip(0.1f),
         discount(0.99f),
-        reweight(1.0f),
-        td_scale_decay(0.999f),
-        n_steps(8),
-        history_iters(16)
+        trace_decay(0.97f),
+        trace_squash(1.0f)
         {}
     };
 
 private:
     Int3 hidden_size; // hidden/output/action size
-    int num_dendrites_per_cell;
-
-    // current history size - fixed after initialization. determines length of wait before updating
-    int history_size;
+    int value_num_dendrites_per_cell;
+    int policy_num_dendrites_per_cell;
 
     Int_Buffer hidden_cis; // hidden states
 
-    Float_Buffer hidden_td_scales;
-
-    Float_Buffer hidden_advs;
     Float_Buffer hidden_acts;
+    Float_Buffer hidden_acts_prev;
+    Float_Buffer hidden_acts_delayed;
+    Float_Buffer hidden_acts_delayed_prev;
 
-    Float_Buffer dendrite_advs;
-    Float_Buffer dendrite_acts;
+    Float_Buffer value_dendrite_acts;
+    Float_Buffer value_dendrite_acts_prev;
+    Float_Buffer policy_dendrite_acts;
+    Float_Buffer policy_dendrite_acts_prev;
+    Float_Buffer policy_dendrite_acts_delayed;
 
-    Circle_Buffer<History_Sample> history_samples; // history buffer, fixed length
+    Float_Buffer hidden_values; // hidden value function output buffer
 
     // visible layers and descriptors
     Array<Visible_Layer> visible_layers;
@@ -94,13 +91,11 @@ private:
     void forward(
         const Int2 &column_pos,
         const Array<Int_Buffer_View> &input_cis,
+        Int_Buffer_View hidden_target_cis_prev,
+        float reward,
+        float mimic,
+        bool learn_enabled,
         unsigned long* state,
-        const Params &params
-    );
-
-    void learn(
-        const Int2 &column_pos,
-        int t,
         const Params &params
     );
 
@@ -108,8 +103,8 @@ public:
     // initialized randomly
     void init_random(
         const Int3 &hidden_size,
-        int num_dendrites_per_cell,
-        int history_capacity,
+        int value_num_dendrites_per_cell,
+        int policy_num_dendrites_per_cell,
         const Array<Visible_Layer_Desc> &visible_layer_descs
     );
 
@@ -119,6 +114,7 @@ public:
         Int_Buffer_View hidden_target_cis_prev,
         bool learn_enabled,
         float reward,
+        float mimic,
         const Params &params
     );
 
@@ -177,22 +173,14 @@ public:
         return hidden_cis;
     }
 
-    // get hidden advs
-    const Float_Buffer &get_hidden_advs() const {
-        return hidden_advs;
+    // get hidden activations (probabilities) for actions
+    const Float_Buffer &get_hidden_acts() const {
+        return hidden_acts;
     }
 
     // get the hidden size
     const Int3 &get_hidden_size() const {
         return hidden_size;
-    }
-
-    int get_history_capacity() const {
-        return history_samples.size();
-    }
-
-    int get_history_size() const {
-        return history_size;
     }
 
     // merge list of decoders and write to this one
--- source/aogmaneo/hierarchy.h
+++ source/aogmaneo/hierarchy.h
@@ -32,23 +32,19 @@ public:
         int up_radius; // encoder radius
         int down_radius; // decoder radius, also shared with actor if there is one
 
-        int history_capacity; // maximum credit assignment horizon in actor
-
         IO_Desc(
             const Int3 &size = Int3(4, 4, 16),
             IO_Type type = prediction,
             int num_dendrites_per_cell = 4,
             int up_radius = 2,
-            int down_radius = 2,
-            int history_capacity = 512
+            int down_radius = 2
         )
         :
         size(size),
         type(type),
         num_dendrites_per_cell(num_dendrites_per_cell),
         up_radius(up_radius),
-        down_radius(down_radius),
-        history_capacity(history_capacity)
+        down_radius(down_radius)
         {}
     };
 
