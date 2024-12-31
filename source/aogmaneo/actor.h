01eb88a9c26e85542389c04100dec29579519b02
Eric Laukien <ericlaukien@gmail.com>
2024-12-31 10:40:47 -0500

    Simplified

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -63,7 +63,7 @@ public:
         leak(0.01f),
         smoothing(0.02f),
         delay_rate(0.002f),
-        policy_clip(0.2f),
+        policy_clip(0.1f),
         discount(0.99f),
         td_scale_decay(0.999f),
         min_steps(8),
