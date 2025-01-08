ff6ad359237719bb000a84c7f82b1c54cfef123e
Eric Laukien <ericlaukien@gmail.com>
2025-01-08 13:30:32 -0500

    OOD penalty tuning

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -54,7 +54,7 @@ public:
         Params()
         :
         lr(0.01f),
-        ood_penalty(0.01f),
+        ood_penalty(0.0f),
         leak(0.01f),
         discount(0.99f),
         td_scale_decay(0.999f),
