2384d99a01a6480be489ca0e67a350d2d3d6c4b6
Eric Laukien <ericlaukien@gmail.com>
2025-01-15 22:07:50 -0500

    Updated

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -56,7 +56,7 @@ public:
         :
         scale(4.0f),
         vlr(0.001f),
-        alr(0.004f),
+        alr(0.01f),
         leak(0.01f),
         discount(0.99f),
         td_scale_decay(0.999f),
