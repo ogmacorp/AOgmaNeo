8430ebec872a70142b9ecfea03d86911af931e31
Eric Laukien <ericlaukien@gmail.com>
2025-01-28 16:34:48 -0500

    Full complement

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -56,7 +56,7 @@ public:
 
         Params()
         :
-        choice(0.1f),
+        choice(0.01f),
         mismatch(2.0f),
         lr(1.0f),
         active_ratio(0.1f),
--- source/aogmaneo/image_encoder.h
+++ source/aogmaneo/image_encoder.h
@@ -48,7 +48,7 @@ public:
         
         Params()
         :
-        choice(0.1f),
+        choice(0.01f),
         vigilance(0.9f),
         lr(0.5f),
         scale(2.0f),
