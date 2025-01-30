7ecbf72dad83c3b9b5181d22a1c8cd526cc15713
Eric Laukien <ericlaukien@gmail.com>
2025-01-30 13:12:00 -0500

    Experiments

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -53,7 +53,7 @@ public:
         scale(4.0f),
         lr(0.02f),
         spatial_recon_tolerance(1),
-        recurrent_recon_tolerance(1)
+        recurrent_recon_tolerance(2)
         {}
     };
 
