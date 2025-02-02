ee49542cacd463a458d4f8588aa2ea544b0f0d5f
Eric Laukien <ericlaukien@gmail.com>
2025-02-01 19:02:59 -0500

    Cleanup

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -51,9 +51,9 @@ public:
         Params()
         :
         scale(4.0f),
-        lr(0.02f),
+        lr(0.01f),
         spatial_recon_tolerance(1),
-        recurrent_recon_tolerance(1)
+        recurrent_recon_tolerance(2)
         {}
     };
 
