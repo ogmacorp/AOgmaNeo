50c3896ee90529b137c21672af6f094fdeaa7baf
Eric Laukien <ericlaukien@gmail.com>
2025-02-03 22:15:50 -0500

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -78,7 +78,7 @@ private:
     Array<Visible_Layer_Desc> visible_layer_descs;
 
     Float_Buffer recurrent_acts;
-    Byte_Buffer recurrent_protos;
+    Float_Buffer recurrent_protos;
 
     Float_Buffer temporal_resources;
     
