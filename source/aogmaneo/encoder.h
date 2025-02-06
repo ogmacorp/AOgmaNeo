8003d1e9951fb8d04efebb28967c4f7d90b90fde
Eric Laukien <ericlaukien@gmail.com>
2025-02-05 19:41:53 -0500

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -50,8 +50,8 @@ public:
 
         Params()
         :
-        scale(4.0f),
-        lr(0.05f),
+        scale(2.0f),
+        lr(0.02f),
         spatial_recon_tolerance(1),
         recurrent_recon_tolerance(1)
         {}
