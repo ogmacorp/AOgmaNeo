9f383322b3f9afe26d95bb4540fb5704ccc11deb
Eric Laukien <ericlaukien@gmail.com>
2025-02-19 15:27:51 -0500

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -57,9 +57,9 @@ public:
         choice(0.0001f),
         spatial_mismatch(2.0f),
         temporal_mismatch(2.0f),
-        lr(0.5f),
-        active_ratio(0.05f),
-        l_radius(3)
+        lr(1.0f),
+        active_ratio(0.1f),
+        l_radius(2)
         {}
     };
 
