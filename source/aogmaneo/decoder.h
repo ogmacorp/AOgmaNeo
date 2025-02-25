a4b8dd049030483281dd5f87bad59e93373754bd
Eric Laukien <ericlaukien@gmail.com>
2025-02-24 22:30:24 -0500

    Tests

--- source/aogmaneo/decoder.h
+++ source/aogmaneo/decoder.h
@@ -37,14 +37,12 @@ public:
         float scale; // scale of activations
         float lr; // weight learning rate
         float leak; // relu leak
-        float bias; // bias toward positive updates
 
         Params()
         :
         scale(8.0f),
         lr(0.05f),
-        leak(0.01f),
-        bias(0.9f)
+        leak(0.01f)
         {}
     };
 
