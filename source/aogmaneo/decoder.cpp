6499edbcf57bb6cc746ec49fe7e538f693521901
Eric Laukien <ericlaukien@gmail.com>
2025-03-03 11:54:45 -0500

    ART dec

--- source/aogmaneo/decoder.cpp
+++ source/aogmaneo/decoder.cpp
@@ -164,10 +164,8 @@ void Decoder::forward(
 
         hidden_dis[hidden_cell_index] = (max_index == -1 ? max_complete_index : max_index);
 
-        float compare_activation = max_complete_activation;
-
-        if (compare_activation > max_compare_activation) {
-            max_compare_activation = compare_activation;
+        if (max_complete_activation > max_compare_activation) {
+            max_compare_activation = max_complete_activation;
             max_compare_index = hc;
         }
     }
