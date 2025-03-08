caca7d327d5fa224ebda48ccdb43c6efe61c5103
Eric Laukien <ericlaukien@gmail.com>
2025-03-08 18:55:51 -0500

    Softplus

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -58,7 +58,7 @@ public:
         spatial_mismatch(2.0f),
         temporal_mismatch(2.0f),
         lr(0.5f),
-        active_ratio(0.2f),
+        active_ratio(0.1f),
         l_radius(2)
         {}
     };
