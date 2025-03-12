b5e8cffa56385ba9920e6fdfddb8d93792914c84
Eric Laukien <ericlaukien@gmail.com>
2025-03-11 22:17:34 -0400

    Tuning

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -57,10 +57,10 @@ public:
         Params()
         :
         vlr(0.001f),
-        plr(0.004f),
+        plr(0.001f),
         leak(0.01f),
         smoothing(0.02f),
-        bias(0.1f),
+        bias(0.5f),
         discount(0.99f),
         td_scale_decay(0.999f),
         min_steps(16),
