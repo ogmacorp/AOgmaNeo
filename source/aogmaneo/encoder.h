8ffe081e3734fd34a3a7699c0534aa55423db688
Eric Laukien <ericlaukien@gmail.com>
2025-03-16 12:43:50 -0400

    Tuning

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -51,7 +51,7 @@ public:
         Params()
         :
         scale(4.0f),
-        lr(0.02f),
+        lr(0.05f),
         spatial_recon_tolerance(2),
         recurrent_recon_tolerance(2)
         {}
