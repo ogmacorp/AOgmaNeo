c51088e4c6a584286cb62892b1d63e54737f8880
Eric Laukien <ericlaukien@gmail.com>
2025-03-19 21:04:32 -0400

    Tests

--- source/aogmaneo/image_encoder.h
+++ source/aogmaneo/image_encoder.h
@@ -52,7 +52,7 @@ public:
         :
         falloff(0.99f),
         choice(0.0001f),
-        vigilance(0.9f),
+        vigilance(0.95f),
         lr(0.5f),
         scale(2.0f),
         rr(0.05f),
