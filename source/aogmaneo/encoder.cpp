850646e17bc6a95982341ef9c47e68fc09a356ae
Eric Laukien <ericlaukien@gmail.com>
2025-03-20 18:04:30 -0400

    Experiments with init

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -243,8 +243,14 @@ void Encoder::init_random(
 
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
-        for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = 255 - (rand() % init_weight_noisei);
+        if (vld.is_recurrent) {
+            for (int i = 0; i < vl.weights.size(); i++)
+                vl.weights[i] = (rand() % 256);
+        }
+        else {
+            for (int i = 0; i < vl.weights.size(); i++)
+                vl.weights[i] = 255 - (rand() % init_weight_noisei);
+        }
 
         vl.recon_sums.resize(num_visible_cells);
     }
