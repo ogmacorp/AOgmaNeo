ba9a467b8724cdd7c1e933ceccab70d051da84d3
Eric Laukien <ericlaukien@gmail.com>
2025-03-29 21:17:53 -0400

    Tests

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -48,8 +48,8 @@ public:
 
         Params()
         :
-        vlr(0.01f),
-        plr(0.1f),
+        vlr(0.004f),
+        plr(0.08f),
         discount(0.99f),
         td_scale_decay(0.9999f),
         trace_decay(0.97f),
