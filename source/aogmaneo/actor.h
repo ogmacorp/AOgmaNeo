51cb4e7718f5410fa03ea016da06975b7ba72b91
Eric Laukien <ericlaukien@gmail.com>
2025-03-30 14:08:39 -0400

    Tests

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -58,7 +58,7 @@ public:
         Params()
         :
         vlr(0.001f),
-        plr(0.05f),
+        plr(0.01f),
         smoothing(0.02f),
         delay_rate(0.001f),
         policy_clip(0.2f),
