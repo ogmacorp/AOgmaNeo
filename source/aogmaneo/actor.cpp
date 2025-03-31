5debd8c2a7319d057df52382c92659e3885eea61
Eric Laukien <ericlaukien@gmail.com>
2025-03-31 13:02:23 -0400

    Value clip

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -379,7 +379,7 @@ void Actor::learn(
 
     float scaled_td_error = td_error / max(limit_small, hidden_td_scales[hidden_column_index]);
     
-    float value_delta = params.vlr * td_error;
+    float value_delta = params.vlr * min(params.value_clip, max(-params.value_clip, td_error));
 
     // probability ratio
     float ratio = hidden_acts[target_ci + hidden_cells_start] / max(limit_small, hidden_acts_delayed[target_ci + hidden_cells_start]);
