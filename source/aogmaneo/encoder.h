7742d43569c693149194ac96275da4239e177c7d
Eric Laukien <ericlaukien@gmail.com>
2025-04-03 12:30:31 -0400

    Tuning

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -58,8 +58,8 @@ public:
         spatial_mismatch(2.0f),
         temporal_mismatch(2.0f),
         lr(1.0f),
-        active_ratio(0.5f),
-        l_radius(1)
+        active_ratio(0.1f),
+        l_radius(2)
         {}
     };
 
