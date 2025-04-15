a736c54a215ee83cfbd435af4e4b8605a65cb9d7
Eric Laukien <ericlaukien@gmail.com>
2025-04-15 18:29:42 -0400

    Tests 2

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -253,7 +253,7 @@ void Encoder::init_random(
         vl.weights.resize(num_hidden_cells * area * vld.size.z);
 
         for (int i = 0; i < vl.weights.size(); i++)
-            vl.weights[i] = (rand() % 256);
+            vl.weights[i] = (rand() % init_weight_noisei);
 
         vl.hidden_sums.resize(num_hidden_cells);
         vl.hidden_totals.resize(num_hidden_cells);
