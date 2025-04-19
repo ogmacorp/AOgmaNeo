705b6373aabf9c61912f34972749613b55b07771
Eric Laukien <ericlaukien@gmail.com>
2025-04-19 15:44:32 -0400

    Tuning

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -30,11 +30,11 @@ public:
 
     // visible layer
     struct Visible_Layer {
-        Float_Buffer weights0;
-        Float_Buffer weights1;
-        
-        Float_Buffer hidden_sums;
-        Float_Buffer hidden_totals;
+        Byte_Buffer weights;
+
+        Int_Buffer recon_sums;
+
+        Float_Buffer deltas;
 
         float importance;
 
@@ -45,23 +45,15 @@ public:
     };
 
     struct Params {
-        float falloff; // neighborhood falloff
-        float choice; // choice parameter, higher makes it select matchier columns over ones with less overall weights (total)
-        float vigilance; // standard ART vigilance
+        float scale; // recon curve
         float lr; // learning rate
-        float active_ratio; // 2nd stage inhibition activity ratio
-        int l_radius; // second stage inhibition radius
-        int n_radius; // neighborhood radius
+        int early_stop; // if target of reconstruction is in top <this number> cells, stop early
 
         Params()
         :
-        falloff(0.99f),
-        choice(0.01f),
-        vigilance(0.95f),
-        lr(0.5f),
-        active_ratio(0.25f),
-        l_radius(2),
-        n_radius(1)
+        scale(2.0f),
+        lr(0.1f),
+        early_stop(1)
         {}
     };
 
@@ -70,18 +62,16 @@ private:
 
     Int_Buffer hidden_cis;
 
-    Byte_Buffer hidden_learn_flags;
-
-    Byte_Buffer hidden_commit_flags;
-
-    Float_Buffer hidden_comparisons;
+    Float_Buffer hidden_acts;
 
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
-
-    // --- kernels ---
     
+    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
+    
+    // --- kernels ---
+
     void forward(
         const Int2 &column_pos,
         const Array<Int_Buffer_View> &input_cis,
@@ -90,7 +80,8 @@ private:
 
     void learn(
         const Int2 &column_pos,
-        const Array<Int_Buffer_View> &input_cis,
+        Int_Buffer_View input_cis,
+        int vli,
         const Params &params
     );
 
@@ -110,9 +101,9 @@ public:
     void clear_state();
 
     // serialization
-    long size() const; // returns size in Bytes
-    long state_size() const; // returns size of state in Bytes
-    long weights_size() const; // returns size of weights in Bytes
+    long size() const; // returns size in bytes
+    long state_size() const; // returns size of state in bytes
+    long weights_size() const; // returns size of weights in bytes
 
     void write(
         Stream_Writer &writer
