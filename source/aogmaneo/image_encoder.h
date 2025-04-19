6a9fa1be06668e228f8249635cccf715aa085a8b
Eric Laukien <ericlaukien@gmail.com>
2025-04-19 11:56:56 -0400

    Tuning

--- source/aogmaneo/image_encoder.h
+++ source/aogmaneo/image_encoder.h
@@ -56,8 +56,8 @@ public:
         lr(0.5f),
         scale(2.0f),
         rr(0.05f),
-        active_ratio(0.5f),
-        l_radius(2),
+        active_ratio(1.0f),
+        l_radius(0),
         n_radius(1)
         {}
     };
