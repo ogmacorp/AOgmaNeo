acb256dc70d96f29b6e94e82995c30296d0f0087
Eric Laukien <ericlaukien@gmail.com>
2025-04-27 19:15:17 -0400

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -59,8 +59,8 @@ public:
         :
         falloff(0.99f),
         choice(0.01f),
-        vigilance_high(0.9f),
-        vigilance_low(0.8f),
+        vigilance_high(0.95f),
+        vigilance_low(0.9f),
         low_activation(0.01f),
         lr(0.5f),
         active_ratio(0.1f),
