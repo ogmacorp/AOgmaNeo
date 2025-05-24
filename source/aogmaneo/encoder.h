2e2769a5e43d962f96969f917c698be9d6e1266d
Eric Laukien <ericlaukien@gmail.com>
2025-05-24 01:56:43 -0400

    ART

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------
 //  AOgmaNeo
-//  Copyright(c) 2020-2025 Ogma Intelligent Systems Corp. All rights reserved.
+//  Copyright(c) 2020-2024 Ogma Intelligent Systems Corp. All rights reserved.
 //
 //  This copy of AOgmaNeo is licensed to you under the terms described
 //  in the AOGMANEO_LICENSE.md file included in this distribution.
@@ -23,7 +23,7 @@ public:
         // defaults
         Visible_Layer_Desc()
         :
-        size(5, 5, 16),
+        size(4, 4, 16),
         radius(2)
         {}
     };
@@ -31,10 +31,9 @@ public:
     // visible layer
     struct Visible_Layer {
         Byte_Buffer weights;
-
-        Int_Buffer recon_sums;
-
-        Float_Buffer deltas;
+        
+        Int_Buffer hidden_sums;
+        Int_Buffer hidden_totals;
 
         float importance;
 
@@ -45,15 +44,19 @@ public:
     };
 
     struct Params {
-        float scale; // recon curve
+        float choice; // choice parameter, higher makes it select matchier columns over ones with less overall weights (total)
+        float vigilance; // ART vigilance
         float lr; // learning rate
-        int early_stop; // if target of reconstruction is in top <this number> cells, stop early
+        float active_ratio; // 2nd stage inhibition activity ratio
+        int l_radius; // second stage inhibition radius
 
         Params()
         :
-        scale(2.0f),
-        lr(0.1f),
-        early_stop(1)
+        choice(0.01f),
+        vigilance(0.8f),
+        lr(0.5f),
+        active_ratio(0.1f),
+        l_radius(2)
         {}
     };
 
@@ -62,16 +65,14 @@ private:
 
     Int_Buffer hidden_cis;
 
-    Float_Buffer hidden_acts;
+    Float_Buffer hidden_comparisons;
 
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
     
-    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
-    
     // --- kernels ---
-
+    
     void forward(
         const Int2 &column_pos,
         const Array<Int_Buffer_View> &input_cis,
@@ -80,8 +81,7 @@ private:
 
     void learn(
         const Int2 &column_pos,
-        Int_Buffer_View input_cis,
-        int vli,
+        const Array<Int_Buffer_View> &input_cis,
         const Params &params
     );
 
@@ -101,9 +101,9 @@ public:
     void clear_state();
 
     // serialization
-    long size() const; // returns size in bytes
-    long state_size() const; // returns size of state in bytes
-    long weights_size() const; // returns size of weights in bytes
+    long size() const; // returns size in Bytes
+    long state_size() const; // returns size of state in Bytes
+    long weights_size() const; // returns size of weights in Bytes
 
     void write(
         Stream_Writer &writer
