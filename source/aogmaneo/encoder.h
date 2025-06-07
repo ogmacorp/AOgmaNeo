24d89cc428534d29e53e6bc5a64cc7f1f8e5584d
Eric Laukien <ericlaukien@gmail.com>
2025-06-06 23:49:06 -0400

    Distributed

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -57,7 +57,7 @@ public:
         :
         falloff(0.99f),
         choice(0.01f),
-        vigilance(0.7f),
+        vigilance(0.8f),
         lr(0.1f),
         active_ratio(0.1f),
         l_radius(2),
