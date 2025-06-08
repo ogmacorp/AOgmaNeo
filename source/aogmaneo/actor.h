1e563e90d2fbd5a031e21dc33e8fccfdc4bd3ad8
Eric Laukien <ericlaukien@gmail.com>
2025-06-08 06:35:34 -0400

    AWAC

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -51,8 +51,8 @@ public:
 
         Params()
         :
-        qlr(0.001f),
-        plr(0.01f),
+        qlr(0.01f),
+        plr(0.1f),
         discount(0.99f),
         n_steps(8),
         history_iters(16)
