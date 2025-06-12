88f71831646e673979bce35f95e58f3ed3230602
Eric Laukien <ericlaukien@gmail.com>
2025-06-12 08:44:34 -0400

    Tuning

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -371,8 +371,29 @@ void Actor::learn(
 
     int hidden_cell_index_target = target_ci + hidden_cells_start;
 
+    // softmax q for weights
+    {
+        float total = 0.0f;
+
+        for (int hc = 0; hc < hidden_size.z; hc++) {
+            int hidden_cell_index = hc + hidden_cells_start;
+        
+            hidden_weights[hidden_cell_index] = expf(params.reweight * (hidden_qs[hidden_cell_index] - max_q_prev));
+
+            total += hidden_weights[hidden_cell_index];
+        }
+
+        float total_inv = 1.0f / max(limit_small, total);
+
+        for (int hc = 0; hc < hidden_size.z; hc++) {
+            int hidden_cell_index = hc + hidden_cells_start;
+
+            hidden_weights[hidden_cell_index] *= total_inv;
+        }
+    }
+
     // AWAC weight
-    float weight = expf(params.reweight * (hidden_qs[hidden_cell_index_target] - max_q_prev));
+    float weight = hidden_weights[hidden_cell_index_target];
 
     // softmax p
     {
