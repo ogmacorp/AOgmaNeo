b754fad9bcc279981aee1aac4f1666533516e0b7
Eric Laukien <ericlaukien@gmail.com>
2025-06-12 10:05:19 -0400

    Enc revert

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -32,8 +32,6 @@ public:
     struct Visible_Layer {
         Byte_Buffer weights;
 
-        Int_Buffer hidden_sums;
-
         Int_Buffer recon_sums;
 
         float importance;
@@ -45,11 +43,13 @@ public:
     };
 
     struct Params {
+        float scale; // recon curve
         float lr; // learning rate
 
         Params()
         :
-        lr(0.05f)
+        scale(1.0f),
+        lr(0.02f)
         {}
     };
 
@@ -58,12 +58,14 @@ private:
 
     Int_Buffer hidden_cis;
 
+    Float_Buffer hidden_acts;
+
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
     
     Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
-
+    
     // --- kernels ---
 
     void forward(
@@ -74,8 +76,9 @@ private:
 
     void learn(
         const Int2 &column_pos,
-        int vli,
         Int_Buffer_View input_cis,
+        int vli,
+        unsigned long* state,
         const Params &params
     );
 
