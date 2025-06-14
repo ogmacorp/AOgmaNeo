ab8c05ab42f9d33fda35024067dd981008fe164b
Eric Laukien <ericlaukien@gmail.com>
2025-06-14 06:49:20 -0400

    Squash

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -162,7 +162,6 @@ void Actor::forward(
 
         float value_rate = params.vlr * td_error;
         float policy_rate = params.plr * td_error;
-        float mimic_rate = params.plr * mimic;
 
         for (int hc = 0; hc < hidden_size.z; hc++) {
             int hidden_cell_index = hc + hidden_cells_start;
@@ -233,7 +232,7 @@ void Actor::forward(
                                 if (vc == in_ci_prev)
                                     vl.policy_traces[wi] += dendrite_acts_prev[dendrite_index] * expf(-params.trace_squash * abs(vl.policy_traces[wi]));
 
-                                vl.policy_weights[wi] += policy_rate * vl.policy_traces[wi] + mimic_rate * dendrite_acts_prev[dendrite_index] * (vc == in_ci_prev);
+                                vl.policy_weights[wi] += policy_rate * vl.policy_traces[wi] + mimic * dendrite_acts_prev[dendrite_index] * (vc == in_ci_prev);
                                 vl.policy_traces[wi] *= params.trace_decay;
                             }
                         }
