a7128317ef55808afabda7d8eb7121cc18b8ee91
Eric Laukien <ericlaukien@gmail.com>
2025-06-14 03:21:37 -0400

    no td scale on value

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -50,8 +50,8 @@ public:
 
         Params()
         :
-        lr(0.001f),
-        bc(0.01f),
+        lr(0.01f),
+        bc(0.1f),
         discount(0.99f),
         n_steps(8),
         history_iters(16)
