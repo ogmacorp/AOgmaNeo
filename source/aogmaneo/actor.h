5eb1004f9e99123590432f12062ff18b52e5e93e
Eric Laukien <ericlaukien@gmail.com>
2025-06-14 00:48:21 -0400

    Tuning

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -55,7 +55,7 @@ public:
         qlr(0.001f),
         plr(0.1f),
         discount(0.99f),
-        reweight(1.0f),
+        reweight(0.1f),
         n_steps(8),
         history_iters(16)
         {}
