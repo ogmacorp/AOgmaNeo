fcd0cbe83eb534053829a85dfe04134f8b7a2880
Eric Laukien <ericlaukien@gmail.com>
2025-06-15 10:04:29 -0400

    Tests

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -383,7 +383,7 @@ void Actor::learn(
     // https://huggingface.co/blog/deep-rl-ppo
     bool policy_clip = (ratio < (1.0f - params.policy_clip) && td_error < 0.0f) || (ratio > (1.0f + params.policy_clip) && td_error > 0.0f);
 
-    float policy_error_partial = (mimic == 0.0f) * params.plr * td_error * (!policy_clip) + mimic;
+    float policy_error_partial = params.plr * td_error * (!policy_clip) + mimic;
 
     for (int di = 0; di < value_num_dendrites_per_cell; di++) {
         int dendrite_index = di + value_dendrites_start;
@@ -599,7 +599,7 @@ void Actor::step(
         for (int vli = 0; vli < visible_layers.size(); vli++) {
             Visible_Layer &vl = visible_layers[vli];
 
-            if (mimic > 0.0f) // if mimicking
+            if (mimic != 0.0f) // if mimicking
                 vl.policy_weights_delayed = vl.policy_weights;
             else {
                 PARALLEL_FOR
