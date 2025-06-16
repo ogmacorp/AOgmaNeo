7aee5b9299c99c7f6e9f5a7ed31058c6e1f06ed5
Eric Laukien <ericlaukien@gmail.com>
2025-06-16 02:07:44 -0400

    Tests

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -212,7 +212,7 @@ void Actor::forward(
                         int wi_base = offset.y + diam * (offset.x + diam * (vc + vld.size.z * hidden_column_index));
 
                         if (vc == in_ci_prev)
-                            vl.value_traces[wi_base] = 1.0f;
+                            vl.value_traces[wi_base] += params.trace_rate * (1.0f - vl.value_traces[wi_base]);
 
                         vl.value_weights[wi_base] += value_rate * vl.value_traces[wi_base];
 
@@ -233,7 +233,7 @@ void Actor::forward(
                                 int wi = di + wi_start;
 
                                 if (vc == in_ci_prev && hc == target_ci)
-                                    vl.policy_traces[wi] = max(vl.policy_weights[wi], (1.0f - hidden_acts_prev[hidden_cell_index]) * dendrite_acts_prev[dendrite_index]);
+                                    vl.policy_traces[wi] += params.trace_rate * (1.0f - vl.policy_traces[wi]) * dendrite_acts_prev[dendrite_index];
 
                                 vl.policy_weights[wi] += ((di >= half_num_dendrites_per_cell) * 2.0f - 1.0f) * (policy_rate * vl.policy_traces[wi] + mimic * ((hc == target_ci) - hidden_acts_prev[hidden_cell_index]) * dendrite_acts_prev[dendrite_index] * (vc == in_ci_prev));
 
