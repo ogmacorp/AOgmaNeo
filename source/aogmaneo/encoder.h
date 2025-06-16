2c773f69257f94971cceef9d7d5e7ac18e4d6ad3
Eric Laukien <ericlaukien@gmail.com>
2025-06-16 07:12:51 -0400

    Tests with topo

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------
 //  AOgmaNeo
-//  Copyright(c) 2020-2025 Ogma Intelligent Systems Corp. All rights reserved.
+//  Copyright(c) 2020-2024 Ogma Intelligent Systems Corp. All rights reserved.
 //
 //  This copy of AOgmaNeo is licensed to you under the terms described
 //  in the AOGMANEO_LICENSE.md file included in this distribution.
@@ -30,15 +30,8 @@ public:
 
     // visible layer
     struct Visible_Layer {
-        Byte_Buffer weights;
-
-        Int_Buffer hidden_sums;
-        Int_Buffer hidden_counts;
-
-        Byte_Buffer recons;
-
-        Byte_Buffer recon_cis;
-
+        Float_Buffer protos;
+        
         float importance;
 
         Visible_Layer()
@@ -48,13 +41,21 @@ public:
     };
 
     struct Params {
-        float vigilance; // match before learning halts
+        float falloff; // SOM neighborhood falloff
+        float choice; // choose used columns more
         float lr; // learning rate
+        float active_ratio; // 2nd stage inhibition activity ratio
+        int l_radius; // second stage inhibition radius
+        int n_radius; // SOM neighborhood radius
 
         Params()
         :
-        vigilance(0.96f),
-        lr(0.05f)
+        falloff(0.9f),
+        choice(0.01f),
+        lr(0.1f),
+        active_ratio(0.1f),
+        l_radius(2),
+        n_radius(1)
         {}
     };
 
@@ -63,23 +64,21 @@ private:
 
     Int_Buffer hidden_cis;
 
+    Float_Buffer hidden_resources;
+
+    Float_Buffer hidden_acts;
+
+    Float_Buffer hidden_comparisons;
+
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
     
-    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
-
     // --- kernels ---
-
+    
     void forward(
         const Int2 &column_pos,
         const Array<Int_Buffer_View> &input_cis,
-        const Params &params
-    );
-
-    void backward(
-        const Int2 &column_pos,
-        int vli,
         const Params &params
     );
 
@@ -102,7 +101,9 @@ public:
         const Params &params // parameters
     );
 
-    void clear_state();
+    void clear_state() {
+        hidden_cis.fill(0);
+    }
 
     // serialization
     long size() const; // returns size in bytes
