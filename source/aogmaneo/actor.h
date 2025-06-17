9070deadcaec55b90bead7421208b2153b59e00d
Eric Laukien <ericlaukien@gmail.com>
2025-06-17 05:06:02 -0400

    TD scales

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -61,7 +61,7 @@ public:
         plr(0.02f),
         smoothing(0.02f),
         delay_rate(0.001f),
-        policy_clip(0.2f),
+        policy_clip(0.1f),
         discount(0.99f),
         td_scale_decay(0.999f),
         min_steps(16),
