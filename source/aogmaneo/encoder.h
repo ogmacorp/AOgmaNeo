a378c75ef24a9f5c416fdd2989027b49a9193566
Eric Laukien <ericlaukien@gmail.com>
2025-06-19 05:52:01 -0400

    Tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -50,12 +50,12 @@ public:
 
         Params()
         :
-        falloff(0.99f),
+        falloff(0.9f),
         choice(0.05f),
         lr(0.1f),
         active_ratio(0.1f),
         l_radius(2),
-        n_radius(1)
+        n_radius(2)
         {}
     };
 
