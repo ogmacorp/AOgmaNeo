7c3b6b4ee2ebf15d183e7cc97870c3861c812c94
Eric Laukien <ericlaukien@gmail.com>
2025-06-22 09:59:58 -0400

    ART 2stage

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -31,11 +31,9 @@ public:
     // visible layer
     struct Visible_Layer {
         Byte_Buffer weights;
-
+        
         Int_Buffer hidden_sums;
-        Int_Buffer hidden_counts;
-
-        Int_Buffer recon_sums;
+        Int_Buffer hidden_totals;
 
         float importance;
 
@@ -46,13 +44,19 @@ public:
     };
 
     struct Params {
-        float scale; // squash on recon
+        float choice; // choice parameter, higher makes it select matchier columns over ones with less overall weights (total)
+        float vigilance; // ART vigilance
         float lr; // learning rate
+        float active_ratio; // 2nd stage inhibition activity ratio
+        int l_radius; // second stage inhibition radius
 
         Params()
         :
-        scale(1.0f),
-        lr(0.02f)
+        choice(0.01f),
+        vigilance(0.9f),
+        lr(0.5f),
+        active_ratio(0.1f),
+        l_radius(2)
         {}
     };
 
@@ -61,14 +65,18 @@ private:
 
     Int_Buffer hidden_cis;
 
+    Byte_Buffer hidden_learn_flags;
+
+    Byte_Buffer hidden_commit_flags;
+
+    Float_Buffer hidden_comparisons;
+
     // visible layers and associated descriptors
     Array<Visible_Layer> visible_layers;
     Array<Visible_Layer_Desc> visible_layer_descs;
     
-    Array<Int3> visible_pos_vlis; // for parallelization, cartesian product of column coordinates and visible layers
-
     // --- kernels ---
-
+    
     void forward(
         const Int2 &column_pos,
         const Array<Int_Buffer_View> &input_cis,
@@ -77,9 +85,7 @@ private:
 
     void learn(
         const Int2 &column_pos,
-        int vli,
-        Int_Buffer_View input_cis,
-        unsigned long* state,
+        const Array<Int_Buffer_View> &input_cis,
         const Params &params
     );
 
@@ -99,9 +105,9 @@ public:
     void clear_state();
 
     // serialization
-    long size() const; // returns size in bytes
-    long state_size() const; // returns size of state in bytes
-    long weights_size() const; // returns size of weights in bytes
+    long size() const; // returns size in Bytes
+    long state_size() const; // returns size of state in Bytes
+    long weights_size() const; // returns size of weights in Bytes
 
     void write(
         Stream_Writer &writer
