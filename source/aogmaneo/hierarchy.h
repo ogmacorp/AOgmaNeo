f03d9771cc022bd413638ce72a1052385bea9448
Eric Laukien <ericlaukien@gmail.com>
2025-06-22 01:35:30 -0400

    Enc variant

--- source/aogmaneo/hierarchy.h
+++ source/aogmaneo/hierarchy.h
@@ -69,7 +69,7 @@ public:
             const Int3 &hidden_size = Int3(5, 5, 16),
             int num_dendrites_per_cell = 4,
             int up_radius = 2,
-            int recurrent_radius = 0,
+            int recurrent_radius = 2,
             int down_radius = 2
         )
         :
@@ -89,7 +89,7 @@ public:
 
         Layer_Params()
         :
-        recurrent_importance(0.1f)
+        recurrent_importance(0.2f)
         {}
     };
 
