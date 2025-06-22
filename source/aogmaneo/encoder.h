c0b1e931c5d8754e4f9eb65c34a8fe40e93afda3
Eric Laukien <ericlaukien@gmail.com>
2025-06-22 10:06:46 -0400

    SOM again

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------
 //  AOgmaNeo
-//  Copyright(c) 2020-2025 Ogma Intelligent Systems Corp. All rights reserved.
+//  Copyright(c) 2020-2024 Ogma Intelligent Systems Corp. All rights reserved.
 //
 //  This copy of AOgmaNeo is licensed to you under the terms described
 //  in the AOGMANEO_LICENSE.md file included in this distribution.
@@ -30,11 +30,8 @@ public:
 
     // visible layer
     struct Visible_Layer {
-        Byte_Buffer weights;
+        Float_Buffer protos;
         
-        Int_Buffer hidden_sums;
-        Int_Buffer hidden_totals;
-
         float importance;
 
         Visible_Layer()
@@ -44,19 +41,21 @@ public:
     };
 
     struct Params {
-        float choice; // choice parameter, higher makes it select matchier columns over ones with less overall weights (total)
-        float vigilance; // ART vigilance
+        float falloff; // SOM neighborhood falloff
+        float choice; // choose used columns more
         float lr; // learning rate
         float active_ratio; // 2nd stage inhibition activity ratio
         int l_radius; // second stage inhibition radius
+        int n_radius; // SOM neighborhood radius
 
         Params()
         :
-        choice(0.01f),
-        vigilance(0.9f),
-        lr(0.5f),
+        falloff(0.99f),
+        choice(0.1f),
+        lr(0.1f),
         active_ratio(0.1f),
-        l_radius(2)
+        l_radius(2),
+        n_radius(1)
         {}
     };
 
@@ -65,9 +64,9 @@ private:
 
     Int_Buffer hidden_cis;
 
-    Byte_Buffer hidden_learn_flags;
+    Float_Buffer hidden_resources;
 
-    Byte_Buffer hidden_commit_flags;
+    Float_Buffer hidden_acts;
 
     Float_Buffer hidden_comparisons;
 
@@ -102,12 +101,14 @@ public:
         const Params &params // parameters
     );
 
-    void clear_state();
+    void clear_state() {
+        hidden_cis.fill(0);
+    }
 
     // serialization
-    long size() const; // returns size in Bytes
-    long state_size() const; // returns size of state in Bytes
-    long weights_size() const; // returns size of weights in Bytes
+    long size() const; // returns size in bytes
+    long state_size() const; // returns size of state in bytes
+    long weights_size() const; // returns size of weights in bytes
 
     void write(
         Stream_Writer &writer
