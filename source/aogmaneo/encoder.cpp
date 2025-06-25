5d6846efeb52a76028ec4851168f5d237e99d40a
Eric Laukien <ericlaukien@gmail.com>
2025-06-25 03:12:29 -0400

    Experimental fast commit ART

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -116,7 +116,7 @@ void Encoder::forward(
 
         float activation = complemented / (params.choice + count_all - total);
 
-        if (match >= params.vigilance && activation > max_activation) {
+        if ((!hidden_committed_flags[hidden_cell_index] || match >= params.vigilance) && activation > max_activation) {
             max_activation = activation;
             max_index = hc;
         }
