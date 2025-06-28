a945fe34c67a07f2b469aa63b7cc1385f8e07890
Eric Laukien <ericlaukien@gmail.com>
2025-06-28 07:29:42 -0400

    ART full complement tests

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -30,10 +30,13 @@ public:
 
     // visible layer
     struct Visible_Layer {
-        Byte_Buffer weights;
+        Byte_Buffer weights0;
+        Byte_Buffer weights1;
         
-        Int_Buffer hidden_sums;
-        Int_Buffer hidden_totals;
+        Int_Buffer hidden_sums0;
+        Int_Buffer hidden_sums1;
+        Int_Buffer hidden_totals0;
+        Int_Buffer hidden_totals1;
 
         float importance;
 
@@ -45,7 +48,8 @@ public:
 
     struct Params {
         float choice; // choice parameter, higher makes it select matchier columns over ones with less overall weights (total)
-        float vigilance; // ART vigilance
+        float vigilance0; // ART vigilance for 0 weights
+        float vigilance1; // ART vigilance for 1 weights
         float lr; // learning rate
         float active_ratio; // 2nd stage inhibition activity ratio
         int l_radius; // second stage inhibition radius
@@ -53,9 +57,10 @@ public:
         Params()
         :
         choice(0.01f),
-        vigilance(0.9f),
+        vigilance0(0.5f),
+        vigilance1(0.9f),
         lr(0.5f),
-        active_ratio(0.1f),
+        active_ratio(0.25f),
         l_radius(2)
         {}
     };
