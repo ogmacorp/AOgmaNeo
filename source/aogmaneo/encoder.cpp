3ef66f344389c6afa53303f67cad41667ab7fefa
Eric Laukien <ericlaukien@gmail.com>
2025-07-05 23:43:04 -0400

    Experiments

--- source/aogmaneo/encoder.cpp
+++ source/aogmaneo/encoder.cpp
@@ -107,11 +107,8 @@ void Encoder::forward(
         float match0 = sum0 / count;
         float match1 = (total1 - sum1) / count_except;
 
-        float activation0 = match0 / (params.choice + total0);
-        float activation1 = match1 / (params.choice + total1);
-
-        float match = max(match0, match1);
-        float activation = min(activation0, activation1);
+        float match = min(match0, match1);
+        float activation = (match0 + match1) / (params.choice + total0 + total1);
 
         if ((!hidden_committed_flags[hidden_cell_index] || match >= params.vigilance) && activation > max_activation) {
             max_activation = activation;
