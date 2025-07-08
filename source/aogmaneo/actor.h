2ff3a42ba79e2d29d8c39dd4bdf8ac39b1708b88
Eric Laukien <ericlaukien@gmail.com>
2025-07-08 00:49:19 -0400

    Experiments 2

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -43,7 +43,7 @@ public:
         float plr; // policy learning rate
         float discount; // discount factor
         float trace_decay; // eligibility trace decay
-        float trace_squash; // trace squashing to prevent divergence
+        float td_scale_decay; // slow decay of max td error abs value
 
         Params()
         :
@@ -51,7 +51,7 @@ public:
         plr(0.001f),
         discount(0.99f),
         trace_decay(0.97f),
-        trace_squash(1.0f)
+        td_scale_decay(0.999f)
         {}
     };
 
