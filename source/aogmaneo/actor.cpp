65c1efc8dae79de9f6e5775777120d4f7d0aa8b1
Eric Laukien <ericlaukien@gmail.com>
2025-07-09 06:08:18 -0400

    Tuning

--- source/aogmaneo/actor.cpp
+++ source/aogmaneo/actor.cpp
@@ -7,6 +7,7 @@
 // ----------------------------------------------------------------------------
 
 #include "actor.h"
+#include <iostream>
 
 using namespace aon;
 
@@ -143,6 +144,7 @@ void Actor::forward(
         hidden_td_scales[hidden_column_index] = max(hidden_td_scales[hidden_column_index] * params.td_scale_decay, abs(td_error));
 
         float scaled_td_error = td_error / max(limit_small, hidden_td_scales[hidden_column_index]);
+        std::cout << scaled_td_error << std::endl;
 
         float value_rate = params.vlr * td_error;
         float policy_rate = params.plr * scaled_td_error;
