d9d91a2a681c3f7f5f4d676fd3f48fa67824b6f0
Eric Laukien <ericlaukien@gmail.com>
2025-07-10 04:40:07 -0400

    Topo

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -164,11 +164,5 @@ public:
     const Int3 &get_hidden_size() const {
         return hidden_size;
     }
-
-    // merge list of encoders and write to this one
-    void merge(
-        const Array<Encoder*> &encoders,
-        Merge_Mode mode
-    );
 };
 }
