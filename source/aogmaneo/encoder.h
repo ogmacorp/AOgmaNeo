28a472e1c6e1d783ce2a09ca870ee9b5bdab325a
Eric Laukien <ericlaukien@gmail.com>
2025-07-10 04:47:53 -0400

    Topo

--- source/aogmaneo/encoder.h
+++ source/aogmaneo/encoder.h
@@ -58,7 +58,7 @@ public:
         falloff(0.99f),
         choice(0.01f),
         vigilance(0.9f),
-        lr(0.1f),
+        lr(0.5f),
         active_ratio(0.1f),
         l_radius(2),
         n_radius(1)
