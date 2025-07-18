743647eff9132cbe1aff0cf288925304f1eb898c
Eric Laukien <ericlaukien@gmail.com>
2025-07-18 04:47:53 -0400

    Removed distribution

--- source/aogmaneo/actor.h
+++ source/aogmaneo/actor.h
@@ -48,7 +48,7 @@ public:
         Params()
         :
         vlr(0.1f),
-        plr(0.01f),
+        plr(0.1f),
         discount(0.99f),
         trace_rate(0.03f),
         td_scale_decay(0.999f)
